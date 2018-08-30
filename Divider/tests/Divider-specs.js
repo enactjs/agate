1e06b8ed1f2f2edef2659a09f6be4e5d0bbad866
Derek Tor <derek.tor@lge.com>
2018-08-30 15:49:55 -0700

    Added Divider and some packages
    Enact-DCO-1.0-Signed-off-by: Derek Tor <derek.tor@lge.com>

---
+++ Divider/tests/Divider-specs.js
@@ -0,0 +1,32 @@
+import React from 'react';
+import {mount} from 'enzyme';
+import Divider from '../Divider';
+import css from '../Divider.less';
+
+describe.only('Divider Specs', () => {
+	it('should render a Divider with content', function () {
+		const content = 'Hello Divider!';
+
+		const divider = mount(
+			<Divider>{content}</Divider>
+		);
+
+		const expected = content;
+		const actual = divider.text();
+
+		expect(actual).to.equal(expected);
+	});
+
+	it('should render with spacing class', function () {
+		const content = 'Hello Divider!';
+
+		const divider = mount(
+			<Divider spacing="medium">{content}</Divider>
+		);
+
+		const expected = true;
+		const actual = divider.find('h3').hasClass(css.medium);
+
+		expect(actual).to.equal(expected);
+	});
+});
