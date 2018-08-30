b38c5a72a4eeca036c66d54926cdefaac2931bac
Blake Stephens <blake.stephens@lge.com>
2018-08-30 13:34:56 -0700

    Added LabeledIcon and LabeledIconButton
    Signed-off-by: Blake Stephens <blake.stephens@lge.com>

---
+++ LabeledIcon/LabeledIcon.js
@@ -0,0 +1,54 @@
+import kind from '@enact/core/kind';
+import UiLabeledIcon from '@enact/ui/LabeledIcon';
+import Pure from '@enact/ui/internal/Pure';
+import compose from 'ramda/src/compose';
+import PropTypes from 'prop-types';
+
+import Icon from '../Icon';
+import Skinnable from '../Skinnable';
+
+import componentCss from './LabeledIcon.less';
+
+const LabeledIconBase = kind({
+	name: 'LabeledIcon',
+
+	propTypes: /** @lends moonstone/Icon.Icon.prototype */ {
+		/**
+		 * The icon content.
+		 *
+		 * May be specified as either:
+		 *
+		 * * A string that represents an icon from the [iconList]{@link ui/Icon.Icon.iconList},
+		 * * An HTML entity string, Unicode reference or hex value (in the form '0x...'),
+		 * * A URL specifying path to an icon image, or
+		 * * An object representing a resolution independent resource (See {@link ui/resolution}).
+		 *
+		 * @type {String|Object}
+		 * @public
+		 */
+		children: PropTypes.oneOfType([PropTypes.string, PropTypes.object]).isRequired
+	},
+
+	render: (props) => UiLabeledIcon.inline({
+		...props,
+		css: componentCss,
+		iconComponent: Icon
+	})
+});
+
+
+const LabeledIconDecorator = compose(
+	Pure,
+	Skinnable
+);
+
+
+const LabeledIcon = LabeledIconDecorator(LabeledIconBase);
+
+
+export default LabeledIcon;
+export {
+	LabeledIcon,
+	LabeledIconBase,
+	LabeledIconDecorator
+};
