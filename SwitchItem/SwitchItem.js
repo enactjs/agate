f94a2da53f15e08975622b18dc1e2ccd0632dda1
Derek Tor <derek.tor@lge.com>
2018-09-05 16:54:52 -0700

    clean up
    Enact-DCO-1.0-Signed-off-by: Derek Tor <derek.tor@lge.com>

--- SwitchItem/SwitchItem.js
+++ SwitchItem/SwitchItem.js
@@ -6,19 +6,19 @@
  */
 
 import React from 'react';
+import PropTypes from 'prop-types';
 
-import Item from '../Item';
-// import AgateSwitch from '@enact/agate/Switch';
+import kind from '@enact/core/kind';
+import AgateSwitch from '../Switch';
+import Icon from '../Icon';
+import Skinnable from '../Skinnable';
+
+import Spottable from '@enact/spotlight/Spottable';
+import Toggleable from '@enact/ui/Toggleable';
+import Touchable from '@enact/ui/Touchable';
+import compose from 'ramda/src/compose';
 
 import componentCss from './SwitchItem.less';
-
-const Switch = ({label, ...rest}) => (
-	<div>
-		<span className={componentCss.label}>{label}</span>
-		{/* <AgateSwitch {...rest} />*/}
-		<span {...rest} />
-	</div>
-);
 
 /**
  * Renders an `Item` with a radio-dot component. Useful to show a selected state on an Item.
@@ -29,13 +29,85 @@ const Switch = ({label, ...rest}) => (
  * @ui
  * @public
  */
-const SwitchItem = ({label, ...rest}) => (
-	<Item
-		{...rest}
-		css={componentCss}
-		// iconComponent={<Switch label={label} />}
-	/>
+
+
+const SwitchItemBase = kind({
+	name: 'SwitchItemBase',
+
+	propTypes: /** @lends agate/Divider.DividerBase.prototype */ {
+		/**
+		 * Customizes the component by mapping the supplied collection of CSS class names to the
+		 * corresponding internal Elements and states of this component.
+		 *
+		 * The following classes are supported:
+		 *
+		 * * `switchItem` - The root class name
+		 *
+		 * @type {Object}
+		 * @public
+		 */
+		css: PropTypes.object,
+
+		/**
+		 * Customize the component used as the switch.
+		 *
+		 * @type {Element|Function}
+		 * @default {@link agate/Switch.Switch}
+		 * @public
+		 */
+		icon: PropTypes.oneOfType([PropTypes.element, PropTypes.func]),
+
+		/**
+		 * If true the switch will be selected.
+		 *
+		 * @type {Boolean}
+		 * @default false {@link agate/Switch.Switch}
+		 * @public
+		 */
+		selected: PropTypes.bool
+	},
+
+	defaultProps: {
+		selected: false
+	},
+
+	styles: {
+		css: componentCss,
+		className: 'switchItem'
+	},
+
+	computed: {
+		icon: ({css, icon}) => (icon ? <Icon small className={css.icon}>{icon}</Icon> : null)
+	},
+
+	render: ({children, css, icon, selected, ...rest}) => {
+
+		return (
+			<div {...rest} css={css}>
+				{icon}
+				<div
+					className={css.text}
+				>
+					{children}
+				</div>
+				<div>
+					{selected ? 'on' : 'off'}
+				</div>
+				<AgateSwitch selected={selected} className={css.switchIcon} />
+			</div>
+		);
+	}
+});
+
+
+const SwitchItemDecorator = compose(
+	Toggleable({toggleProp: 'onTap'}),
+	Touchable,
+	Spottable,
+	Skinnable
 );
+
+const SwitchItem = SwitchItemDecorator(SwitchItemBase);
 
 export default SwitchItem;
 export {
