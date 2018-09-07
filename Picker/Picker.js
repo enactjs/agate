90f527abc434805346b56620fc38ab9c128947e9
Derek Tor <derek.tor@lge.com>
2018-09-07 13:53:51 -0700

    Added value to onChange
    Enact-DCO-1.0-Signed-off-by: Derek Tor <derek.tor@lge.com>

--- Picker/Picker.js
+++ Picker/Picker.js
@@ -27,7 +27,7 @@ class PickerBase extends Component {
 		this.setState(({index}) => {
 			const nextIndex = index + 1;
 			if (nextIndex < this.props.children.length) {
-				this.props.onChange(index);
+				this.props.onChange({index: index, value: this.props.children[index]});
 				return {index: nextIndex};
 			}
 		});
@@ -37,7 +37,7 @@ class PickerBase extends Component {
 		this.setState(({index}) => {
 			const nextIndex = index - 1;
 			if (nextIndex >= 0) {
-				this.props.onChange(index);
+				this.props.onChange({index: index, value: this.props.children[index]});
 				return {index: nextIndex};
 			}
 		});
