feb8ad96bbdc72e1d1bee64f07f5adf69b23db62
Derek Tor <derek.tor@lge.com>
2018-09-07 11:51:12 -0700

    Adding Picker
    Enact-DCO-1.0-Signed-off-by: Derek Tor <derek.tor@lge.com>

---
+++ Picker/Picker.js
@@ -0,0 +1,82 @@
+import React, {Component} from 'react';
+import Button from '../Button';
+import Skinnable from '../Skinnable';
+
+import css from './Picker.less';
+import Touchable from '@enact/ui/Touchable';
+import Spottable from '@enact/spotlight/Spottable';
+import compose from 'ramda/src/compose';
+
+import PropTypes from 'prop-types';
+
+const TouchableDiv = Touchable('div');
+const SpottableDiv = Spottable('div');
+
+class PickerBase extends Component {
+	static propTypes = {
+		onChange: PropTypes.func
+	}
+
+	constructor (props) {
+		super(props);
+		this.state = {
+			index: 0
+		};
+	}
+
+	handleIncrement = () => {
+		this.setState(({index}) => {
+			const nextIndex = index + 1;
+			if (nextIndex < this.props.children.length) {
+				this.props.onChange(index);
+				return {index: nextIndex};
+			}
+		});
+	}
+
+	handleDecrement = () => {
+		this.setState(({index}) => {
+			const nextIndex = index - 1;
+			if (nextIndex >= 0) {
+				this.props.onChange(index);
+				return {index: nextIndex};
+			}
+		});
+	}
+
+	handleFlick = (ev) => {
+		if (ev.direction === 'vertical') {
+			if (ev.velocityX > ev.velocityY) {
+				this.handleIncrement();
+			} else if (ev.velocityX < ev.velocityY) {
+				this.handleDecrement();
+			}
+		}
+	}
+
+	render () {
+		const values = this.props.children;
+
+		return (
+			<TouchableDiv className={css.picker} onFlick={this.handleFlick}>
+				<SpottableDiv className={css.item} onClick={this.handleDecrement}>{this.state.index > 0 ? values[this.state.index - 1] : ''}</SpottableDiv>
+				<div className={`${css.item} ${css.currentIndex}`}>{values[this.state.index]}</div>
+				<SpottableDiv className={css.item} onClick={this.handleIncrement}>{this.state.index < values.length - 1 ? values[this.state.index + 1] : ''}</SpottableDiv>
+			</TouchableDiv>
+		);
+	}
+}
+
+const PickerDecorator = compose(
+	Skinnable
+);
+
+const Picker = PickerDecorator(PickerBase);
+
+export default Picker;
+
+export {
+	Picker,
+	PickerBase,
+	PickerDecorator
+};
