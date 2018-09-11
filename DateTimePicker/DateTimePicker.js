7738721a1eee85c591cc7b6e458ee22d5e4eb4d8
Derek Tor <derek.tor@lge.com>
2018-09-11 10:24:21 -0700

    Fixed some layout
    Enact-DCO-1.0-Signed-off-by: Derek Tor <derek.tor@lge.com>

--- DateTimePicker/DateTimePicker.js
+++ DateTimePicker/DateTimePicker.js
@@ -40,51 +40,55 @@ class DateTimePicker extends React.Component {
 	render () {
 		return (
 			<Row>
-				<Cell>
-					<Row align=" space-evenly">
-						<Column align="center">
-							<Picker onChange={this.handleTimeChange('month')}>
-								{['1', '2', '3', '4', '5', '6', '7', '8', '9', '10', '11', '12']}
-							</Picker>
-							<p>Month</p>
-						</Column>
-						<Column align="center">
-							<Picker onChange={this.handleTimeChange('day')}>
-								{['1', '2', '3', '4', '5', '6', '7', '8', '9', '10', '11', '12', '13', '14', '15', '16', '17', '18', '19', '20', '21', '22', '23', '24', '25', '26', '27', '28', '29', '30', '31']}
-							</Picker>
-							<p>Day</p>
-						</Column>
-						<Column align="center">
-							<Picker onChange={this.handleTimeChange('year')}>
-								{['2018', '2019', '2020', '2021', '2022', '2023', '2024', '2025', '2026', '2027']}
-							</Picker>
-							<p>Year</p>
-						</Column>
+				<Cell size="70%">
+					<Row>
+						<Cell size="30%">
+							<Row align=" space-evenly">
+								<Column align="center">
+									<Picker onChange={this.handleTimeChange('month')}>
+										{['1', '2', '3', '4', '5', '6', '7', '8', '9', '10', '11', '12']}
+									</Picker>
+									<p>Month</p>
+								</Column>
+								<Column align="center">
+									<Picker onChange={this.handleTimeChange('day')}>
+										{['1', '2', '3', '4', '5', '6', '7', '8', '9', '10', '11', '12', '13', '14', '15', '16', '17', '18', '19', '20', '21', '22', '23', '24', '25', '26', '27', '28', '29', '30', '31']}
+									</Picker>
+									<p>Day</p>
+								</Column>
+								<Column align="center">
+									<Picker onChange={this.handleTimeChange('year')}>
+										{['2018', '2019', '2020', '2021', '2022', '2023', '2024', '2025', '2026', '2027']}
+									</Picker>
+									<p>Year</p>
+								</Column>
+							</Row>
+						</Cell>
+						<Cell size="30%">
+							<Row align=" space-evenly">
+								<Column align="center">
+									<Picker onChange={this.handleTimeChange('hour')}>
+										{['12', '1', '2', '3', '4', '5', '6', '7', '8', '9', '10', '11']}
+									</Picker>
+									<p>Hour</p>
+								</Column>
+								<Column align="center">
+									<Picker onChange={this.handleTimeChange('minute')}>
+										{['00', '01', '02', '03', '04', '05', '06', '07', '08', '09', '10', '11', '12', '13', '14', '15', '16', '17', '18', '19', '20', '21', '22', '23', '24', '25', '26', '27', '28', '29', '30', '31', '32', '33', '34', '35', '36', '37', '38', '39', '40', '41', '42', '43', '44', '45', '46', '47', '48', '49', '50', '51', '52', '53', '54', '55', '56', '57', '58', '59']}
+									</Picker>
+									<p>Minute</p>
+								</Column>
+								<Column align="center">
+									<Picker onChange={this.handleTimeChange('meridiem')}>
+										{['AM', 'PM']}
+									</Picker>
+									<p>{'AM/PM'}</p>
+								</Column>
+							</Row>
+						</Cell>
 					</Row>
 				</Cell>
-				<Cell>
-					<Row align=" space-evenly">
-						<Column align="center">
-							<Picker onChange={this.handleTimeChange('hour')}>
-								{['12', '1', '2', '3', '4', '5', '6', '7', '8', '9', '10', '11']}
-							</Picker>
-							<p>Hour</p>
-						</Column>
-						<Column align="center">
-							<Picker onChange={this.handleTimeChange('minute')}>
-								{['00', '01', '02', '03', '04', '05', '06', '07', '08', '09', '10', '11', '12', '13', '14', '15', '16', '17', '18', '19', '20', '21', '22', '23', '24', '25', '26', '27', '28', '29', '30', '31', '32', '33', '34', '35', '36', '37', '38', '39', '40', '41', '42', '43', '44', '45', '46', '47', '48', '49', '50', '51', '52', '53', '54', '55', '56', '57', '58', '59']}
-							</Picker>
-							<p>Minute</p>
-						</Column>
-						<Column align="center">
-							<Picker onChange={this.handleTimeChange('meridiem')}>
-								{['AM', 'PM']}
-							</Picker>
-							<p>{'AM/PM'}</p>
-						</Column>
-					</Row>
-				</Cell>
-				<Cell>
+				<Cell size="30%">
 					<Column>
 						<Button onClick={this.onSave}>Set Date & Time</Button>
 					</Column>
