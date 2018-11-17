ad2f209c663509ba9c0d6cccbc9a92f36dd3bfbb
Blake Stephens <blake.stephens@lge.com>
2018-11-16 23:36:36 -0800

    Added a drawer component
    It's basic, but it will be a fine starting point.
    The Icon is included, in case we want to eventually add a close/handle actuator.
    
    Signed-off-by: Blake Stephens <blake.stephens@lge.com>

---
+++ Drawer/Drawer.js
@@ -0,0 +1,84 @@
+import compose from 'ramda/src/compose';
+import kind from '@enact/core/kind';
+import PropTypes from 'prop-types';
+import React from 'react';
+import Layout, {Cell} from '@enact/ui/Layout';
+import Slottable from '@enact/ui/Slottable';
+import Transition from '@enact/ui/Transition';
+
+import Skinnable from '../Skinnable';
+// import Icon from '../Icon';
+
+import PopupState from '../Popup/PopupState';
+
+import componentCss from './Drawer.less';
+
+const DrawerBase = kind({
+	name: 'Drawer',
+
+	propTypes: {
+		css: PropTypes.object,
+		footer: PropTypes.node,
+		header: PropTypes.node,
+		noAnimation: PropTypes.bool,
+		onHide: PropTypes.func,
+		open: PropTypes.bool
+	},
+
+	defaultProps: {
+		noAnimation: false,
+		open: false,
+		orientation: 'vertical'
+	},
+
+	styles: {
+		css: componentCss,
+		className: 'drawer'
+	},
+
+	render: ({footer, children, css, noAnimation, onHide, open, header, ...rest}) => {
+		return (
+			<Transition
+				noAnimation={noAnimation}
+				visible={open}
+				direction="left"
+				duration="short"
+				type="slide"
+				className={css.drawerTransitionContainer}
+				onHide={onHide}
+				css={css}
+			>
+				<Layout
+					{...rest}
+				>
+					{/* <Icon small onClick={onClose} className={css.closeButton}>closex</Icon>*/}
+					{header ? <Cell shrink className={css.header}>{header}</Cell> : null}
+					<Cell className={css.content}>
+						{children}
+					</Cell>
+					{footer ? <Cell shrink className={css.footer}>{footer}</Cell> : null}
+				</Layout>
+			</Transition>
+		);
+	}
+});
+
+/**
+ * Applies Agate specific behaviors to [DividerBase]{@link agate/Divider.DividerBase}.
+ *
+ * @hoc
+ * @memberof agate/Divider
+ * @mixes agate/Skinnable.Skinnable
+ * @public
+ */
+
+const DrawerDecorator = compose(
+	Slottable({slots: ['header', 'footer']}),
+	Skinnable({prop: 'skin'}),
+	PopupState
+);
+
+const Drawer = DrawerDecorator(DrawerBase);
+
+export default Drawer;
+export {Drawer, DrawerBase};
