d497377e648fa6e3365ace78364f66e4cc4651c2
Dave Freeman <dave.freeman@lge.com>
2019-08-12 12:20:26 -0500

    update Panels for arranger changes

--- Panels/Panels.js
+++ Panels/Panels.js
@@ -1,52 +1,18 @@
 import kind from '@enact/core/kind';
 import PropTypes from 'prop-types';
 import React from 'react';
-import ViewManager, {shape, SlideArranger} from '@enact/ui/ViewManager';
+import ViewManager, {shape, SlideBottomArranger as VerticalArranger, SlideRightArranger as HorizontalArranger} from '@enact/ui/ViewManager';
 
 import Panel from './Panel';
 import TabbedPanels from './TabbedPanels';
 
 import componentCss from './Panels.module.less';
 
-import easing from 'eases/back-out';
-import * as arrange from '@enact/ui/ViewManager/arrange';
-
-const slideIn = arrange.reverse(arrange.fadeIn);
-const slideOut = arrange.reverse(arrange.fadeOut);
-
-const HorizontalArranger = SlideArranger({
-	amount: 50,
-	enter: 'right',
-	leave: 'left'
-});
-HorizontalArranger.enter = arrange.ease(
-	easing,
-	arrange.compose(slideIn, HorizontalArranger.enter)
-);
-HorizontalArranger.leave = arrange.ease(
-	easing,
-	arrange.compose(slideOut, HorizontalArranger.leave)
-);
-
-const VerticalArranger = SlideArranger({
-	amount: 50,
-	enter: 'bottom',
-	leave: 'top'
-});
-VerticalArranger.enter = arrange.ease(
-	easing,
-	arrange.compose(slideIn, VerticalArranger.enter)
-);
-VerticalArranger.leave = arrange.ease(
-	easing,
-	arrange.compose(slideOut, VerticalArranger.leave)
-);
-
 const mapChildren = (childs) => React.Children.map(childs, (child, index) => {
 	return child ? React.cloneElement(child, {
 		'data-index': index
 	}) : null;
-})
+});
 
 const PanelsBase = kind({
 	name: 'Panels',
