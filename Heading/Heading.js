8a8c6767e556dcdae85e976b4ec313eaabfd13f1
Blake Stephens <blake.stephens@lge.com>
2019-09-05 14:21:51 -0700

    PLAT-84726: Add Heading component (#125)
    * PLAT-84726: Added Heading component
    
    Enact-DCO-1.0-Signed-off-by: Blake Stephens <blake.stephens@lge.com>
    
    Signed-off-by: Blake Stephens <blake.stephens@lge.com>
    
    * Cleaned out crufty docs
    
    Enact-DCO-1.0-Signed-off-by: Blake Stephens <blake.stephens@lge.com>
    
    Signed-off-by: Blake Stephens <blake.stephens@lge.com>
    
    * Added missing color value
    
    Enact-DCO-1.0-Signed-off-by: Blake Stephens <blake.stephens@lge.com>
    
    Signed-off-by: Blake Stephens <blake.stephens@lge.com>
    
    * Updated customColor to color, default the spacing to auto, tightened title spacing
    
    Enact-DCO-1.0-Signed-off-by: Blake Stephens <blake.stephens@lge.com>
    
    Signed-off-by: Blake Stephens <blake.stephens@lge.com>
    
    Integrated-By: Ryan Duffy (ryan.duffy@lge.com)

---
+++ Heading/Heading.js
@@ -0,0 +1,147 @@
+/**
+ * Agate styled labeled Heading components and behaviors
+ *
+ * @example
+ * <Heading
+ *   size="large"
+ *   spacing="small"
+ * >
+ *   A Content Section Heading
+ * </Heading>
+ *
+ * @module agate/Heading
+ * @exports Heading
+ * @exports HeadingBase
+ * @exports HeadingDecorator
+ */
+
+import kind from '@enact/core/kind';
+import Pure from '@enact/ui/internal/Pure';
+import PropTypes from 'prop-types';
+import compose from 'ramda/src/compose';
+import UiHeading from '@enact/ui/Heading';
+
+import Skinnable from '../Skinnable';
+
+import componentCss from './Heading.module.less';
+
+/**
+ * A labeled Heading component.
+ *
+ * This component is most often not used directly but may be composed within another component as it
+ * is within [Heading]{@link agate/Heading.Heading}.
+ *
+ * @class HeadingBase
+ * @memberof agate/Heading
+ * @ui
+ * @public
+ */
+const HeadingBase = kind({
+	name: 'Heading',
+
+	propTypes: /** @lends agate/Heading.HeadingBase.prototype */ {
+		/**
+		 * Set a custom color to be used by the Heading. Defaults to the current accent color if
+		 * none is specified.
+		 *
+		 * @type {String}
+		 * @default null
+		 * @public
+		 */
+		color: PropTypes.string,
+
+		css: PropTypes.object,
+
+		/**
+		 * Adds a horizontal-rule (line) under the component
+		 *
+		 * @type {Boolean}
+		 * @public
+		 */
+		showLine: PropTypes.bool,
+
+		/**
+		 * The size of the spacing around the Heading.
+		 *
+		 * Allowed values include:
+		 * * `'auto'` - Value is based on the `size` prop for automatic usage.
+		 * * `'large'` - Specifically assign the `'large'` spacing.
+		 * * `'medium'` - Specifically assign the `'medium'` spacing.
+		 * * `'small'` - Specifically assign the `'small'` spacing.
+		 * * `'none'` - No spacing at all. Neighboring elements will directly touch the Heading.
+		 *
+		 * @type {('auto'|'title'|'large'|'medium'|'small'|'none')}
+		 * @default 'auto'
+		 * @public
+		 */
+		spacing: PropTypes.oneOf(['auto', 'title', 'large', 'medium', 'small', 'none'])
+	},
+
+	defaultProps: {
+		spacing: 'auto'
+	},
+
+	styles: {
+		css: componentCss,
+		className: 'heading'
+	},
+
+	computed: {
+		className: ({showLine, styler}) => styler.append({showLine}),
+		style: ({color, style}) => ({
+			...style,
+			'--agate-heading-accent': color
+		})
+	},
+
+	render: ({css, ...rest}) => {
+		delete rest.color;
+		delete rest.showLine;
+		return UiHeading.inline({css, ...rest});
+	}
+});
+
+/**
+ * Applies Agate specific behaviors to [HeadingBase]{@link agate/Heading.HeadingBase}.
+ *
+ * @hoc
+ * @memberof agate/Heading
+ * @mixes agate/Skinnable.Skinnable
+ * @public
+ */
+const HeadingDecorator = compose(
+	Pure,
+	Skinnable
+);
+
+/**
+ * A labeled Heading component, ready to use in Agate applications.
+ *
+ * `Heading` may be used as a header to group related components.
+ *
+ * Usage:
+ * ```
+ * <Heading
+ *   spacing="medium"
+ * >
+ *   Related Settings
+ * </Heading>
+ * <CheckboxItem>A Setting</CheckboxItem>
+ * <CheckboxItem>A Second Setting</CheckboxItem>
+ * ```
+ *
+ * @class Heading
+ * @memberof agate/Heading
+ * @extends agate/Heading.HeadingBase
+ * @mixes agate/Heading.HeadingDecorator
+ * @ui
+ * @public
+ */
+const Heading = HeadingDecorator(HeadingBase);
+
+export default Heading;
+export {
+	Heading,
+	HeadingBase,
+	HeadingDecorator
+};
