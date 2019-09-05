e6fe0ed97fdc703e632e4847e88dc9bb7ea29f22
Blake Stephens <blake.stephens@lge.com>
2019-09-05 15:29:50 -0700

    PLAT-84897: Add PopupMenu component (#127)
    * PLAT-84726: Added Heading component
    
    Enact-DCO-1.0-Signed-off-by: Blake Stephens <blake.stephens@lge.com>
    
    Signed-off-by: Blake Stephens <blake.stephens@lge.com>
    
    * PLAT-85175: Updated Icon and IconButton with huge size, Button with new colors
    
    Enact-DCO-1.0-Signed-off-by: Blake Stephens <blake.stephens@lge.com>
    
    Signed-off-by: Blake Stephens <blake.stephens@lge.com>
    
    * PLAT-84897: Added PopupMenu component
    
    Enact-DCO-1.0-Signed-off-by: Blake Stephens <blake.stephens@lge.com>
    
    Signed-off-by: Blake Stephens <blake.stephens@lge.com>
    
    * Cleaned out crufty docs
    
    Enact-DCO-1.0-Signed-off-by: Blake Stephens <blake.stephens@lge.com>
    
    Signed-off-by: Blake Stephens <blake.stephens@lge.com>
    
    * Added missing color value
    
    Enact-DCO-1.0-Signed-off-by: Blake Stephens <blake.stephens@lge.com>
    
    Signed-off-by: Blake Stephens <blake.stephens@lge.com>
    
    * Updated customColor to color, default the spacing to auto, tightened title spacing
    
    Enact-DCO-1.0-Signed-off-by: Blake Stephens <blake.stephens@lge.com>
    
    Signed-off-by: Blake Stephens <blake.stephens@lge.com>
    
    * Removed comment cruft
    
    Enact-DCO-1.0-Signed-off-by: Blake Stephens <blake.stephens@lge.com>
    
    Signed-off-by: Blake Stephens <blake.stephens@lge.com>
    
    * Hey! Get outa here, PopupMenuState.js; you're not needed.
    
    Enact-DCO-1.0-Signed-off-by: Blake Stephens <blake.stephens@lge.com>
    
    Signed-off-by: Blake Stephens <blake.stephens@lge.com>
    
    Reviewed-By: Dave Freeman (dave.freeman@lge.com)
    Integrated-By: Ryan Duffy (ryan.duffy@lge.com)

---
+++ PopupMenu/PopupMenu.js
@@ -0,0 +1,90 @@
+import compose from 'ramda/src/compose';
+import kind from '@enact/core/kind';
+import PropTypes from 'prop-types';
+import React from 'react';
+import Layout, {Cell} from '@enact/ui/Layout';
+import Slottable from '@enact/ui/Slottable';
+import Transition from '@enact/ui/Transition';
+
+import Skinnable from '../Skinnable';
+import Heading from '../Heading';
+import IconButton from '../IconButton';
+
+import PopupState from '../Popup/PopupState';
+
+import componentCss from './PopupMenu.module.less';
+
+const PopupMenuBase = kind({
+	name: 'PopupMenu',
+	propTypes: {
+		closeButton: PropTypes.bool,
+		css: PropTypes.object,
+		noAnimation: PropTypes.bool,
+		onCloseButtonClick: PropTypes.func,
+		onHide: PropTypes.func,
+		open: PropTypes.bool,
+		title: PropTypes.string
+	},
+
+	defaultProps: {
+		noAnimation: false,
+		closeButton: false,
+		open: false
+	},
+
+	styles: {
+		css: componentCss,
+		className: 'popupMenu'
+	},
+
+	render: ({children, closeButton, css, noAnimation, onCloseButtonClick, onHide, open, title, ...rest}) => {
+		return (
+			<Transition
+				noAnimation={noAnimation}
+				visible={open}
+				direction="down"
+				duration="short"
+				type="fade"
+				className={css.popupTransitionContainer}
+				onHide={onHide}
+				css={css}
+			>
+				<Layout orientation="vertical" align="center center" {...rest}>
+					<Cell className={css.title} shrink>
+						<Heading size="title">{title}</Heading>
+					</Cell>
+					<Cell className={css.body} shrink>
+						{children}
+						{closeButton ? <IconButton
+							icon="cancel"
+							backgroundOpacity="lightOpaque"
+							onClick={onCloseButtonClick}
+							className={css.closeButton}
+							size="huge"
+						>cancel</IconButton> : null}
+					</Cell>
+				</Layout>
+			</Transition>
+		);
+	}
+});
+
+/**
+ * Applies Agate specific behaviors to [PopupMenuBase]{@link agate/PopupMenu.PopupMenuBase}.
+ *
+ * @hoc
+ * @memberof agate/PopupMenu
+ * @mixes agate/Skinnable.Skinnable
+ * @public
+ */
+
+const PopupMenuDecorator = compose(
+	Slottable({slots: ['title']}),
+	Skinnable({prop: 'skin'}),
+	PopupState
+);
+
+const PopupMenu = PopupMenuDecorator(PopupMenuBase);
+
+export default PopupMenu;
+export {PopupMenu, PopupMenuBase};
