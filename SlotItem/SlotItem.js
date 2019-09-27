ab1de62364430a007d7a42c42aae04add4d7dd6e
Ryan Duffy <ryan.duffy@lge.com>
2019-09-27 14:13:07 -0700

    PLAT-84631: Consolidate item logic (#138)
    * consolidate item logic
    
    Signed-off-by: Ryan Duffy <ryan.duffy@lge.com>
    
    * put layout in Cell
    
    Signed-off-by: Ryan Duffy <ryan.duffy@lge.com>
    
    * fix labelPosition default and css
    
    Signed-off-by: Ryan Duffy <ryan.duffy@lge.com>
    
    * shim consumers of Item/SlotItem
    
    Signed-off-by: Ryan Duffy <ryan.duffy@lge.com>
    
    * update Item and IconItem styles
    
    * use iconBefore and iconAfter for IconItem
    
    * add icon size to IconItem
    
    * add active and selected styles
    
    * update selected outline variable
    
    * fix IconItem props
    
    Signed-off-by: Ryan Duffy <ryan.duffy@lge.com>
    
    * add deprecation notices
    
    Signed-off-by: Ryan Duffy <ryan.duffy@lge.com>
    
    * update Item variables
    
    * remove duplicate item line heights
    
    * set label padding to 10px
    
    * guard min/max sizing with inline label classes
    
    Signed-off-by: Ryan Duffy <ryan.duffy@lge.com>
    
    * remove extra space
    
    Signed-off-by: Ryan Duffy <ryan.duffy@lge.com>
    
    * fix iconitem classes
    
    Signed-off-by: Ryan Duffy <ryan.duffy@lge.com>
    
    * allow css overrides of item
    
    Signed-off-by: Ryan Duffy <ryan.duffy@lge.com>
    
    * remove LabeledItem cruft
    
    Signed-off-by: Ryan Duffy <ryan.duffy@lge.com>
    
    * fix prop types
    
    Signed-off-by: Ryan Duffy <ryan.duffy@lge.com>
    
    * add inline styles
    
    Signed-off-by: Ryan Duffy <ryan.duffy@lge.com>
    
    * Updated SwitchItem to use the new Item format
    
    Fixed an unintentionally awkwardly large icon for Switch too
    
    Enact-DCO-1.0-Signed-off-by: Blake Stephens <blake.stephens@lge.com>
    
    Signed-off-by: Blake Stephens <blake.stephens@lge.com>

--- SlotItem/SlotItem.js
+++ SlotItem/SlotItem.js
@@ -1,43 +1,20 @@
-import kind from '@enact/core/kind';
-import Spottable from '@enact/spotlight/Spottable';
-import {ItemDecorator as UiItemDecorator} from '@enact/ui/Item';
-import {MarqueeDecorator} from '@enact/ui/Marquee';
-import Pure from '@enact/ui/internal/Pure';
-import UiSlotItem from '@enact/ui/SlotItem';
-import compose from 'ramda/src/compose';
+import deprecate from '@enact/core/internal/deprecate';
 import React from 'react';
 
-import {ItemBase} from '../Item';
-import Skinnable from '../Skinnable';
+import Item from '../Item';
 
-import componentCss from './SlotItem.module.less';
-
-const SlotItemBase = kind({
-	name: 'SlotItem',
-
-	render: (props) => {
-		return (
-			<UiSlotItem
-				{...props}
-				component={ItemBase}
-				css={componentCss}
-			/>
-		);
+const SlotItemBase = deprecate(
+	(props) => (
+		<Item {...props} />
+	),
+	{
+		name: 'SlotItem',
+		replacedBy: 'Item'
 	}
-});
-
-const SlotItemDecorator = compose(
-	Pure,
-	UiItemDecorator,
-	Spottable,
-	MarqueeDecorator({className: componentCss.content, invalidateProps: ['inline', 'autoHide']}),
-	Skinnable
 );
 
-const SlotItem = SlotItemDecorator(SlotItemBase);
-
-export default SlotItem;
+export default SlotItemBase;
 export {
-	SlotItem,
+	SlotItemBase as SlotItem,
 	SlotItemBase
 };
