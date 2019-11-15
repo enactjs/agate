aa97f18a62f540b30e4aa73bc4398ef9cbff9314
Ryan Duffy <ryan.duffy@lge.com>
2019-11-14 19:02:48 -0800

    add state queuing when the window blurs
    Signed-off-by: Ryan Duffy <ryan.duffy@lge.com>

--- data/ProviderDecorator/ProviderDecorator.js
+++ data/ProviderDecorator/ProviderDecorator.js
@@ -1,4 +1,5 @@
 import freeze from 'deep-freeze';
+import {on, off} from '@enact/core/dispatcher';
 import hoc from '@enact/core/hoc';
 import {produce} from 'immer';
 import React, {Component} from 'react';
@@ -16,10 +17,40 @@ const ProviderDecorator = hoc({state: {}}, (config, Wrapped) => {
 		constructor (props) {
 			super(props);
 			this.state = defaultState;
+			this.focused = true;
+			this.queue = [];
+		}
+
+		componentDidMount () {
+			on('blur', this.handleBlur, window);
+			on('focus', this.handleFocus, window);
+		}
+
+		componentWillUnount () {
+			off('blur', this.handleBlur, window);
+			off('focus', this.handleFocus, window);
+		}
+
+		handleBlur = () => {
+			this.focused = false;
+		}
+
+		handleFocus = () => {
+			this.focused = true;
+
+			while (this.queue.length) {
+				this.setState(this.queue.shift());
+			}
 		}
 
 		updateAppState = (cb) => {
-			this.setState(produce(cb));
+			const updater = produce(cb);
+
+			if (this.focused) {
+				this.setState(updater);
+			} else {
+				this.queue.push(updater);
+			}
 		};
 
 		getState () {
