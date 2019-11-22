cec99cddbaa82adc550b614687f2f51f7d961167
RenukaAtale <renuka.atale@lge.com>
2019-11-22 11:31:16 -0800

    PLAT-88683 Iconbutton needs to be reconciled with Button with Icon component (#169)
    * Updated the button component to have changes from Button Icon component
    
    * Update after the PR review comments
    
    * Fixed several issues that came up
    
    * Transparent and translucent buttons had the wrong icon color
    * line-height was never set for button
    * legacy iconbutton variables are now used to support button-with-only-an-icon
    * states properly colored
    * sizes are set appropriately for Gallium
    * misc formatting issues
    
    Enact-DCO-1.0-Signed-off-by: Blake Stephens <blake.stephens@lge.com>
    
    Signed-off-by: Blake Stephens <blake.stephens@lge.com>
    
    * Corrected huge minWidth combination
    
    Enact-DCO-1.0-Signed-off-by: Blake Stephens <blake.stephens@lge.com>
    
    Signed-off-by: Blake Stephens <blake.stephens@lge.com>
    
    * .noChildren can safely be replaced by .minWidth
    
    Enact-DCO-1.0-Signed-off-by: Blake Stephens <blake.stephens@lge.com>
    
    Signed-off-by: Blake Stephens <blake.stephens@lge.com>
    
    * Fix unused declaration
    
    Enact-DCO-1.0-Signed-off-by: Roy Sutton roy.sutton@lge.com
    
    * Update minWidth calculation
    
    Enact-DCO-1.0-Signed-off-by: Roy Sutton roy.sutton@lge.com

--- Button/Button.js
+++ Button/Button.js
@@ -16,6 +16,7 @@ import Spottable from '@enact/spotlight/Spottable';
 import {ButtonBase as UiButtonBase, ButtonDecorator as UiButtonDecorator} from '@enact/ui/Button';
 import Pure from '@enact/ui/internal/Pure';
 import PropTypes from 'prop-types';
+import React from 'react';
 import compose from 'ramda/src/compose';
 
 import {IconBase} from '../Icon';
@@ -93,11 +94,11 @@ const ButtonBase = kind({
 		/**
 		 * The size of the button.
 		 *
-		 * @type {('small'|'large')}
+		 * @type {('smallest'|'small'|'large'|'huge')}
 		 * @default 'large'
 		 * @public
 		 */
-		size: PropTypes.oneOf(['small', 'large']),
+		size: PropTypes.oneOf(['smallest', 'small', 'large', 'huge']),
 
 		/**
 		 * Specify how this button will be used. Is it a standalone button, or is it in a grid of
@@ -121,13 +122,17 @@ const ButtonBase = kind({
 	},
 
 	computed: {
-		className: ({backgroundOpacity, highlighted, joinedPosition, selected, type, styler}) => styler.append(
+		className: ({backgroundOpacity, highlighted, joinedPosition, selected, type, styler, size}) => styler.append(
 			backgroundOpacity,
+			size,
 			type,
 			(joinedPosition && 'joined' + cap(joinedPosition)),  // If `joinedPosition` is present, prepend the word "joined" to the variable, so the classes are clearer.
-			{highlighted, selected}
+			{
+				highlighted,
+				selected
+			}
 		),
-		minWidth: ({children}) => (!children)
+		minWidth: ({children}) => (React.Children.count(children) === 0 || children === '')
 	},
 
 	render: ({css, ...rest}) => {
