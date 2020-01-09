3f4f778f9450531ac0726e803987850c6adad881
Teck <teck.liew@lge.com>
2020-01-09 12:37:12 -0800

    PLAT-96102: Add CheckboxItem (#187)
    * add CheckboxItem
    
    * separate out ToggleIcon and ToggleItem
    
    * cleanup
    
    * add padding and height to CheckboxItem
    
    * define styles for other skins
    
    * use Item for SlotItemBase component
    
    * try using agate/Item
    
    Signed-off-by: Ryan Duffy <ryan.duffy@lge.com>
    
    * use Item
    
    * use clean up Switch and use internal/ToggleIcon
    
    * move Checkbox to its own component
    
    * clean up
    
    * format css rules into one block for Checkbox
    
    * reorg Checkbox variables
    
    Signed-off-by: Ryan Duffy <ryan.duffy@lge.com>
    
    * remove CheckboxItem height
    
    Co-authored-by: Ryan Duffy <ryan@tiqtech.com>
    
    Reviewed-By: Jeremy Thomas (jeremy.thomas@lge.com)
    Integrated-By: Ryan Duffy (ryan.duffy@lge.com)

---
+++ Checkbox/Checkbox.js
@@ -0,0 +1,74 @@
+/**
+ * Agate styled checkbox component.
+ *
+ * @example
+ * <Checkbox onToggle={console.log} />
+ *
+ * @module agate/Checkbox
+ * @exports Checkbox
+ * @exports CheckboxBase
+ */
+
+import kind from '@enact/core/kind';
+import PropTypes from 'prop-types';
+import React from 'react';
+
+import Icon from '../Icon/Icon';
+import ToggleIcon from '../internal/ToggleIcon/ToggleIcon';
+
+import css from './Checkbox.module.less';
+
+/**
+ * A checkbox component, ready to use in Agate applications.
+ *
+ * `Checkbox` may be used independently to represent a toggleable state but is more commonly used as
+ * part of [CheckboxItem]{@link agate/CheckboxItem}.
+ *
+ * Usage:
+ * ```
+ * <Checkbox selected />
+ * ```
+ *
+ * @class Checkbox
+ * @memberof agate/Checkbox
+ * @extends agate/internal/ToggleIcon.ToggleIcon
+ * @ui
+ * @private
+ */
+const CheckboxBase = kind({
+	name: 'Checkbox',
+
+	propTypes: /** @lends agate/Checkbox.Checkbox.prototype */ {
+		/**
+		 * The icon displayed when `selected`.
+		 *
+		 * @see {@link agate/Icon.Icon.children}
+		 * @type {String|Object}
+		 * @default	'check'
+		 * @public
+		 */
+		children: PropTypes.string
+	},
+
+	defaultProps: {
+		children: 'check'
+	},
+
+	render: ({children, ...rest}) => {
+		return (
+			<ToggleIcon
+				{...rest}
+				css={css}
+				iconComponent={Icon}
+			>
+				{children}
+			</ToggleIcon>
+		);
+	}
+});
+
+export default CheckboxBase;
+export {
+	CheckboxBase as Checkbox,
+	CheckboxBase
+};
---
+++ internal/ToggleIcon/ToggleIcon.js
@@ -0,0 +1,72 @@
+/**
+ * Agate styled toggle icon.
+ *
+ * @example
+ * <ToggleIcon selected>check</ToggleIcon>
+ *
+ * @module agate/ToggleIcon
+ * @exports ToggleIcon
+ * @exports ToggleIconBase
+ * @exports ToggleIconDecorator
+ */
+
+import kind from '@enact/core/kind';
+import React from 'react';
+import compose from 'ramda/src/compose';
+
+import Pure from '@enact/ui/internal/Pure';
+import UiToggleIcon from '@enact/ui/ToggleIcon';
+
+import Icon from '../../Icon/Icon';
+import Skinnable from '../../Skinnable/Skinnable';
+
+/**
+ * A component that indicates a boolean state.
+ *
+ * @class ToggleIconBase
+ * @memberof agate/ToggleIcon
+ * @extends ui/ToggleIcon.ToggleIcon
+ * @ui
+ * @private
+ */
+const ToggleIconBase = kind({
+	name: 'ToggleIcon',
+
+	render: (props) => {
+		return (
+			<UiToggleIcon {...props} iconComponent={Icon} />
+		);
+	}
+});
+
+/**
+ * Agate-specific behaviors to apply to `ToggleIconBase`.
+ *
+ * @hoc
+ * @memberof agate/ToggleIcon
+ * @mixes agate/Skinnable.Skinnable
+ * @private
+ */
+const ToggleIconDecorator = compose(
+	Pure,
+	Skinnable
+);
+
+/**
+ * A customizable Agate starting point [Icon]{@link agate/Icon.Icon} that responds to the
+ * `selected` prop.
+ *
+ * @class ToggleIcon
+ * @memberof agate/ToggleIcon
+ * @extends agate/ToggleIcon.ToggleIconBase
+ * @mixes agate/ToggleIcon.ToggleIconDecorator
+ * @private
+ */
+const ToggleIcon = ToggleIconDecorator(ToggleIconBase);
+
+export default ToggleIcon;
+export {
+	ToggleIcon,
+	ToggleIconBase,
+	ToggleIconDecorator
+};
