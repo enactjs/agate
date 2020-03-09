86b19a704cf09a0059ef7b240751080cb8ef4729
YunBum SUNG <yb.sung@lge.com>
2020-03-09 15:33:20 +0900

    PLAT-101582: Replace querySelector with refs in VirtualList and Scroller (#220)
    * Replace querySelector with refs in VirtualList and Scroller
    
    Enact-DCO-1.0-Signed-off-by: YB Sung (yb.sung@lge.com)
    
    * Use Enact PLAT-101582 Branch for travis
    
    Enact-DCO-1.0-Signed-off-by: YB Sung (yb.sung@lge.com)
    
    * Fix numOfItems
    
    Enact-DCO-1.0-Signed-off-by: YB Sung (yb.sung@lge.com)
    
    * Fix lint errors
    
    Enact-DCO-1.0-Signed-off-by: YB Sung (yb.sung@lge.com)
    
    * Remove itemContainerRef
    
    Enact-DCO-1.0-Signed-off-by: YB Sung (yb.sung@lge.com)
    
    * Revert "Use Enact PLAT-101582 Branch for travis"
    
    This reverts commit 63e99225ae9c3046fc809a1dafed7e0f1e984fbb.

--- useScroll/useScroll.js
+++ useScroll/useScroll.js
@@ -281,6 +281,7 @@ const useScroll = (props) => {
 
 	const scrollContainerRef = useRef();
 	const scrollContentRef = useRef();
+	const itemRefs = useRef([]);
 
 	const horizontalScrollbarRef = useRef();
 	const verticalScrollbarRef = useRef();
@@ -413,7 +414,9 @@ const useScroll = (props) => {
 	});
 
 	assignProperties('scrollContentProps', {
+		...(props.itemRenderer ? {itemRefs} : {}),
 		onUpdate: handleScrollerUpdate,
+		scrollContainerRef,
 		scrollAndFocusScrollbarButton,
 		setThemeScrollContentHandle,
 		spotlightId,
