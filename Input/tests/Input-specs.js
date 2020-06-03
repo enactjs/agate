20123c5aff99b825eb440a716a38f64683b79cdd
daniel-stoian-lgp <63335068+daniel-stoian-lgp@users.noreply.github.com>
2020-06-03 03:45:55 +0300

    FORTECH-26-27-28-30: Added silicon skin styles for Button, Input, Radioitem, Scroller. (#253)
    * Added styling for Agate Button for gallium and silicon skins
    
    * [Agate] Added styles for silicon skin for Button
    
    :Release Notes:
    Added styles for silicon skin for Button
    
    :Detailed Notes:
    Added styles for silicon skin for Button
    
    :Testing Performed:
    
    :QA Notes:
    
    :Issues Addressed:
    [FORTECH-26] Settings view: integrate Button from Agate
    
    * [Agate] Added styles for silicon skin for Button
    
    :Release Notes:
    Added styles for silicon skin for Button
    
    :Detailed Notes:
    Added styles for silicon skin for Button
    
    :Testing Performed:
    
    :QA Notes:
    
    :Issues Addressed:
    [FORTECH-26] Settings view: integrate Button from Agate
    
    * [Agate] Added styles for silicon skin for Button
    
    :Release Notes:
    Added styles for silicon skin for Button
    
    :Detailed Notes:
    Added styles for silicon skin for Button
    
    :Testing Performed:
    
    :QA Notes:
    
    :Issues Addressed:
    [FORTECH-26] Settings view: integrate Button from Agate
    
    * [Agate] Added Silicon styles for Button
    
    :Release Notes:
    [Agate] Added Silicon styles for Button
    
    :Detailed Notes:
    [Agate] Added Silicon styles for Button
    
    :Testing Performed:
    
    :QA Notes:
    
    :Issues Addressed:
    [FORTECH-26] [Agate] Added Silicon styles for Button
    
    * [Agate] Added Silicon styles for Button
    
    :Release Notes:
    [Agate] Added Silicon styles for Button
    
    :Detailed Notes:
    [Agate] Added Silicon styles for Button
    
    :Testing Performed:
    
    :QA Notes:
    
    :Issues Addressed:
    [FORTECH-26]- [FORTECH-31] [Agate] Added Silicon styles for Button,Scroller,RadioItem, Input, BodyText
    
    * [Agate] Added Silicon styles for Button, Scroller,RadioItem, Input, BodyText
    
    :Release Notes:
    
    :Detailed Notes:
    [Agate] Added Silicon styles for Button, Scroller,RadioItem, Input, BodyText
    
    :Testing Performed:
    
    :QA Notes:
    
    :Issues Addressed:
    [FORTECH-26]- [FORTECH-31] [Agate] Added Silicon styles for Button,Scroller,RadioItem, Input, BodyText
    
    * [Agate] Added Silicon styles for Button, Scroller,RadioItem, Input, BodyText
    
    :Detailed Notes:
    [Agate] Added Silicon styles for Button, Scroller,RadioItem, Input, BodyText
    
    :Issues Addressed:
    [FORTECH-26] ->[FORTECH-31] [Agate] Add Agate styles for Button,Scroller,RadioItem, Input, BodyText
    
    * Added tests for Button, Input, RadioItem and Scroller
    Removed style to non-existent variable for Button
    
    * Removed unnecessary color definition in colors-silicon-night.less
    Removed min-wdth for Input and Button
    Removed LGSmartUI font-family, based on latest information
    
    * Fixed issues reported on code review
    
    * Updated styles for Input based on code-review
    Fixed Button focus background-color
    
    * Fixed missing empty line
    
    * Fixed input focus styles for silicon
    Fixed input-focused-text-color for gallium skin
    
    * Added fixes based on Blake's comments
    
    * Added fixes based on Blake's comments
    
    * Added active mixin.

--- Input/tests/Input-specs.js
+++ Input/tests/Input-specs.js
@@ -1,11 +1,53 @@
-/*
 import React from 'react';
 import {mount} from 'enzyme';
-import sinon from 'sinon';
 import Input from '../Input';
-*/
 
-describe.skip('Input', () => {
-	// TODO: write actual agate/Input tests
-	test('should not make Travis fail', function () {});
+describe('Input Specs', () => {
+	test('should have an input element', () => {
+		const subject = mount(
+			<Input />
+		);
+		expect(subject.find('input')).toHaveLength(1);
+	});
+
+	test('should include a placeholder if specified', () => {
+		const subject = mount(
+			<Input placeholder="hello" />
+		);
+
+		expect(subject.find('input').prop('placeholder')).toBe('hello');
+	});
+
+	test('should callback onChange when the text changes', () => {
+		const handleChange = jest.fn();
+		const value = 'blah';
+		const evt = {target: {value: value}};
+		const subject = mount(
+			<Input onChange={handleChange} />
+		);
+
+		subject.find('input').simulate('change', evt);
+
+		const expected = value;
+		const actual = handleChange.mock.calls[0][0].value;
+
+		expect(actual).toBe(expected);
+	});
+
+	test('should be able to be disabled', () => {
+		const subject = mount(
+			<Input disabled />
+		);
+
+		expect(subject.find('input').prop('disabled')).toBe(true);
+	});
+
+	test('should reflect the value if specified', () => {
+		const subject = mount(
+			<Input value="hello" />
+		);
+
+		expect(subject.find('input').prop('value')).toBe('hello');
+	});
+
 });
