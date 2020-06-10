cc9c0f61a98901c6bb480e8b2c808abf57fba1a6
Adrian Cocoara <adrian.cocoara@lgepartner.com>
2020-06-10 13:32:05 +0300

    Removed one empty line

--- samples/sampler/stories/default/ImageItemKitchenSink.js
+++ samples/sampler/stories/default/ImageItemKitchenSink.js
@@ -3,7 +3,6 @@ import React from 'react';
 import {storiesOf} from '@storybook/react';
 
 import Heading from '@enact/agate/Heading';
-
 import ImageItem from '@enact/agate/ImageItem';
 
 ImageItem.displayName = 'ImageItem';
