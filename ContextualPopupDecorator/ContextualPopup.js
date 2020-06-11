83936dc533cff69d58defcbf7692057d93455c52
Stanca Pop <stanca.pop@lgepartner.com>
2020-06-11 17:49:07 +0300

    Added ContextualPopupDecorator component - work in progress

---
+++ ContextualPopupDecorator/ContextualPopup.js
@@ -0,0 +1,197 @@
+import kind from '@enact/core/kind';
+import PropTypes from 'prop-types';
+import React from 'react';
+
+import $L from '../internal/$L';
+import Button from '../Button';
+import Skinnable from '../Skinnable';
+
+import css from './ContextualPopup.module.less';
+
+/**
+ * An SVG arrow for {@link agate/ContextualPopupDecorator/ContextualPopup.ContextualPopup}.
+ *
+ * @class ContextualPopupArrow
+ * @memberof agate/ContextualPopupDecorator
+ * @ui
+ * @private
+ */
+const ContextualPopupArrow = kind({
+	name: 'ContextualPopupArrow',
+
+	propTypes: /** @lends agate/ContextualPopupDecorator.ContextualPopupArrow.prototype */ {
+		direction: PropTypes.oneOf(['up', 'down', 'left', 'right'])
+	},
+
+	defaultProps: {
+		direction: 'down'
+	},
+
+	styles: {
+		css,
+		className: 'arrow'
+	},
+
+	computed: {
+		className: ({direction, styler}) => styler.append(direction, css.arrow)
+	},
+
+	render: (props) => (
+		<svg {...props} viewBox="0 0 30 30">
+			<path d="M0 18 L15 0 L30 18" className={css.arrowBorder} />
+			<path d="M15 2 L0 20 L30 20 Z" className={css.arrowFill} />
+		</svg>
+	)
+});
+
+const ContextualPopupRoot = Skinnable(
+	'div'
+);
+
+/**
+ * A popup component used by
+ * [ContextualPopupDecorator]{@link agate/ContextualPopupDecorator.ContextualPopupDecorator} to
+ * wrap its [popupComponent]{@link agate/ContextualPopupDecorator.popupComponent}.
+ *
+ * `ContextualPopup` is usually not used directly but is made available for unique application use
+ * cases.
+ *
+ * @class ContextualPopup
+ * @memberof agate/ContextualPopupDecorator
+ * @ui
+ * @public
+ */
+const ContextualPopupBase = kind({
+	name: 'ContextualPopup',
+
+	propTypes: /** @lends agate/ContextualPopupDecorator.ContextualPopup.prototype */ {
+		/**
+		 * The contents of the popup.
+		 *
+		 * @type {Node}
+		 * @required
+		 * @public
+		 */
+		children: PropTypes.node.isRequired,
+
+		/**
+		 * Style object for arrow position.
+		 *
+		 * @type {Object}
+		 * @public
+		 */
+		arrowPosition: PropTypes.shape({
+			bottom: PropTypes.number,
+			left: PropTypes.number,
+			right: PropTypes.number,
+			top: PropTypes.number
+		}),
+
+		/**
+		 * Style object for container position.
+		 *
+		 * @type {Object}
+		 * @public
+		 */
+		containerPosition: PropTypes.shape({
+			bottom: PropTypes.number,
+			left: PropTypes.number,
+			right: PropTypes.number,
+			top: PropTypes.number
+		}),
+
+		/**
+		 * Called with the reference to the container node.
+		 *
+		 * @type {Function}
+		 * @public
+		 */
+		containerRef: PropTypes.func,
+
+		/**
+		 * Direction of ContextualPopup.
+		 *
+		 * Can be one of: `'up'`, `'down'`, `'left'`, or `'right'`.
+		 *
+		 * @type {('up'|'down'|'left'|'right')}
+		 * @default 'down'
+		 * @public
+		 */
+		direction: PropTypes.oneOf(['up', 'down', 'left', 'right']),
+
+		/**
+		 * Called when the close button is clicked.
+		 *
+		 * @type {Function}
+		 * @public
+		 */
+		onCloseButtonClick: PropTypes.func,
+
+		/**
+		 * Shows the arrow.
+		 *
+		 * @type {Boolean}
+		 * @default false
+		 * @public
+		 */
+		showArrow: PropTypes.bool,
+
+		/**
+		 * Shows the close button.
+		 *
+		 * @type {Boolean}
+		 * @default false
+		 * @public
+		 */
+		showCloseButton: PropTypes.bool
+	},
+
+	defaultProps: {
+		direction: 'down',
+		showCloseButton: false
+	},
+
+	styles: {
+		css,
+		className: 'container'
+	},
+
+	computed: {
+		className: ({showCloseButton, styler}) => styler.append({reserveClose: showCloseButton}),
+		closeButton: ({showCloseButton, onCloseButtonClick}) => {
+			if (showCloseButton) {
+				return (
+					<Button
+						aria-label={$L('Close')}
+						backgroundOpacity="transparent"
+						className={css.closeButton}
+						icon="closex"
+						onTap={onCloseButtonClick}
+						size="small"
+					/>
+				);
+			}
+		}
+	},
+
+	render: ({arrowPosition, containerPosition, containerRef, children, className, closeButton, direction, showArrow, ...rest}) => {
+		delete rest.onCloseButtonClick;
+		delete rest.showCloseButton;
+
+		return (
+			<ContextualPopupRoot aria-live="off" role="alert" {...rest} className={css.contextualPopup}>
+				<div className={className} style={containerPosition} ref={containerRef}>
+					{children}
+					{closeButton}
+				</div>
+				{showArrow ? <ContextualPopupArrow direction={direction} style={arrowPosition} /> : null}
+			</ContextualPopupRoot>
+		);
+	}
+});
+
+export default ContextualPopupBase;
+export {
+	ContextualPopupBase as ContextualPopup,
+	ContextualPopupBase
+};
---
+++ samples/sampler/stories/default/ContextualPopupDecorator.js
@@ -0,0 +1,55 @@
+import {ContextualPopupDecorator} from '@enact/agate/ContextualPopupDecorator';
+import BodyText from '../../../../../moonstone/BodyText';
+import Button from '@enact/agate/Button';
+import ri from '@enact/ui/resolution';
+import React from 'react';
+import {storiesOf} from '@storybook/react';
+
+import {boolean, select, text} from '@enact/storybook-utils/addons/knobs';
+import {mergeComponentMetadata} from '@enact/storybook-utils';
+import {action} from '@enact/storybook-utils/addons/actions';
+
+
+const ContextualButton = ContextualPopupDecorator(Button);
+ContextualButton.displayName = 'ContextualButton';
+
+const Config = mergeComponentMetadata('ContextualPopupDecorator', Button, ContextualButton);
+
+// NOTE: Something about the HOC is inhibiting accessing its defaultProps, so we're adding them here
+// manually. This can (should) be revisited later to find out why and a solution.
+Config.defaultProps = {
+	direction: 'down',
+	open: true,
+	showCloseButton: true,
+	spotlightRestrict: 'self-first'
+};
+
+const renderPopup = () => (
+	<div>{text('popup string', {groupId: 'Popup'}, 'Hello Contextual Popup')}</div>
+);
+
+storiesOf('Agate', module)
+	.add(
+		'ContextualPopupDecorator',
+		() => (
+			<div style={{textAlign: 'center', marginTop: ri.unit(99, 'rem')}}>
+				<ContextualButton
+					direction={select('direction', ['up', 'down', 'left', 'right'], Config)}
+					noAutoDismiss={boolean('noAutoDismiss', Config)}
+					onClose={action('onClose')}
+					open={boolean('open', Config)}
+					popupComponent={renderPopup}
+					showCloseButton={boolean('showCloseButton', Config)}
+					spotlightRestrict={select('spotlightRestrict', ['none', 'self-first', 'self-only'], Config)}
+				>
+					{text('button string', Config, 'Hello Contextual Button')}
+				</ContextualButton>
+				<BodyText centered>Use KNOBS to interact with the ContextualPopup.</BodyText>
+			</div>
+		),
+		{
+			info: {
+				text: 'Basic usage of ContextualPopupDecorator'
+			}
+		}
+	);
