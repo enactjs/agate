30c094bad154edcae003ad7934c7251861304ebc
adrian-cocoara-lgp <63335823+adrian-cocoara-lgp@users.noreply.github.com>
2020-06-24 08:10:23 +0300

    FORTECH-37 Add ImageItem (#267)
    * Added agate/ImageItem
    Added styles for gallium and silicon
    Added tests
    Added to storybook
    
    * Small changes to fix lint warnings
    
    * Removed duplicate styling code that comes from ui/ImageItem
    
    * Changed '@mixes moonstone/Marquee' with '@mixes ui/Marquee'
    
    * Code changes based on code review
    
    * Removed one empty line
    
    * Added description for ImageItem
    
    * Fixed ImageItem's hover issue
    
    * Fixed ImageItem's hover issue
    
    * Code changes based on code review

---
+++ samples/sampler/stories/default/ImageItemKitchenSink.js
@@ -0,0 +1,19 @@
+import ri from '@enact/ui/resolution';
+import React from 'react';
+import {storiesOf} from '@storybook/react';
+
+import Heading from '@enact/agate/Heading';
+import ImageItem from '@enact/agate/ImageItem';
+
+ImageItem.displayName = 'ImageItem';
+
+storiesOf('Agate QA.ImageItem', module)
+	.add(
+		'Kitchen Sink',
+		() => (
+			<div style={{width: ri.scaleToRem(400), height: ri.scaleToRem(300)}}>
+				<Heading showline>ImageItem</Heading>
+				<ImageItem src="http://placehold.it/300x400/9037ab/ffffff&text=Image0" />
+			</div>
+		)
+	);
