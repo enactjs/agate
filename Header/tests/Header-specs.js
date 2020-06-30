5908b925ea75c508e95da66a90f34647d69f1dd5
Adrian Cocoara <adrian.cocoara@lgepartner.com>
2020-06-30 09:50:24 +0300

    Updated silicon styles for Header Added unit tests

---
+++ Header/tests/Header-specs.js
@@ -0,0 +1,44 @@
+import {mount} from 'enzyme';
+import React from 'react';
+
+import {Header} from '../Header';
+
+describe('Header', () => {
+	test('should render a title when `title` prop is set', () => {
+		const subject = mount(
+			<Header title="title"/>
+		);
+
+		const expected = 'title';
+		const actual = subject.prop('title');
+
+		expect(actual).toBe(expected);
+	});
+
+	test('should render a subtitle when `subtitle` prop is set', () => {
+		const subject = mount(
+			<Header
+				title="title"
+				subtitle="subtitle"/>
+		);
+
+		const expected = 'subtitle';
+		const actual = subject.prop('subtitle');
+
+		expect(actual).toBe(expected);
+	});
+
+	test('should render a title above when `titleAbove` prop is set', () => {
+		const subject = mount(
+			<Header
+				title="title"
+				titleAbove="title above"
+			/>
+		);
+
+		const expected = 'title above';
+		const actual = subject.prop('titleAbove');
+
+		expect(actual).toBe(expected);
+	});
+});
