c98375cb1aed3cb90f14f3b174dcffe02646acec
Stanca Pop <stanca.pop@lgepartner.com>
2020-07-17 11:56:37 +0300

    Fixed lint errors

--- samples/sampler/stories/default/TooltipDecorator.js
+++ samples/sampler/stories/default/TooltipDecorator.js
@@ -1,6 +1,6 @@
 import {mergeComponentMetadata} from '@enact/storybook-utils';
 import {action} from '@enact/storybook-utils/addons/actions';
-import {boolean, select, text, number} from '@enact/storybook-utils/addons/knobs';
+import {boolean, select, text} from '@enact/storybook-utils/addons/knobs';
 import {storiesOf} from '@storybook/react';
 import Button, {ButtonBase} from '@enact/agate/Button';
 import UiButton from '@enact/ui/Button';
@@ -8,11 +8,6 @@ import React from 'react';
 
 Button.displayName = 'TooltipDecorator';
 const Config = mergeComponentMetadata('TooltipDecorator', UiButton, ButtonBase, Button);
-
-// Set up some defaults for info and knobs
-const prop = {
-	casing: ['preserve', 'sentence', 'word', 'upper'],
-};
 
 storiesOf('Agate', module)
 	.add(
