edec89e139d453e4410fe3b47c7df1518941441e
Stanca Pop <stanca.pop@lgepartner.com>
2020-07-17 15:14:53 +0300

    fixes according to review

--- samples/sampler/stories/default/TooltipDecorator.js
+++ samples/sampler/stories/default/TooltipDecorator.js
@@ -3,17 +3,19 @@ import {action} from '@enact/storybook-utils/addons/actions';
 import {boolean, select, text} from '@enact/storybook-utils/addons/knobs';
 import {storiesOf} from '@storybook/react';
 import Button, {ButtonBase} from '@enact/agate/Button';
+import TooltipDecorator from '@enact/agate/TooltipDecorator';
 import UiButton from '@enact/ui/Button';
 import React from 'react';
 
 Button.displayName = 'TooltipDecorator';
 const Config = mergeComponentMetadata('TooltipDecorator', UiButton, ButtonBase, Button);
+const TooltipButton = TooltipDecorator({tooltipDestinationProp: 'decoration'}, Button);
 
 storiesOf('Agate', module)
 	.add(
 		'TooltipDecorator',
 		() => (
-			<Button
+			<TooltipButton
 				backgroundOpacity={select('backgroundOpacity', ['opaque', 'lightOpaque', 'transparent'], Config)}
 				disabled={boolean('disabled', Config)}
 				onClick={action('onClick')}
@@ -22,7 +24,7 @@ storiesOf('Agate', module)
 				tooltipText={text('tooltip', Config, 'Tooltip')}
 			>
 				{text('children', Button, 'Click me')}
-			</Button>
+			</TooltipButton>
 		),
 		{
 			text: 'The basic TooltipDecorator'
