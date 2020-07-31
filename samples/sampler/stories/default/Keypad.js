d1b3b0726509e5fa884486d2aaf8974c45282c06
Stanca Pop <stanca.pop@lgepartner.com>
2020-07-31 09:42:30 +0300

    fixes after code review

--- samples/sampler/stories/default/Keypad.js
+++ samples/sampler/stories/default/Keypad.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import {storiesOf} from '@storybook/react';
 
-import Keypad from '../../../../Keypad';
+import Keypad from '@enact/agate/Keypad';
 
 storiesOf('Agate', module)
 	.add(
