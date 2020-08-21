95a3f5c5fc1a51b56897a248e49afaeeaf1913c9
Daniel Stoian <daniel.stoian@lgepartner.com>
2020-08-21 10:26:15 +0300

    mediaplayer audio playback

--- MediaPlayer/MediaPlayer.js
+++ MediaPlayer/MediaPlayer.js
@@ -1,5 +1,6 @@
 import kind from '@enact/core/kind';
 import EnactPropTypes from '@enact/core/internal/prop-types';
+import {adaptEvent, call, forKey, forward, forwardWithPrevent, handle, preventDefault, stopImmediate, returnsTrue} from '@enact/core/handle';
 import Pure from '@enact/ui/internal/Pure';
 import Media from '@enact/ui/Media';
 import Slottable from '@enact/ui/Slottable';
@@ -12,6 +13,15 @@ import MediaControls from './MediaControls';
 import MediaSlider from './MediaSlider';
 import Skinnable from '../Skinnable';
 import Times from './Times';
+import $L from '../internal/$L';
+import {Job} from '@enact/core/util';
+import Announce from '@enact/ui/AnnounceDecorator/Announce';
+
+const forwardWithState = (type) => adaptEvent(call('addStateToEvent'), forwardWithPrevent(type));
+
+// provide forwarding of events on media controls
+const forwardPlay = forwardWithState('onPlay');
+const forwardPause = forwardWithState('onPause');
 
 /**
  * Provides Agate-themed media player components.
@@ -20,10 +30,10 @@ import Times from './Times';
  * @exports MediaPlayer
  */
 
-const MediaPlayerBase = kind({
-	name: 'MediaPlayer',
+const MediaPlayerBase = class extends React.Component {
+	static displayName = 'MediaPlayer'
 
-	propTypes: /** @lends agate/MediaPlayer.MediaPlayerBase.prototype */ {
+	static propTypes =  /** @lends agate/MediaPlayer.MediaPlayerBase.prototype */ {
 		/**
 		 * Any children `<source>` tag elements will be sent directly to the media element as
 		 * sources.
@@ -46,29 +56,184 @@ const MediaPlayerBase = kind({
 		 * @default 'video'
 		 * @public
 		 */
-		mediaComponent: EnactPropTypes.renderable
-	},
+		mediaComponent: EnactPropTypes.renderable,
 
-	defaultProps: {
+		/**
+		 * Called when video is paused
+		 *
+		 * @type {Function}
+		 * @public
+		 */
+		onPause: PropTypes.func,
+
+		/**
+		 * Called when video is played
+		 *
+		 * @type {Function}
+		 * @public
+		 */
+		onPlay: PropTypes.func,
+	}
+
+	static defaultProps = {
 		mediaComponent: 'audio'
-	},
+	}
 
-	styles: {
-		css,
-		className: 'mediaPlayer'
-	},
+	constructor (props) {
+		super(props);
 
-	render: ({mediaComponent, source, ...rest}) => {
+		// Internal State
+		this.audio = null;
+
+		this.state = {
+			paused: true,
+		};
+	}
+
+	handle = handle.bind(this)
+
+	handlePlay = this.handle(
+		forwardPlay,
+		() => this.play()
+	)
+
+	handlePause = this.handle(
+		forwardPause,
+		() => this.pause()
+	)
+
+	announceJob = new Job(msg => (this.announceRef && this.announceRef.announce(msg)), 200)
+
+	announce = (msg) => {
+		this.announceJob.start(msg);
+	}
+
+	//
+	// Handled Media events
+	//
+	addStateToEvent = (ev) => {
+		return {
+			// More props from `ev` may be added here as needed, but a full copy via `...ev`
+			// overloads Storybook's Action Logger and likely has other perf fallout.
+			type: ev.type,
+			// Specific state variables are included in the outgoing calback payload, not all of them
+			...this.getMediaState()
+		};
+	}
+
+	/**
+	 * Returns an object with the current state of the media including `currentTime`, `duration`,
+	 * `paused`, `playbackRate`, `proportionLoaded`, and `proportionPlayed`.
+	 *
+	 * @function
+	 * @memberof sandstone/VideoPlayer.VideoPlayerBase.prototype
+	 * @returns {Object}
+	 * @public
+	 */
+	getMediaState = () => {
+		return {
+			//currentTime       : this.state.currentTime,
+			//duration          : this.state.duration,
+			paused            : this.state.paused,
+			//playbackRate      : this.video.playbackRate,
+			//proportionLoaded  : this.state.proportionLoaded,
+			//proportionPlayed  : this.state.proportionPlayed
+		};
+	}
+
+	/**
+	 * The primary means of interacting with the `<video>` element.
+	 *
+	 * @param  {String} action The method to preform.
+	 * @param  {Multiple} props  The arguments, in the format that the action method requires.
+	 *
+	 * @private
+	 */
+	send = (action, props) => {
+		//this.clearPulsedPlayback();
+		//this.showFeedback();
+		//this.startDelayedFeedbackHide();
+		this.audio[action](props);
+	}
+
+	handleEvent = () => {
+		const el = this.audio;
+		const updatedState = {
+			paused: el.paused,
+		};
+
+		// If there's an error, we're obviously not loading, no matter what the readyState is.
+		if (updatedState.error) updatedState.loading = false;
+
+		this.setState(updatedState);
+	}
+
+
+	play = () => {
+		// must happen before send() to ensure feedback uses the right value
+		// TODO: refactor into this.state member
+		this.send('play');
+		this.announce($L('Play'));
+	}
+
+
+	pause = () => {
+		// must happen before send() to ensure feedback uses the right value
+		// TODO: refactor into this.state member
+		this.send('pause');
+		this.announce($L('Pause'));
+	}
+
+	setAudioRef = (node) => {
+		this.audio = node;
+		this.setMedia();
+	}
+
+	setAnnounceRef = (node) => {
+		this.announceRef = node;
+	}
+
+	setMedia ({setMedia} = this.props) {
+		if (setMedia) {
+			setMedia(this.audio);
+		}
+	}
+
+	render ()  {
+		const {
+			mediaComponent,
+			source,
+			...rest
+		} = this.props;
+
+		rest.css = css;
+		rest.className = css.mediaPlayer;
+
 		return (
 			<div className={css.mediaPlayer} {...rest}>
+				<Media mediaComponent={mediaComponent} source={source} autoPlay controls {...rest} ref={this.setAudioRef} onUpdate={this.handleEvent} />
 				<MediaSlider />
 				<Times />
-				<MediaControls />
-				<Media mediaComponent={mediaComponent} source={source} controls autoPlay />
+				<MediaControls
+					//onClose={this.handleMediaControlsClose}
+					//onFastForward={this.handleFastForward}
+					//onJump={this.handleJump}
+					//onJumpBackwardButtonClick={this.onJumpBackward}
+					//onJumpForwardButtonClick={this.onJumpForward}
+					onPause={this.handlePause}
+					onPlay={this.handlePlay}
+					//onRewind={this.handleRewind}
+					//onToggleMore={this.handleToggleMore}
+					paused={this.state.paused}
+					//spotlightId={this.mediaControlsSpotlightId}
+					//spotlightDisabled={!this.state.mediaControlsVisible || spotlightDisabled}
+					visible={this.state.mediaControlsVisible}
+				/>
+				<Announce ref={this.setAnnounceRef} />
 			</div>
 		);
 	}
-});
+};
 
 /**
  * A higher-order component that adds Agate specific behaviors to `MediaPlayer`.
