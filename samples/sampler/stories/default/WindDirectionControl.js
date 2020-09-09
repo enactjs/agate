d2edf8bd02b2bab3cad995d48db57704d9d9d785
Adrian Cocoara <adrian.cocoara@lgepartner.com>
2020-09-09 15:26:53 +0300

    implemented WindDirectionControl

---
+++ samples/sampler/stories/default/WindDirectionControl.js
@@ -0,0 +1,26 @@
+import {mergeComponentMetadata} from '@enact/storybook-utils';
+import {select} from '@enact/storybook-utils/addons/knobs';
+import React from 'react';
+import {storiesOf} from '@storybook/react';
+import WindDirectionControl from '@enact/agate/WindDirectionControl';
+
+WindDirectionControl.displayName = 'WindDirectionControl';
+const Config = mergeComponentMetadata('WindDirectionControl', WindDirectionControl);
+
+storiesOf('Agate', module)
+	.add(
+		'WindDirectionControl',
+		() => (
+			<div style={{marginTop: "40px"}}>
+				<WindDirectionControl
+					airDirection={select('airDirection', ['', 'airDown', 'airRight', 'airUp'], Config, '')}
+				/>
+			</div>
+		),
+		{
+			text: 'The basic WindDirectionControl'
+		}
+	);
+
+
+
