0d7577209ee163432acaa5eca81cbcb4bc953e41
Stanca Pop <stanca.pop@lgepartner.com>
2020-10-19 14:21:16 +0300

    fixed lint warnings

--- tests/screenshot/apps/components/RangePicker.js
+++ tests/screenshot/apps/components/RangePicker.js
@@ -3,8 +3,8 @@ import React from 'react';
 
 const RangePickerTests = [
 	<RangePicker />,
-	<RangePicker orientation="horizontal"/>,
-	<RangePicker min={0} max={20} step={5}/>
+	<RangePicker orientation="horizontal" />,
+	<RangePicker min={0} max={20} step={5} />
 ];
 
 export default RangePickerTests;
--- tests/screenshot/apps/components/Slider.js
+++ tests/screenshot/apps/components/Slider.js
@@ -4,7 +4,7 @@ import React from 'react';
 const SliderTests = [
 	<Slider />,
 	<Slider active />,
-	<Slider min={0} max={20} progressAnchor={0.4}/>
+	<Slider min={0} max={20} progressAnchor={0.4} />
 ];
 
 export default SliderTests;
