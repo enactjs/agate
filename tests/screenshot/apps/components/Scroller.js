61ed1f01a3203d4de5f6a841b69b37f830f531fc
Daniel Stoian <daniel.stoian@lgepartner.com>
2020-10-19 16:24:20 +0300

    Added screenshot tests

--- tests/screenshot/apps/components/Scroller.js
+++ tests/screenshot/apps/components/Scroller.js
@@ -3,8 +3,8 @@ import React from 'react';
 
 const ScrollerTests = [
 	<Scroller>
-			Lorem ipsum dolor sit amet, consectetur adipiscing elit.<br />
-			Aenean id blandit nunc. Donec lacinia nisi vitae mi dictum, eget pulvinar nunc tincidunt. Integer vehicula tempus rutrum. Sed efficitur neque in arcu dignissim cursus.
+		Lorem ipsum dolor sit amet, consectetur adipiscing elit.<br />
+		Aenean id blandit nunc. Donec lacinia nisi vitae mi dictum, eget pulvinar nunc tincidunt. Integer vehicula tempus rutrum. Sed efficitur neque in arcu dignissim cursus.
 	</Scroller>,
 	<Scroller verticalScrollbar="visible">
 		Lorem ipsum dolor sit amet, consectetur adipiscing elit.<br />
