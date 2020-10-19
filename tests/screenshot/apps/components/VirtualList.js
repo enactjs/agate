ef9bce98f950a88b5ee46307e8d22d050ef1ba33
Daniel Stoian <daniel.stoian@lgepartner.com>
2020-10-19 16:40:57 +0300

    lint warnings fixes

--- tests/screenshot/apps/components/VirtualList.js
+++ tests/screenshot/apps/components/VirtualList.js
@@ -1,17 +1,7 @@
-import Item from '../../../../Item';
 import VirtualList from '../../../../VirtualList';
 import React from 'react';
 
-const items = [];
-const renderItem = () => ({index}) => {
-	return (
-		<Item>
-			{items[index]}
-		</Item>
-	);
-};
-
 const VirtualListTests = [
-	<VirtualList dataSize={10} itemSize={144} itemRenderer={renderItem} />
+	<VirtualList dataSize={10} itemSize={144} />
 ];
 export default VirtualListTests;
