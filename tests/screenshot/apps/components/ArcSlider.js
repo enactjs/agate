8f117938020f410096d6d134c73d99e9b45f5211
Daniel Stoian <daniel.stoian@lgepartner.com>
2020-10-19 17:40:05 +0300

    added new tests for ArcSliser/ArcPicker

--- tests/screenshot/apps/components/ArcSlider.js
+++ tests/screenshot/apps/components/ArcSlider.js
@@ -1,10 +1,9 @@
 import ArcSlider from '../../../../ArcSlider';
 import React from 'react';
-import ArcPicker from "../../../../ArcPicker";
 
 const ArcSliderTests = [
 	<ArcSlider />,
-	<ArcPicker endAngle={300} startAngle={10} />
+	<ArcSlider endAngle={300} startAngle={10} />
 ];
 
 export default ArcSliderTests;
