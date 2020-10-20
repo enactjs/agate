b4a393bf36813702035545e76a595d9aebcfda54
Daniel Stoian <daniel.stoian@lgepartner.com>
2020-10-20 13:32:32 +0300

    fixed tests

--- tests/screenshot/apps/components/FanSpeedControl.js
+++ tests/screenshot/apps/components/FanSpeedControl.js
@@ -2,7 +2,7 @@ import FanSpeedControl from '../../../../FanSpeedControl';
 import React from 'react';
 
 const FanSpeedControlTests = [
-	<FanSpeedControl icon="fan" />
+	<FanSpeedControl icon="fan" min={1} max={10} />
 ];
 
 export default FanSpeedControlTests;
--- tests/screenshot/apps/components/VirtualList.js
+++ tests/screenshot/apps/components/VirtualList.js
@@ -1,7 +1,24 @@
 import VirtualList from '../../../../VirtualList';
 import React from 'react';
+import ri from "@enact/ui/resolution";
+import Item from "../../../../Item";
+
+const items = ['Item 000', 'Item 001', 'Item 002', 'Item 003', 'Item 004', 'Item 005', 'Item 006', 'Item 007', 'Item 008', 'Item 009'];
+const renderItem = () => ({index, ...rest}) => {
+	const itemStyle = {
+		borderBottom: ri.unit(6, 'rem') + ' solid #202328',
+		boxSizing: 'border-box',
+		height: ri.unit(144, 'rem')
+	};
+
+	return (
+		<Item {...rest} style={itemStyle}>
+			{items[index]}
+		</Item>
+	);
+};
 
 const VirtualListTests = [
-	<VirtualList dataSize={10} itemSize={144} />
+	<VirtualList dataSize={10} itemSize={ri.scale( 144)} itemRenderer={renderItem(ri.scale(144))} />
 ];
 export default VirtualListTests;
