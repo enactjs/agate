b10b0fd32aae4b310d25ba8dc8a24c296e1df1d5
Stanca Pop <stanca.pop@lgepartner.com>
2020-10-20 10:24:18 +0300

    fixed mediaplayer screenshot test

--- tests/screenshot/apps/components/MediaPlayer.js
+++ tests/screenshot/apps/components/MediaPlayer.js
@@ -2,9 +2,15 @@ import MediaPlayer from '../../../../MediaPlayer';
 import React from 'react';
 
 const MediaPlayerTests = [
-	<MediaPlayer />,
-	<MediaPlayer paused />,
-	<MediaPlayer shuffle />
+	<MediaPlayer>
+		<source src="https://sampleswap.org/mp3/artist/254731/BossPlayer_Your-Right-Here-160.mp3" type="audio/mp3" />
+	</MediaPlayer>,
+	<MediaPlayer paused>
+		<source src="https://sampleswap.org/mp3/artist/254731/BossPlayer_Your-Right-Here-160.mp3" type="audio/mp3" />
+	</MediaPlayer>,
+	<MediaPlayer shuffle>
+		<source src="https://sampleswap.org/mp3/artist/254731/BossPlayer_Your-Right-Here-160.mp3" type="audio/mp3" />
+	</MediaPlayer>
 ];
 
 export default MediaPlayerTests;
