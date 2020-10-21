7809c465e0779a24b1bb4dde701a3dbaa29d5e21
Stanca Pop <stanca.pop@lgepartner.com>
2020-10-21 17:37:53 +0300

    fixed lint warnings

--- tests/screenshot/apps/components/TabGroup.js
+++ tests/screenshot/apps/components/TabGroup.js
@@ -2,19 +2,21 @@ import TabGroup from '../../../../TabGroup';
 import React from 'react';
 
 const TabGroupTests = [
-	<TabGroup tabPosition="before"
-			  tabs={[
-				  {title: 'Home', icon: 'home'},
-				  {title: 'Settings', icon: 'setting'},
-				  {title: 'Theme', icon: 'display'}
-			  ]}
+	<TabGroup
+		tabPosition="before"
+		tabs={[
+			{title: 'Home', icon: 'home'},
+			{title: 'Settings', icon: 'setting'},
+			{title: 'Theme', icon: 'display'}
+		]}
 	/>,
-	<TabGroup tabPosition="after"
-			  tabs={[
-				  {title: 'Home', icon: 'home'},
-				  {title: 'Settings', icon: 'setting'},
-				  {title: 'Theme', icon: 'display'}
-			  ]}
+	<TabGroup
+		tabPosition="after"
+		tabs={[
+			{title: 'Home', icon: 'home'},
+			{title: 'Settings', icon: 'setting'},
+			{title: 'Theme', icon: 'display'}
+		]}
 	/>
 ];
 
