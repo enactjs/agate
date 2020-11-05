d9249097003a80967fa8619643c896504aa260c8
daniel-stoian-lgp <63335068+daniel-stoian-lgp@users.noreply.github.com>
2020-11-05 02:49:34 +0200

    PLAT-122910: Added more component views for screenshot test - set 2 (#384)
    * Added screenshot tests
    
    * Added screenshot tests
    
    * lint warnings fixes
    
    * lint warnings fixes
    
    * added new tests for ArcSliser/ArcPicker
    
    * added new tests for ArcSliser/ArcPicker
    
    * small fix
    
    * fixed tests
    
    * lint warning fixes
    
    * lint warning fixes
    
    * lint warning fixes
    
    * Add additional empty line for each test
    
    Enact-DCO-1.0-Signed-off-by: YB Sung (yb.sung@lge.com)
    
    * updated screenshot tests
    
    * updated screenshot tests
    
    * updated screenshot tests
    
    * updated screenshot tests
    
    * fixed merge conflicts
    
    Co-authored-by: YunBum Sung <yb.sung@lge.com>

--- tests/screenshot/apps/importer.js
+++ tests/screenshot/apps/importer.js
@@ -1,16 +1,21 @@
+import ArcPicker from '../../../ArcPicker';
+import ArcSlider from '../../../ArcSlider';
 import BodyText from '../../../BodyText';
 import Button from '../../../Button';
+import CheckboxItem from '../../../CheckboxItem';
 import ColorPicker from '../../../ColorPicker';
 import ContextualPopupDecorator from '../../../ContextualPopupDecorator';
 import DatePicker from '../../../DatePicker';
 import DateTimePicker from '../../../DateTimePicker';
 import Drawer from '../../../Drawer';
 import Dropdown from '../../../Dropdown';
+import FanSpeedControl from '../../../FanSpeedControl';
 import FullscreenPopup from '../../../FullscreenPopup';
 import Header from '../../../Header';
 import Heading from '../../../Heading';
 import Icon from '../../../Icon';
 import Image from '../../../Image';
+import ImageItem from '../../../ImageItem';
 import IncrementSlider from '../../../IncrementSlider';
 import Input from '../../../Input';
 import Item from '../../../Item';
@@ -25,28 +30,40 @@ import PopupMenu from '../../../PopupMenu';
 import ProgressBar from '../../../ProgressBar';
 import RadioItem from '../../../RadioItem';
 import RangePicker from '../../../RangePicker';
+import Scroller from '../../../Scroller';
 import Slider from '../../../Slider';
 import SliderButton from '../../../SliderButton';
+import Spinner from '../../../Spinner';
 import SwitchItem from '../../../SwitchItem';
 import TabGroup from '../../../TabGroup';
 import TimePicker from '../../../TimePicker';
+import TemperatureControl from '../../../TemperatureControl';
+import ThumbnailItem from '../../../ThumbnailItem';
 import ToggleButton from '../../../ToggleButton';
 import TooltipDecorator from '../../../TooltipDecorator';
+import VirtualList from '../../../VirtualList';
+import WindDirectionControl from '../../../WindDirectionControl';
 
 const AgateExports = {
+
+	ArcPicker,
+	ArcSlider,
 	BodyText,
 	Button,
 	ColorPicker,
+	CheckboxItem,
 	ContextualPopupDecorator,
 	DatePicker,
 	DateTimePicker,
 	Drawer,
 	Dropdown,
+	FanSpeedControl,
 	FullscreenPopup,
 	Header,
 	Heading,
 	Icon,
 	Image,
+	ImageItem,
 	IncrementSlider,
 	Input,
 	Item,
@@ -61,13 +78,19 @@ const AgateExports = {
 	ProgressBar,
 	RadioItem,
 	RangePicker,
+	Scroller,
 	Slider,
 	SliderButton,
+	Spinner,
 	SwitchItem,
 	TabGroup,
 	TimePicker,
+	TemperatureControl,
+	ThumbnailItem,
 	ToggleButton,
-	TooltipDecorator
+	TooltipDecorator,
+	VirtualList,
+	WindDirectionControl
 };
 
 export default AgateExports;
