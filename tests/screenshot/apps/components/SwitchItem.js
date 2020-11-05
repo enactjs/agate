ca1e4a837626cd03245638046773a1c42075aed6
Daniel Stoian <daniel.stoian@lgepartner.com>
2020-11-05 11:57:30 +0200

    code review fixes + added screenshot tests

--- tests/screenshot/apps/components/SwitchItem.js
+++ tests/screenshot/apps/components/SwitchItem.js
@@ -1,13 +1,3 @@
-// import SwitchItem from '../../../../SwitchItem';
-// import React from 'react';
-//
-// const SwitchItemTests = [
-// 	<SwitchItem />
-// ];
-//
-// export default SwitchItemTests;
-
-
 import SwitchItem from '../../../../SwitchItem';
 import React from 'react';
 
