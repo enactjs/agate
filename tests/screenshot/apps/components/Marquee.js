d02be47ffec61550c1f752a31e99042758582b69
Stanca Pop <stanca.pop@lgepartner.com>
2020-11-06 18:05:38 +0200

    added screenshot tests for Marquee

---
+++ tests/screenshot/apps/components/Marquee.js
@@ -0,0 +1,8 @@
+import Marquee from '../../../../Marquee';
+import React from 'react';
+
+const MarqueeTests = [
+	<Marquee speed={100}>The quick brown fox jumped over the lazy dog. The bean bird flies at sundown.</Marquee>,
+	<Marquee rtl>The quick brown fox jumped over the lazy dog. The bean bird flies at sundown.</Marquee>
+];
+export default MarqueeTests;
