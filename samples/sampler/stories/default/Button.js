13c473047d15a22035d4f9d583deb3cfeabd4c29
Stanca Pop <stanca.pop@lgepartner.com>
2020-11-09 18:10:37 +0200

    fixes after review

--- samples/sampler/stories/default/Button.js
+++ samples/sampler/stories/default/Button.js
@@ -18,7 +18,18 @@ const prop = {
 	casing: ['preserve', 'sentence', 'word', 'upper'],
 	colors: ['', '#E6444B', '#FDC902', '#986AAD', '#4E75E1', '#30CC83', '#44C8D5', '#47439B', '#2D32A6', '#4E75E1'],
 	iconPosition: ['', 'before', 'after'],
-	joinedPosition: ['', 'left', 'center', 'right']
+	joinedPosition: ['', 'left', 'center', 'right'],
+	minWidth: {'undefined/null (automatic)': '', 'true (enforce)': true, 'false (ignore)': 'false'}
+};
+
+// The following is needed to allow us to disambiguate between minWidth=false and minWidth=undefined
+const threeWayBoolean = (value) => {
+	switch (value) {
+		case 'true': return true;
+		case 'false': return false;
+		case '': return null;
+		default: return value;
+	}
 };
 
 const SkinnedButton = Skinnable(
@@ -39,6 +50,7 @@ const SkinnedButton = Skinnable(
 				icon={select('icon', icons, Config)}
 				iconPosition={select('iconPosition', prop.iconPosition, Config)}
 				joinedPosition={select('joinedPosition', prop.joinedPosition, Config)}
+				minWidth={threeWayBoolean(select('minWidth', prop.minWidth, Config))}
 				onClick={action('onClick')}
 				selected={boolean('selected', Config)}
 				size={select('size', ['smallest', 'small', 'large', 'huge'], Config)}
