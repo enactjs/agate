25335331270dc9a295974e99a04627b0b7d46bbc
daniel-stoian-lgp <63335068+daniel-stoian-lgp@users.noreply.github.com>
2020-11-09 11:29:23 +0200

    Update samples/sampler/stories/default/ArcSlider.js
    Co-authored-by: Mikyung Kim <mikyung27.kim@lge.com>

--- samples/sampler/stories/default/ArcSlider.js
+++ samples/sampler/stories/default/ArcSlider.js
@@ -1,6 +1,6 @@
 import {mergeComponentMetadata} from '@enact/storybook-utils';
 import {action} from '@enact/storybook-utils/addons/actions';
-import {select, number, boolean} from '@enact/storybook-utils/addons/knobs';
+import {boolean, select, number} from '@enact/storybook-utils/addons/knobs';
 import React from 'react';
 import {storiesOf} from '@storybook/react';
 
