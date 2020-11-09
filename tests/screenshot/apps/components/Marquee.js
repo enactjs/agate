0e9adb4d2cff7104787b34abfc9466bd5a694d02
Stanca Pop <stanca.pop@lgepartner.com>
2020-11-09 16:37:12 +0200

    enhanced Marquee story

--- tests/screenshot/apps/components/Marquee.js
+++ tests/screenshot/apps/components/Marquee.js
@@ -2,7 +2,15 @@ import Marquee from '../../../../Marquee';
 import React from 'react';
 
 const MarqueeTests = [
-	<Marquee speed={100}>The quick brown fox jumped over the lazy dog. The bean bird flies at sundown.</Marquee>,
-	<Marquee rtl>The quick brown fox jumped over the lazy dog. The bean bird flies at sundown.</Marquee>
+	<Marquee speed={100}>
+		The quick brown fox jumped over the lazy dog. The bean bird flies at sundown.
+		The quick brown fox jumped over the lazy dog. The bean bird flies at sundown.
+		The quick brown fox jumped over the lazy dog. The bean bird flies at sundown.
+	</Marquee>,
+	<Marquee disabled>
+		The quick brown fox jumped over the lazy dog. The bean bird flies at sundown.
+		The quick brown fox jumped over the lazy dog. The bean bird flies at sundown.
+		The quick brown fox jumped over the lazy dog. The bean bird flies at sundown.
+	</Marquee>
 ];
 export default MarqueeTests;
