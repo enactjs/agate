e72d00ade26bca88d3e06ed8be43b4b62d8d61ce
Adrian Cocoara <adrian.cocoara@lgepartner.com>
2020-11-11 09:07:47 +0200

    *WIP* - refactor checkbox

--- Checkbox/Checkbox.js
+++ Checkbox/Checkbox.js
@@ -19,6 +19,13 @@ import ToggleIcon from '../internal/ToggleIcon/ToggleIcon';
 
 import css from './Checkbox.module.less';
 
+// Imports added from sandstone
+import Spottable from '@enact/spotlight/Spottable';
+import Touchable from '@enact/ui/Touchable';
+import Toggleable from '@enact/ui/Toggleable';
+import compose from 'ramda/src/compose';
+import Skinnable from '../Skinnable';
+
 /**
  * A checkbox component, ready to use in Agate applications.
  *
@@ -48,28 +55,99 @@ const CheckboxBase = kind({
 		 * @default	'check'
 		 * @public
 		 */
-		children: PropTypes.string
+		children: PropTypes.string,
+
+		// Additional prop-types
+		css: PropTypes.object,
+		disabled: PropTypes.bool,
+		indeterminate: PropTypes.bool,
+		indeterminateIcon: PropTypes.oneOfType([PropTypes.string, PropTypes.object]),
+		selected: PropTypes.bool,
+		standalone: PropTypes.bool
 	},
 
 	defaultProps: {
-		children: 'check'
+		children: 'check',
+
+		// Additional defaultProps
+		indeterminate: false,
+		indeterminateIcon: 'minus',
+		selected: false
 	},
 
-	render: ({children, ...rest}) => {
+	// Additional styles block
+	styles: {
+		css: css,
+		className: 'toggleIcon'
+	},
+
+	// Additional computed block
+	computed: {
+		className: ({indeterminate, selected, styler}) => styler.append({selected, indeterminate}),
+		children: ({indeterminate, indeterminateIcon, children}) => (indeterminate ? indeterminateIcon : children)
+	},
+
+	// Render function from Sandstone
+	render: ({children, css, disabled, selected, ...rest}) => {
+		delete rest.indeterminate;
+		delete rest.indeterminateIcon;
+		delete rest.standalone;
+
+		console.log(rest);
 		return (
-			<ToggleIcon
+			<div
 				{...rest}
-				css={css}
-				iconComponent={Icon}
+				aria-checked={selected}
+				aria-disabled={disabled}
+				disabled={disabled}
+				role="checkbox"
 			>
-				{children}
-			</ToggleIcon>
-		);
+				<div>
+					<Icon
+						size="small"
+						className={css.icon}
+					>
+						{children}
+					</Icon>
+				</div>
+			</div>
+		)
 	}
+
+	// render: ({children, ...rest}) => {
+	// 	return (
+	// 		<ToggleIcon
+	// 			{...rest}
+	// 			css={css}
+	// 			iconComponent={Icon}
+	// 		>
+	// 			{children}
+	// 		</ToggleIcon>
+	// 	);
+	// }
 });
 
-export default CheckboxBase;
+// Compose like sandstone
+const CheckboxDecorator = compose(
+	Toggleable({toggleProp: 'onClick'}),
+	Touchable,
+	Spottable,
+	Skinnable
+);
+
+// Assign variable like sandstone
+const Checkbox = CheckboxDecorator(CheckboxBase);
+
+// Sandstone-like exports
+export default Checkbox;
 export {
-	CheckboxBase as Checkbox,
-	CheckboxBase
+	Checkbox,
+	CheckboxBase,
+	CheckboxDecorator
 };
+
+// export default CheckboxBase;
+// export {
+// 	CheckboxBase as Checkbox,
+// 	CheckboxBase
+// };
