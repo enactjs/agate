50c8c99adfd9032cac7a59b423e86d076ec48a8a
Stanca <63341832+stanca-pop-lgp@users.noreply.github.com>
2020-11-12 02:29:53 +0200

    PLAT-123667: Visual transition for Picker components (#364)
    * added transition animation to picker components
    
    * fixed lint warnings
    
    * fixed tests
    
    * fixes after commit
    
    * fixes after review
    
    * fixes
    
    * Update internal/Picker/PickerItem.js
    
    Co-authored-by: YunBum SUNG <yb.sung@lge.com>
    
    * review fixes
    
    * added changeAdapter
    
    * fixed lint warnings
    
    * added description to ChangeAdapter hoc
    
    * fixed hourPicker animation
    
    Co-authored-by: YunBum SUNG <yb.sung@lge.com>

--- TimePicker/TimePickerBase.js
+++ TimePicker/TimePickerBase.js
@@ -38,13 +38,17 @@ class HourPicker extends React.Component {
 		super(props);
 
 		this.state = {
+			noAnimation: false,
 			prevValue: props.value
 		};
 	}
 
 	static getDerivedStateFromProps (props, state) {
 		if (state.prevValue !== props.value) {
+			const hours = props.hasMeridiem ? hours12 : hours24;
+
 			return {
+				noAnimation: hours[state.prevValue] === hours[props.value],
 				prevValue: props.value
 			};
 		}
@@ -57,7 +61,7 @@ class HourPicker extends React.Component {
 		const hours = hasMeridiem ? hours12 : hours24;
 
 		return (
-			<DateComponentPicker {...rest}>
+			<DateComponentPicker {...rest} noAnimation={this.state.noAnimation}>
 				{hours}
 			</DateComponentPicker>
 		);
