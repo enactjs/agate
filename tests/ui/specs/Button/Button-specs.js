2ee4157385af17333ac3a524355799fa0a2547fd
Stanca Pop <stanca.pop@lgepartner.com>
2020-11-23 21:41:15 +0200

    added more ui tests

--- tests/ui/specs/Button/Button-specs.js
+++ tests/ui/specs/Button/Button-specs.js
@@ -9,9 +9,34 @@ describe('Button', function () {
 	const {
 		buttonDefault,
 		buttonDisabled,
+		buttonFalseMinWidth,
 		buttonJoinedRight,
+		buttonSizeSmall,
+		buttonTransparent,
 		iconButton
 	} = Page.components;
+
+	describe('button appearance', function () {
+		it('should display an icon button', function () {
+			expect(iconButton.isIconButton).to.be.true();
+		});
+
+		it('should not display a large size button', function () {
+			expect(buttonSizeSmall.isLarge).to.not.be.true();
+		});
+
+		it('should not have minWidth class', function () {
+			expect(buttonFalseMinWidth.isMinWidth).to.not.be.true();
+		});
+
+		it('should have default opaque backgroundOpacity', function () {
+			expect(buttonDefault.isOpaque).to.be.true();
+		});
+
+		it('should be transparent', function () {
+			expect(buttonTransparent.isTransparent).to.be.true();
+		});
+	});
 
 	describe('5-way', function () {
 		it('should focus disabled button on 5-way right', function () {
