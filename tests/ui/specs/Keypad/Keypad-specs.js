7afef03dde99ce0baae11eefaba08963df48365a
Daniel Stoian <daniel.stoian@lgepartner.com>
2020-11-23 17:57:25 +0200

    added ui tests for keypad

---
+++ tests/ui/specs/Keypad/Keypad-specs.js
@@ -0,0 +1,45 @@
+const Page = require('./KeypadPage');
+
+describe('Keypad', function () {
+
+	beforeEach(function () {
+		Page.open();
+	});
+
+	describe('5 way', function () {
+		const keypad = Page.components.keypadDefault;
+
+		it('should have focus on first button at start', function () {
+			expect(keypad.button(1).isFocused()).to.be.true();
+		});
+
+		it('can navigate down/left/right/up between keys', function () {
+			expect(keypad.button(1).isFocused()).to.be.true();
+			Page.spotlightDown();
+			expect(keypad.button(4).isFocused()).to.be.true();
+			Page.spotlightRight();
+			expect(keypad.button(5).isFocused()).to.be.true();
+			Page.spotlightUp();
+			expect(keypad.button(2).isFocused()).to.be.true();
+			Page.spotlightLeft();
+			expect(keypad.button(1).isFocused()).to.be.true();
+		});
+	});
+
+	describe('5 way disabled', function () {
+		const keypad = Page.components.keypadDisabled;
+
+		it('buttons are focusable when navigating down/left/right/up', function () {
+			keypad.focus();
+			expect(keypad.button(1).isFocused()).to.be.true();
+			Page.spotlightDown();
+			expect(keypad.button(4).isFocused()).to.be.true();
+			Page.spotlightRight();
+			expect(keypad.button(5).isFocused()).to.be.true();
+			Page.spotlightUp();
+			expect(keypad.button(2).isFocused()).to.be.true();
+			Page.spotlightLeft();
+			expect(keypad.button(1).isFocused()).to.be.true();
+		});
+	});
+});
