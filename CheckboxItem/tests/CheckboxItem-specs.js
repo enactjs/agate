4754074bf02a93cfe89f7dbf9203d852b263dc5e
Adrian Cocoara <adrian.cocoara@lgepartner.com>
2020-11-23 10:33:24 +0200

    added unit tests for CheckboxItem

--- CheckboxItem/tests/CheckboxItem-specs.js
+++ CheckboxItem/tests/CheckboxItem-specs.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import {mount} from 'enzyme';
 import {CheckboxItemBase} from '../CheckboxItem';
+import css from '../../Checkbox/Checkbox.module.less';
 
 describe('CheckboxItem Specs', () => {
 	test('should support a custom icon', () => {
@@ -15,5 +16,45 @@ describe('CheckboxItem Specs', () => {
 		const actual = subject.find('Checkbox').prop('children');
 
 		expect(actual).toBe(expected);
+	});
+
+	test('should render correct icon when selected', () => {
+
+		const subject = mount(
+			<CheckboxItemBase selected />
+		);
+
+		const expected = 1;
+		const actual =  subject.find('Checkbox').find(`.${css.selected}`).length;
+
+		expect(actual).toBe(expected);
+	});
+
+	test('should include the indeterminate class when indeterminate', () => {
+
+		const subject = mount(
+			<CheckboxItemBase indeterminate>
+				Hello CheckboxItem
+			</CheckboxItemBase>
+		);
+
+		const expected = 1;
+		const actual = subject.find('Checkbox').find(`.${css.indeterminate}`).length;
+
+		expect(actual).toBe(expected);
+	});
+
+	test('should not include the indeterminate class when not indeterminate', () => {
+
+		const subject = mount(
+			<CheckboxItemBase>
+				Hello CheckboxItem
+			</CheckboxItemBase>
+		);
+
+		const expected = css.indeterminate;
+		const actual = subject.find('Checkbox').find(`.${css.indeterminate}`);
+
+		expect(actual).not.toContain(expected);
 	});
 });
