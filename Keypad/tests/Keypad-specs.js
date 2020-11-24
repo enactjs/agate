4edaa93e6974ce3e2483794ca32026a9df3c9eca
Daniel Stoian <daniel.stoian@lgepartner.com>
2020-11-24 12:05:52 +0200

    added unit tests for keypad

---
+++ Keypad/tests/Keypad-specs.js
@@ -0,0 +1,44 @@
+import React from 'react';
+import {mount} from 'enzyme';
+import Keypad from '../Keypad';
+
+import css from '../Keypad.module.less';
+
+const click = (key, value) => key.find(`.${css.button}[aria-label="${value}"]`).simulate('click');
+
+describe('Keypad Specs', () => {
+	test('should add new digits on every digit click', () => {
+		const handleChange = jest.fn();
+
+		const keypad = mount(
+			<Keypad onChange={handleChange} />
+		);
+
+		click(keypad, 2);
+		click(keypad, 5);
+		click(keypad, 7);
+
+		const expected = '257';
+		const actual = handleChange.mock.calls[2][0].value;
+
+		expect(actual).toBe(expected);
+	});
+
+	test('should remove digits on every backspace button click', () => {
+		const handleChange = jest.fn();
+
+		const keypad = mount(
+			<Keypad onChange={handleChange} />
+		);
+
+		click(keypad, 2);
+		click(keypad, 5);
+		click(keypad, 7);
+		click(keypad, 'backspace');
+
+		const expected = '25';
+		const actual = handleChange.mock.calls[3][0].value;
+
+		expect(actual).toBe(expected);
+	});
+});
