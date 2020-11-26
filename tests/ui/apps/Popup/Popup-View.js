40d3cbbf6ff2c0e2a35e95f05eeff7b288dddf56
Stanca Pop <stanca.pop@lgepartner.com>
2020-11-26 16:43:01 +0200

    fix

--- tests/ui/apps/Popup/Popup-View.js
+++ tests/ui/apps/Popup/Popup-View.js
@@ -4,14 +4,8 @@ import ThemeDecorator from '../../../../ThemeDecorator';
 import React, {Component} from 'react';
 import spotlight from '@enact/spotlight';
 import SpotlightContainerDecorator from '@enact/spotlight/SpotlightContainerDecorator';
-import Toggleable from '@enact/ui/Toggleable';
 
 const Container = SpotlightContainerDecorator('div');
-const AnnotatedPopup = Toggleable({toggle: null, activate: 'onOpen', deactivate: 'onHide'}, ({selected, ...rest}) => {
-	return (
-		<Popup {...rest} data-popup-open={selected || null} />
-	);
-});
 
 const style = {
 	main: {
@@ -71,7 +65,7 @@ class app extends Component {
 					<Button id="buttonPopup9" onClick={() => this.clickHandler({open9: true})}>scrimType none</Button>
 					<Button id="buttonPopup10" onClick={this.togglePopup}>Toggle Open</Button>
 				</div>
-				<AnnotatedPopup
+				<Popup
 					id="popup1"
 					open={this.state.open1}
 					noAnimation={false}
@@ -85,8 +79,8 @@ class app extends Component {
 						<Button id="buttonOK" onClick={() => this.clickHandler({open1: false})}>OK</Button>
 						<Button id="buttonCancel" onClick={() => this.clickHandler({open1: false})}>Cancel</Button>
 					</Container>
-				</AnnotatedPopup>
-				<AnnotatedPopup
+				</Popup>
+				<Popup
 					id="popup2"
 					open={this.state.open2}
 					noAnimation={false}
@@ -100,8 +94,8 @@ class app extends Component {
 						<Button id="buttonOK" onClick={() => this.clickHandler({open2: false})}>OK</Button>
 						<Button id="buttonCancel" onClick={() => this.clickHandler({open2: false})}>Cancel</Button>
 					</Container>
-				</AnnotatedPopup>
-				<AnnotatedPopup
+				</Popup>
+				<Popup
 					id="popup3"
 					open={this.state.open3}
 					noAnimation={false}
@@ -110,8 +104,8 @@ class app extends Component {
 					onClose={() => this.clickHandler({open3: false})}
 				>
 					<div>Popup with no Component</div>
-				</AnnotatedPopup>
-				<AnnotatedPopup
+				</Popup>
+				<Popup
 					id="popup4"
 					open={this.state.open4}
 					noAnimation
@@ -125,8 +119,8 @@ class app extends Component {
 						<Button id="buttonOK" onClick={() => this.clickHandler({open4: false})}>OK</Button>
 						<Button id="buttonCancel" onClick={() => this.clickHandler({open4: false})}>Cancel</Button>
 					</Container>
-				</AnnotatedPopup>
-				<AnnotatedPopup
+				</Popup>
+				<Popup
 					id="popup5"
 					open={this.state.open5}
 					noAnimation={false}
@@ -140,8 +134,8 @@ class app extends Component {
 						<Button id="buttonOK" onClick={() => this.clickHandler({open5: false})}>OK</Button>
 						<Button id="buttonCancel" onClick={() => this.clickHandler({open5: false})}>Cancel</Button>
 					</Container>
-				</AnnotatedPopup>
-				<AnnotatedPopup
+				</Popup>
+				<Popup
 					id="popup6"
 					open={this.state.open6}
 					noAnimation={false}
@@ -155,8 +149,8 @@ class app extends Component {
 						<Button id="buttonOK" onClick={() => this.clickHandler({open6: false})}>OK</Button>
 						<Button id="buttonCancel" onClick={() => this.clickHandler({open6: false})}>Cancel</Button>
 					</Container>
-				</AnnotatedPopup>
-				<AnnotatedPopup
+				</Popup>
+				<Popup
 					id="popup7"
 					open={this.state.open7}
 					noAnimation={false}
@@ -170,8 +164,8 @@ class app extends Component {
 						<Button id="buttonOK" onClick={() => this.clickHandler({open7: false})}>OK</Button>
 						<Button id="buttonCancel" onClick={() => this.clickHandler({open7: false})}>Cancel</Button>
 					</Container>
-				</AnnotatedPopup>
-				<AnnotatedPopup
+				</Popup>
+				<Popup
 					id="popup8"
 					open={this.state.open8}
 					noAnimation={false}
@@ -186,8 +180,8 @@ class app extends Component {
 						<Button id="buttonOK" onClick={() => this.clickHandler({open8: false})}>OK</Button>
 						<Button id="buttonCancel" onClick={() => this.clickHandler({open8: false})}>Cancel</Button>
 					</Container>
-				</AnnotatedPopup>
-				<AnnotatedPopup
+				</Popup>
+				<Popup
 					id="popup9"
 					open={this.state.open9}
 					noAnimation={false}
@@ -202,13 +196,13 @@ class app extends Component {
 						<Button id="buttonOK" onClick={() => this.clickHandler({open9: false})}>OK</Button>
 						<Button id="buttonCancel" onClick={() => this.clickHandler({open9: false})}>Cancel</Button>
 					</Container>
-				</AnnotatedPopup>
-				<AnnotatedPopup
+				</Popup>
+				<Popup
 					id="popup10"
 					open={this.state.open10}
 				>
 					<Button>close</Button>
-				</AnnotatedPopup>
+				</Popup>
 			</div>
 		);
 	}
