aa548dc76769cdec1dbf424bbfc6f96132ba3e42
Stanca Pop <stanca.pop@lgepartner.com>
2020-11-26 13:29:11 +0200

    fixed import

--- tests/ui/apps/Dropdown/InScroller/DropdownInScroller-View.js
+++ tests/ui/apps/Dropdown/InScroller/DropdownInScroller-View.js
@@ -3,7 +3,7 @@ import Scroller from '../../../../../Scroller';
 import ThemeDecorator from '../../../../../ThemeDecorator';
 import React from 'react';
 import spotlight from '@enact/spotlight';
-import {scaleToRem} from 'enact/packages/ui/resolution';
+import {scaleToRem} from '@enact/ui/resolution';
 
 // NOTE: Forcing pointer mode off so we can be sure that regardless of webOS pointer mode the app
 // runs the same way
