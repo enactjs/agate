5037cde42a80e84ce5e9233c7046dbfee7a54fd9
Daniel Stoian <daniel.stoian@lgepartner.com>
2020-11-27 10:53:26 +0200

    added tests for ArcPicker

--- ArcPicker/tests/ArcPicker-specs.js
+++ ArcPicker/tests/ArcPicker-specs.js
@@ -10,15 +10,15 @@ describe('ArcPicker Specs', () => {
 			const arcPicker = mount(
 				<ArcPicker onChange={handleChange}>{[1, 2, 3, 4]}</ArcPicker>
 			);
-			//find second Arc and click on the second <path> element inside of it
-			arcPicker.find(`Arc`).at(1).find('path').at(1).simulate('click');
+			// find second Arc and click on the second <path> element inside of it
+			arcPicker.find('Arc').at(1).find('path').at(1).simulate('click');
 
 			const expected = 2;
 			const actual = handleChange.mock.calls[0][0].value;
 			expect(actual).toBe(expected);
 
-			//find fourth Arc and click on the second <path> element inside of it
-			arcPicker.find(`Arc`).at(3).find('path').at(1).simulate('click');
+			// find fourth Arc and click on the second <path> element inside of it
+			arcPicker.find('Arc').at(3).find('path').at(1).simulate('click');
 
 			const secondExpected = 4;
 			const secondActual = handleChange.mock.calls[1][0].value;
