b21138fd44c6202add8c288c89853730c6873b73
Daniel Stoian <daniel.stoian@lgepartner.com>
2020-12-03 16:07:10 +0200

    small fix

--- tests/ui/apps/ArcSlider/ArcSlider-View.js
+++ tests/ui/apps/ArcSlider/ArcSlider-View.js
@@ -3,6 +3,8 @@ import Heading from '../../../../Heading';
 import ThemeDecorator from '../../../../ThemeDecorator';
 import React from 'react';
 import spotlight from '@enact/spotlight';
+import {scaleToRem} from "@enact/ui/resolution";
+import Scroller from "../../../../Scroller";
 
 // NOTE: Forcing pointer mode off so we can be sure that regardless of webOS pointer mode the app
 // runs the same way
@@ -10,12 +12,14 @@ spotlight.setPointerMode(false);
 
 const app = (props) => <div {...props}>
 	<div>
-		<Heading>ArcSlider default</Heading>
-		<ArcSlider id="arcSliderDefault" />
-		<Heading>ArcSlider custom min, max, endAngle, startAngle</Heading>
-		<ArcSlider endAngle={350} foregroundColor="#fdc902" min={0} max={2} startAngle={10} id="arcSliderCustom" />
-		<Heading>ArcSlider disabled</Heading>
-		<ArcSlider disabled id="arcSliderDisabled" />
+		<Scroller style={{height: scaleToRem(900)}}>
+			<Heading>ArcSlider default</Heading>
+			<ArcSlider id="arcSliderDefault" />
+			<Heading>ArcSlider custom min, max, endAngle, startAngle</Heading>
+			<ArcSlider endAngle={350} foregroundColor="#fdc902" min={0} max={2} startAngle={10} id="arcSliderCustom" />
+			<Heading>ArcSlider disabled</Heading>
+			<ArcSlider disabled id="arcSliderDisabled" />
+		</Scroller>
 	</div>
 </div>;
 
