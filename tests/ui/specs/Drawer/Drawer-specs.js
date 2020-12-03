c218ee140b4ac071043d8edcb4ab1f418bfb1949
Stanca Pop <stanca.pop@lgepartner.com>
2020-12-03 23:34:34 +0200

    added ui tests

---
+++ tests/ui/specs/Drawer/Drawer-specs.js
@@ -0,0 +1,288 @@
+let Page = require('./DrawerPage'),
+	{validateTitle, expectClosed, expectNoneScrimOpen, expectOpen} = require('./Drawer-utils.js');
+
+describe('Drawer', function () {
+
+	const drawerCommon = Page.drawerCommon;
+
+	beforeEach(function () {
+		Page.open();
+	});
+
+	it('should focus the first button on start', function () {
+		expect(drawerCommon.buttonDrawer1.isFocused()).to.be.true();
+	});
+
+	it('should not have the drawer on start', function () {
+		expectClosed(drawerCommon);
+	});
+
+	describe('with no line', function () {
+
+		const drawer = Page.components.drawer1;
+
+		it('should have correct heading', function () {
+			drawerCommon.buttonDrawer1.click();
+
+			Page.waitForOpen(drawer);
+
+			expectOpen(drawerCommon);
+			validateTitle(drawer, 'Drawer with no line');
+		});
+
+		it('should not have a line under the heading text', function () {
+			drawerCommon.buttonDrawer1.click();
+
+			Page.waitForOpen(drawer);
+
+			expectOpen(drawerCommon);
+			expect(drawer.hasLine).to.not.be.true();
+		});
+
+		describe('using 5-way', function () {
+
+			it('should open drawer', function () {
+				Page.spotlightSelect();
+				Page.waitForOpen(drawer);
+
+				expectOpen(drawerCommon);
+				expect(drawer.isOpen).to.be.true();
+			});
+
+			it('should close drawer with cancel button on 5-way right in drawer container', function () {
+				Page.spotlightSelect();
+				Page.waitForOpen(drawer);
+
+				expectOpen(drawerCommon);
+
+				Page.spotlightDown();
+				Page.spotlightSelect();
+
+				Page.waitForClose(drawer);
+				expectClosed(drawerCommon);
+			});
+		});
+
+		describe('using pointer', function () {
+			it('should open the drawer with scrim on click', function () {
+				drawerCommon.buttonDrawer1.click();
+				Page.waitForOpen(drawer);
+
+				expectOpen(drawerCommon);
+			});
+
+			it('should close the drawer and scrim on cancel click in drawer container', function () {
+				drawerCommon.buttonDrawer1.click();
+				Page.waitForOpen(drawer);
+
+				expectOpen(drawerCommon);
+
+				drawer.buttonCancel.click();
+				Page.waitForClose(drawer);
+
+				expectClosed(drawerCommon);
+			});
+		});
+	});
+
+	describe('with noAnimation', function () {
+
+		const drawer = Page.components.drawer2;
+
+		it('should have correct heading', function () {
+			drawerCommon.buttonDrawer2.click();
+			browser.pause(100); // needed to pass instead of waitTransitionEnd
+			expectOpen(drawerCommon);
+			validateTitle(drawer, 'Drawer without animation');
+		});
+
+		describe('using 5-way', function () {
+
+			it('should open drawer', function () {
+				Page.spotlightRight();
+				Page.spotlightSelect();
+				browser.pause(100); // needed to pass instead of waitTransitionEnd
+
+				expectOpen(drawerCommon);
+				expect(drawer.isOpen).to.be.true();
+			});
+
+			it('should close drawer with cancel button on 5-way right in drawer container', function () {
+				Page.spotlightRight();
+				Page.spotlightSelect();
+				browser.pause(100); // needed to pass instead of waitTransitionEnd
+
+				expectOpen(drawerCommon);
+
+				Page.spotlightDown();
+				Page.spotlightSelect();
+
+				browser.pause(100); // needed to pass instead of waitTransitionEnd
+				expectClosed(drawerCommon);
+			});
+		});
+
+		describe('using pointer', function () {
+
+			it('should open the drawer with scrim on click', function () {
+				drawerCommon.buttonDrawer2.click();
+				browser.pause(100); // needed to pass instead of waitTransitionEnd
+
+				expectOpen(drawerCommon);
+			});
+
+			it('should close the drawer and scrim on cancel click in drawer container', function () {
+				drawerCommon.buttonDrawer2.click();
+				browser.pause(100); // needed to pass instead of waitTransitionEnd
+
+				expectOpen(drawerCommon);
+
+				drawer.buttonCancel.click();
+				browser.pause(100); // needed to pass instead of waitTransitionEnd
+
+				expectClosed(drawerCommon);
+			});
+		});
+	});
+
+	describe('with scrimType - transparent', function () {
+
+		const drawer = Page.components.drawer3;
+
+		it('should have correct heading', function () {
+			drawerCommon.buttonDrawer3.click();
+			Page.waitForOpen(drawer);
+
+			expectOpen(drawerCommon);
+			validateTitle(drawer, 'Drawer with transparent scrim');
+		});
+
+		describe('using 5-way', function () {
+
+			it('should open drawer', function () {
+				Page.spotlightRight();
+				Page.spotlightRight();
+				Page.spotlightSelect();
+				Page.waitForOpen(drawer);
+
+				expectOpen(drawerCommon);
+				expect(drawer.isOpen).to.be.true();
+			});
+
+			it('should close drawer with cancel button on 5-way right in drawer container', function () {
+				Page.spotlightRight();
+				Page.spotlightRight();
+				Page.spotlightSelect();
+				Page.waitForOpen(drawer);
+
+				expectOpen(drawerCommon);
+
+				Page.spotlightDown();
+				Page.spotlightSelect();
+
+				Page.waitForClose(drawer);
+				expectClosed(drawerCommon);
+			});
+		});
+
+		describe('using pointer', function () {
+
+			it('should open the drawer with scrim on click', function () {
+				drawerCommon.buttonDrawer3.click();
+				Page.waitForOpen(drawer);
+
+				expectOpen(drawerCommon);
+			});
+
+			it('should close the drawer and scrim on cancel click in drawer container', function () {
+				drawerCommon.buttonDrawer3.click();
+				Page.waitForOpen(drawer);
+
+				expectOpen(drawerCommon);
+
+				drawer.buttonCancel.click();
+				Page.waitForClose(drawer);
+
+				expectClosed(drawerCommon);
+			});
+		});
+
+		describe('with scrimType - none', function () {
+
+			const drawer = Page.components.drawer4;
+
+			it('should have correct heading', function () {
+				drawerCommon.buttonDrawer4.click();
+				Page.waitForOpen(drawer);
+
+				expectNoneScrimOpen(drawerCommon);
+				validateTitle(drawer, 'Drawer without scrim');
+			});
+
+			describe('using 5-way', function () {
+
+				it('should open drawer', function () {
+					Page.spotlightRight();
+					Page.spotlightRight();
+					Page.spotlightRight();
+					Page.spotlightSelect();
+					Page.waitForOpen(drawer);
+
+					expectNoneScrimOpen(drawerCommon);
+					expect(drawer.isOpen).to.be.true();
+				});
+
+				it('should close drawer with cancel button on 5-way right in drawer container', function () {
+					Page.spotlightRight();
+					Page.spotlightRight();
+					Page.spotlightRight();
+					Page.spotlightSelect();
+					Page.waitForOpen(drawer);
+
+					expectNoneScrimOpen(drawerCommon);
+
+					Page.spotlightDown();
+					Page.spotlightSelect();
+
+					Page.waitForClose(drawer);
+					expectClosed(drawerCommon);
+				});
+			});
+
+			describe('using pointer', function () {
+
+				it('should open the drawer without scrim on click', function () {
+					drawerCommon.buttonDrawer4.click();
+
+					expectNoneScrimOpen(drawerCommon);
+				});
+
+				it('should close the popup on click in drawer container', function () {
+					drawerCommon.buttonDrawer4.click();
+					Page.waitForOpen(drawer);
+
+					expectNoneScrimOpen(drawerCommon);
+
+					Page.waitForOpen(drawer);
+					drawer.buttonOK.click();
+					Page.waitForClose(drawer);
+
+					expectClosed(drawerCommon);
+				});
+
+				it('should close the drawer on cancel click in drawer container', function () {
+					drawerCommon.buttonDrawer4.click();
+					Page.waitForOpen(drawer);
+
+					expectNoneScrimOpen(drawerCommon);
+
+					Page.waitForOpen(drawer);
+					drawer.buttonCancel.click();
+					Page.waitForClose(drawer);
+
+					expectClosed(drawerCommon);
+				});
+			});
+		});
+	});
+});
---
+++ tests/ui/specs/Drawer/DrawerPage.js
@@ -0,0 +1,93 @@
+'use strict';
+const {element, getText, hasClass, Page} = require('@enact/ui-test-utils/utils');
+
+class DrawerCommon {
+
+	get buttonDrawer1 () {
+		return element('#buttonDrawer1', browser);
+	}
+	get buttonDrawer2 () {
+		return element('#buttonDrawer2', browser);
+	}
+	get buttonDrawer3 () {
+		return element('#buttonDrawer3', browser);
+	}
+	get buttonDrawer4 () {
+		return element('#buttonDrawer4', browser);
+	}
+	get drawerLayer () {
+		return element('#floatLayer', browser);
+	}
+	get isDrawerExist () {
+		return this.drawerLayer.$('.Drawer_Drawer_drawer').isExisting();
+	}
+	get isScrimExist () {
+		return this.drawerLayer.$('.enact_ui_FloatingLayer_Scrim_scrim').isExisting();
+	}
+}
+
+class DrawerInterface {
+
+	constructor (id) {
+		this.id = id;
+	}
+
+	get self () {
+		return element(`#${this.id}`, browser);
+	}
+	get buttonOK () {
+		return element(`#${this.id} #buttonOK`, browser);
+	}
+	get buttonCancel () {
+		return element(`#${this.id} #buttonCancel`, browser);
+	}
+	get drawer () {
+		return element(`#${this.id}`, browser);
+	}
+	get title () {
+		return getText(element(`#${this.id}>div>h5`, browser));
+	}
+	get isOpen () {
+		return $(`.enact_ui_Transition_Transition_shown #${this.id}`).isExisting();
+	}
+	get hasLine () {
+		return hasClass('Heading_Heading_showLine', this.self);
+	}
+}
+
+class DrawerPage extends Page {
+
+	constructor () {
+		super();
+		this.title = 'Drawer Test';
+
+		this.components = {};
+		this.drawerCommon = new DrawerCommon('drawerMain');
+		this.components.drawer1 = new DrawerInterface('drawer1');
+		this.components.drawer2 = new DrawerInterface('drawer2');
+		this.components.drawer3 = new DrawerInterface('drawer3');
+		this.components.drawer4 = new DrawerInterface('drawer4');
+	}
+
+	open (urlExtra) {
+		super.open('Drawer-View', urlExtra);
+	}
+
+	clickPopupFloatLayer () {
+		$('#floatLayer').click();
+	}
+
+	waitForOpen (selector, timeout) {
+		if (typeof selector !== 'string') selector = `#${selector.id}`;
+
+		$(selector).waitForExist({timeout});
+	}
+
+	waitForClose (selector, timeout) {
+		if (typeof selector !== 'string') selector = `#${selector.id}`;
+
+		$(selector).waitForExist({timeout, reverse: true});
+	}
+}
+
+module.exports = new DrawerPage();
