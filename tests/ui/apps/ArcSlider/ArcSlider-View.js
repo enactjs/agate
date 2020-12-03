13010e891ccc9768fcb3206b7719584f5772b89c
Daniel Stoian <daniel.stoian@lgepartner.com>
2020-12-03 16:18:15 +0200

    small fix

--- tests/ui/apps/ArcSlider/ArcSlider-View.js
+++ tests/ui/apps/ArcSlider/ArcSlider-View.js
@@ -4,7 +4,7 @@ import Scroller from '../../../../Scroller';
 import ThemeDecorator from '../../../../ThemeDecorator';
 import React from 'react';
 import spotlight from '@enact/spotlight';
-import {scaleToRem} from "@enact/ui/resolution";
+import {scaleToRem} from '@enact/ui/resolution';
 
 // NOTE: Forcing pointer mode off so we can be sure that regardless of webOS pointer mode the app
 // runs the same way
