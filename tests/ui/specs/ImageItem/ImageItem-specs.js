dbc0535ce2d4bc3971e9ae3ece49370a406f9a65
Adrian Cocoara <adrian.cocoara@lgepartner.com>
2020-12-04 17:34:13 +0200

    added UI tests and updated unit tests for ImageItem

---
+++ tests/ui/specs/ImageItem/ImageItem-specs.js
@@ -0,0 +1,99 @@
+const Page = require('./ImageItemPage');
+
+describe('ImageItem', function () {
+	beforeEach(function () {
+		Page.open();
+	});
+
+	it('should have focus on first ImageItem at start', function () {
+		expect(Page.components.imageItemDefault.self.isFocused()).to.be.true();
+	});
+
+	describe('default', function () {
+		const imageItem = Page.components.imageItemDefault;
+
+		it('should have correct text', function () {
+			expect(imageItem.textContent).to.equal('Image Item caption');
+		});
+
+		it('should display an image', function () {
+			expect(imageItem.image).to.be.true();
+		});
+
+		describe('5-way', function () {
+			it('should be able to focus the image item', function () {
+				Page.components.imageItemLongCaption.focus();
+				Page.spotlightUp();
+
+				expect(imageItem.self.isFocused()).to.be.true();
+			});
+		});
+	});
+
+	describe('long caption', function () {
+		const imageItem = Page.components.imageItemLongCaption;
+
+		it('should wrap caption with Marquee with long caption', function () {
+			expect(imageItem.valueText).to.equal('Image Item with longer caption has Marquee applied');
+		});
+
+		it('should display an image', function () {
+			expect(imageItem.image).to.be.true();
+		});
+
+		describe('5-way', function () {
+			it('should be able to focus the image item', function () {
+				Page.components.imageItemDefault.focus();
+				Page.spotlightDown();
+
+				expect(imageItem.self.isFocused()).to.be.true();
+			});
+		});
+	});
+
+	describe('caption overlay', function () {
+		const imageItem = Page.components.imageItemCaptionOverlay;
+
+		it('should display correct text', function () {
+			expect(imageItem.textContent).to.equal('Image Item caption overlay');
+		});
+
+		it('should display caption overlay', function () {
+			expect(imageItem.isOverlay).to.be.true();
+		});
+
+		it('should display an image', function () {
+			expect(imageItem.image).to.be.true();
+		});
+
+		describe('5-way', function () {
+			it('should be able to focus the image item', function () {
+				Page.components.imageItemLongCaption.focus();
+				Page.spotlightDown();
+
+				expect(imageItem.self.isFocused()).to.be.true();
+			});
+		});
+	});
+
+	describe('disabled', function () {
+		const imageItem = Page.components.imageItemDisabled;
+
+		it('should display correct text', function () {
+			expect(imageItem.textContent).to.equal('Image Item disabled');
+		});
+
+		it('should display an image', function () {
+			expect(imageItem.image).to.be.true();
+		});
+
+		describe('5-way', function () {
+			it('should be able to focus the image item', function () {
+				Page.components.imageItemCaptionOverlay.focus();
+				Page.spotlightDown();
+
+				expect(imageItem.self.isFocused()).to.be.true();
+			});
+		});
+	});
+});
