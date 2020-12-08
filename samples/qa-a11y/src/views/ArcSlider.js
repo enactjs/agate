a8e65039500df5f53999dd5280cd1257181c8831
YunBum Sung <yb.sung@lge.com>
2020-12-08 15:34:12 +0900

    Fix
    Enact-DCO-1.0-Signed-off-by: YB Sung (yb.sung@lge.com)

--- samples/qa-a11y/src/views/ArcSlider.js
+++ samples/qa-a11y/src/views/ArcSlider.js
@@ -11,17 +11,15 @@ const ArcSliderView = () => (
 	<>
 		<Section title="Default" vertical>
 			<ArcSlider alt="Normal" />
-			<ArcSlider alt="With slotCenter" slotCenter="Text" />
+			<div />
 			<ArcSlider alt="Disabled" disabled />
-			<ArcSlider alt="Disabled with slotCenter" disabled slotCenter="Text" />
 			<Button icon="arrowdown" />
 		</Section>
 
 		<Section title="Aria-ValueText" vertical>
 			<AriaValueTextSlider alt="Aria-valuetext" aria-valuetext="This is a Label." />
-			<AriaValueTextSlider alt="Aria-valuetext with slotCenter" aria-valuetext="This is a Label." slotCenter="Text" />
+			<div />
 			<AriaValueTextSlider alt="Aria-valuetext and Disabled" aria-valuetext="This is a Label." disabled />
-			<AriaValueTextSlider alt="Aria-valuetext and Disabled with slotCenter" aria-valuetext="This is a Label." disabled slotCenter="Text" />
 			<Button icon="arrowup" />
 		</Section>
 	</>
