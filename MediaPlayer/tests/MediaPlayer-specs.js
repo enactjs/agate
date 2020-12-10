b8620e638ac9a9e4c318fa85509c028ccbd2e490
Stanca Pop <stanca.pop@lgepartner.com>
2020-12-10 17:22:36 +0200

    ordered imports

--- MediaPlayer/tests/MediaPlayer-specs.js
+++ MediaPlayer/tests/MediaPlayer-specs.js
@@ -1,5 +1,5 @@
-import React from 'react';
 import {mount} from 'enzyme';
+import React from 'react';
 
 import MediaPlayer from '../MediaPlayer';
 import css from '../../Slider/Slider.module.less';
