9bfc3c367c02d35d6f3921a977f50018035bdad5
Daniel Stoian <daniel.stoian@lgepartner.com>
2020-12-10 12:55:28 +0200

    added unit tests for ColorPicker

--- ColorPicker/tests/ColorPicker-specs.js
+++ ColorPicker/tests/ColorPicker-specs.js
@@ -2,12 +2,16 @@ import React from 'react';
 import {mount} from 'enzyme';
 
 import ColorPicker from '../ColorPicker';
-import css from '../ColorPicker.module.less';
+
+const activate = (slider) => slider.simulate('keyup', {keyCode: 13});
+const keyDown = (keyCode) => (slider) => slider.simulate('keydown', {keyCode});
+
+const leftKeyDown = keyDown(37);
+const rightKeyDown = keyDown(39);
 
 describe('ColorPicker', () => {
-
 	test(
-		'should emit an onChange event when selecting a different color',
+		'should change value when selecting a different color',
 		() => {
 			const handleChange = jest.fn();
 			const subject = mount(
@@ -19,13 +23,13 @@ describe('ColorPicker', () => {
 				</ColorPicker>
 			);
 
-			const base = subject.find('ColorPicker').first();
+			// first extend color picker
+			subject.find('SwatchButton').simulate('click');
+			// now click on blue color. it is the third SwatchButton in the component now
+			subject.find('SwatchButton').at(2).simulate('click');
 
-			base.prop('onChange')({value: 'blue'});
-
-			const expected = 1;
-			const actual = handleChange.mock.calls.length;
-
+			const expected = 'blue';
+			const actual = handleChange.mock.calls[0][0].value;
 			expect(actual).toBe(expected);
 		}
 	);
@@ -43,16 +47,92 @@ describe('ColorPicker', () => {
 				</ColorPicker>
 			);
 
-			const base = subject.find(`div.${css.sliders}`).exists();
+			// first extend color picker
+			subject.find('SwatchButton').simulate('click');
+			// click on the "More" button
+			subject.find('Button[aria-label=\'More\']').simulate('click');
+			// focus slider and move it with right key
+			activate(subject.find('Cell[aria-label=\'Hue\'] Slider'));
+			rightKeyDown(subject.find('Cell[aria-label=\'Hue\'] Slider'));
 
-			console.log(base);
+			const expected = 1;
+			const actual = handleChange.mock.calls.length;
+			expect(actual).toBe(expected);
+		}
+	);
 
-			// base.prop('onChange')({value: 30});
-			//
-			// const expected = 1;
-			// const actual = handleChange.mock.calls.length;
-			//
-			// expect(actual).toBe(expected);
+	test(
+		'should emit an onChange event when changing saturation',
+		() => {
+			const handleChange = jest.fn();
+			const subject = mount(
+				<ColorPicker
+					onChange={handleChange}
+					value="pink"
+				>
+					{['red', 'blue', 'yellow', 'pink']}
+				</ColorPicker>
+			);
+
+			// first extend color picker
+			subject.find('SwatchButton').simulate('click');
+			// click on the "More" button
+			subject.find('Button[aria-label=\'More\']').simulate('click');
+			// focus slider and move it with left key
+			activate(subject.find('Cell[aria-label=\'Saturation\'] Slider'));
+			leftKeyDown(subject.find('Cell[aria-label=\'Saturation\'] Slider'));
+
+			const expected = 1;
+			const actual = handleChange.mock.calls.length;
+			expect(actual).toBe(expected);
+		}
+	);
+
+	test(
+		'should emit an onChange event when changing lightness',
+		() => {
+			const handleChange = jest.fn();
+			const subject = mount(
+				<ColorPicker
+					onChange={handleChange}
+					value="pink"
+				>
+					{['red', 'blue', 'yellow', 'pink']}
+				</ColorPicker>
+			);
+
+			// first extend color picker
+			subject.find('SwatchButton').simulate('click');
+			// click on the "More" button
+			subject.find('Button[aria-label=\'More\']').simulate('click');
+			// focus slider and move it with right key
+			activate(subject.find('Cell[aria-label=\'Lightness\'] Slider'));
+			rightKeyDown(subject.find('Cell[aria-label=\'Lightness\'] Slider'));
+
+			const expected = 1;
+			const actual = handleChange.mock.calls.length;
+			expect(actual).toBe(expected);
+		}
+	);
+
+	test(
+		'should not extend palette when disabled',
+		() => {
+			const handleChange = jest.fn();
+			const subject = mount(
+				<ColorPicker
+					disabled
+					onChange={handleChange}
+					value="pink"
+				>
+					{['red', 'blue', 'yellow', 'pink']}
+				</ColorPicker>
+			);
+
+			// extend color picker
+			subject.find('SwatchButton').simulate('click');
+			// there is only one SwatchButton. It means the palette is not extended
+			expect(subject.find('SwatchButton').length).toBe(1);
 		}
 	);
 });
