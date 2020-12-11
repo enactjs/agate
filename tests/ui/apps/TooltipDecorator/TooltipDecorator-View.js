0b0960d6f0921ef413e14d3c63e57941e73fc965
Stanca Pop <stanca.pop@lgepartner.com>
2020-12-11 16:59:03 +0200

    fixes after review

--- tests/ui/apps/TooltipDecorator/TooltipDecorator-View.js
+++ tests/ui/apps/TooltipDecorator/TooltipDecorator-View.js
@@ -1,8 +1,9 @@
 import Button from '../../../../Button';
 import ThemeDecorator from '../../../../ThemeDecorator';
 import TooltipDecorator from '../../../../TooltipDecorator';
-import React, {Component} from 'react';
+import React from 'react';
 import spotlight from '@enact/spotlight';
+import {scaleToRem} from '@enact/ui/resolution';
 
 const TooltipButton = TooltipDecorator({tooltipDestinationProp: 'decoration'}, Button);
 
@@ -10,38 +11,32 @@ const TooltipButton = TooltipDecorator({tooltipDestinationProp: 'decoration'}, B
 // runs the same way
 spotlight.setPointerMode(false);
 
-class app extends Component {
-
-	render () {
-		return (
-			<div style={{padding: '40px'}}>
-				<TooltipButton
-					id="tooltipButton1"
-					tooltipPosition="right middle"
-					tooltipText="Hello Tooltip Button 1"
-				>
-					Tooltip Button 1
-				</TooltipButton>
-				<TooltipButton
-					id="tooltipButton2"
-					backgroundOpacity="transparent"
-					selected
-					tooltipText="Hello Tooltip Button 2"
-					tooltipDelay={1000}
-				>
-					Tooltip Button 2
-				</TooltipButton>
-				<TooltipButton
-					id="tooltipButton3"
-					disabled
-					size="small"
-					tooltipText="Hello Tooltip Button 3"
-				>
-					Tooltip Button 3
-				</TooltipButton>
-			</div>
-		);
-	}
-}
+const app = (props) =>
+	<div {...props} style={{padding: scaleToRem(40)}}>
+		<TooltipButton
+			id="tooltipButton1"
+			tooltipPosition="right middle"
+			tooltipText="Hello Tooltip Button 1"
+		>
+			Tooltip Button 1
+		</TooltipButton>
+		<TooltipButton
+			id="tooltipButton2"
+			backgroundOpacity="transparent"
+			selected
+			tooltipText="Hello Tooltip Button 2"
+			tooltipDelay={1000}
+		>
+			Tooltip Button 2
+		</TooltipButton>
+		<TooltipButton
+			id="tooltipButton3"
+			disabled
+			size="small"
+			tooltipText="Hello Tooltip Button 3"
+		>
+			Tooltip Button 3
+		</TooltipButton>
+	</div>;
 
 export default ThemeDecorator(app);
