b9387c1490f7a95a74e398995742bd612b0488d8
Daniel Stoian <daniel.stoian@lgepartner.com>
2020-12-16 19:36:12 +0200

    added ui tests for virtual list

---
+++ tests/ui/specs/VirtualList/VirtualList-specs.js
@@ -0,0 +1,297 @@
+const Page = require('./VirtualListPage'),
+	{expectFocusedItem, expectNoFocusedItem, waitUntilFocused} = require('./VirtualList-utils');
+
+describe('VirtualList', function () {
+	beforeEach(function () {
+		Page.open();
+	});
+
+	// it('should meet initial conditions', function () {
+	// 	expect(Page.buttonHideScrollbar.isFocused()).to.be.true();
+	// });
+
+	describe('LTR locale', function () {
+		// it('should focus first item on first focus', function () {
+		// 	Page.buttonLeft.moveTo();
+		// 	Page.spotlightRight();
+		// 	expectFocusedItem(0);
+		// });
+		//
+		// it('should position Scrollbar Track on right side in LTR', function () {
+		// 	expect(Page.getListRect().right < Page.getVerticalScrollbarRect().right).to.be.true();
+		// });
+		//
+		// it('should position Scroll thumb on top/bottom when reaching to the edge with 5-way and Page Down', function () {
+		// 	// set dataSize to 30
+		// 	Page.inputfieldNumItems.moveTo();
+		// 	Page.spotlightSelect();
+		// 	Page.backSpace();
+		// 	Page.backSpace();
+		// 	Page.backSpace();
+		// 	Page.numPad(3);
+		// 	Page.numPad(0);
+		// 	Page.backKey();
+		// 	Page.spotlightDown();
+		// 	// Move focus to the first item ('Item 00').
+		// 	Page.buttonLeft.moveTo();
+		// 	Page.spotlightRight();
+		// 	// Verify Spotlight displays on the first item.
+		// 	expectFocusedItem(0, 'focus Item 0');
+		// 	// Verify Scroll thumb's position appears shortly at the top of the Scrollbar track.
+		// 	expect(Page.getScrollThumbPosition(), 'Up').to.be.equal('0');
+		// 	// Press Page Down.
+		// 	Page.pageDown();
+		// 	// Verify Spotlight hides.
+		// 	expectNoFocusedItem();
+		// 	waitUntilFocused(4, 'focus Item 4');
+		// 	// Press Page Down.
+		// 	Page.pageDown();
+		// 	// Verify Spotlight hides.
+		// 	expectNoFocusedItem();
+		// 	waitUntilFocused(8, 'focus Item 8');
+		// 	// 5-way Down several times to scroll down the list.
+		// 	Page.fiveWayToItem(20);
+		// 	// 5-way Spot the last item.
+		// 	Page.fiveWayToItem(29);
+		// 	// Verify Spotlight displays on the last item.
+		// 	waitUntilFocused(29, 'focus last Item');
+		// 	Page.delay(1000);
+		// 	// Verify Scroll thumb's position appears shortly at the bottom of the Scrollbar track.
+		// 	expect(Page.getScrollThumbPosition(), 'Down').to.be.equal('1');
+		// 	// 5-way Spot the first item.
+		// 	Page.pageUp();
+		// 	waitUntilFocused(25, 'focus Item 23');
+		// 	Page.pageUp();
+		// 	waitUntilFocused(21, 'focus Item 21');
+		// 	Page.pageUp();
+		// 	waitUntilFocused(17, 'focus Item 17');
+		// 	Page.pageUp();
+		// 	waitUntilFocused(13, 'focus Item 13');
+		// 	Page.pageUp();
+		// 	waitUntilFocused(9, 'focus Item 9');
+		// 	Page.pageUp();
+		// 	waitUntilFocused(5, 'focus Item 5');
+		// 	Page.pageUp();
+		// 	// Verify Spotlight displays on the first item.
+		// 	waitUntilFocused(0, 'focus Item 0');
+		// 	// Verify Scroll thumb's position appears shortly at the top of the Scrollbar track.
+		// 	expect(Page.getScrollThumbPosition(), 'Up').to.be.equal('0');
+		// });
+		//
+		// it('should Items Animate via 5-way Up and Down on Last Item on the page - vertical', function () {
+		// 	let bottomId;
+		// 	Page.buttonLeft.moveTo();
+		// 	Page.spotlightRight(); // needed to focus Item 00 and get into that container
+		// 	// Position the pointer on the last item in a current page.
+		// 	bottomId = Page.bottomVisibleItemId();
+		// 	Page.item(bottomId).moveTo();
+		// 	// Verify Spotlight displays on the item.
+		// 	Page.delay(1000); // needed to run on mpc
+		// 	expectFocusedItem(Number((Page.bottomVisibleItemId().slice(4))), 'focus bottomId');
+		// 	// 5-way Down to the item below the last item on the current page.
+		// 	Page.spotlightDown();
+		// 	// Verify The list Scrolled Up.  2 The Spotted item is placed on the Bottom.
+		// 	Page.delay(1000); // needed to run on mpc
+		// 	expectFocusedItem(Number((Page.bottomVisibleItemId().slice(4))), 'focus bottomId');
+		// 	// 5-way Up to the previous item.
+		// 	Page.spotlightUp();
+		// 	// Verify The list *does not* Scroll Down.
+		// 	// Check the bottomVisibleItem is still the same as the one before 5-way Up to check the list did not scroll Down
+		// 	expect(Number(bottomId.slice(4)) === ((Number((Page.bottomVisibleItemId().slice(4))))) - 1).to.be.true();
+		// 	// Verify  The Spotted item is placed above the item on the Bottom.
+		// 	expectFocusedItem(Number((bottomId.slice(4))), 'focus bottomId');
+		// 	// 5-way Up to the first item ('*Item 000*').
+		// 	Page.fiveWayToItem(0);
+		// 	// Verify The list Scroll Down  and the spotted item is placed on the Top.
+		// 	expectFocusedItem(Number((Page.topVisibleItemId().slice(4))), 'focus Item 00');
+		// 	expectFocusedItem(0, 'focus Item 00');  // to double check it is really top item
+		// });
+
+		// it('Navigate between inside and outside of a list', function () {
+		// 	// 5-way Down to the 10th item.
+		// 	Page.buttonLeft.moveTo();
+		// 	Page.spotlightRight();
+		// 	Page.fiveWayToItem(10);
+		// 	// 5-way Spot Item 00.
+		// 	Page.fiveWayToItem(0);
+		// 	// Verify: Spotlight displays on Item 00.
+		// 	expectFocusedItem(0);
+		// 	// 5-way Up. Verify Spotlight displays on the Top button.
+		// 	Page.spotlightUp();
+		// 	expect(Page.buttonTop.isFocused()).to.be.true();
+		// });
+		//
+		// it('should focus last item when entering from outside after scrolling via 5way, function () {
+		// 	// Move focus to the first item ('Item 00').
+		// 	Page.buttonLeft.moveTo();
+		// 	Page.spotlightRight();
+		//
+		// 	// 5-way Down to the 21st item ('Item 20').
+		// 	Page.fiveWayToItem(20);
+		//
+		// 	// Position the pointer on 'JumpToItem' button and select
+		// 	Page.buttonTop.moveTo();
+		// 	Page.spotlightSelect();
+		//
+		// 	// Move focus to the list
+		// 	Page.buttonLeft.moveTo();
+		// 	Page.spotlightRight();
+		//
+		// 	// Verify Spotlight displays on the 21st item ('Item 20');
+		// 	expectFocusedItem(20);
+		// });
+		//
+		// it('should  display childProps', function () {
+		// 	// Set translate ScrollMode.
+		// 	//Page.buttonNativeScroll.moveTo();
+		// 	//Page.spotlightSelect();
+		// 	// Verify 1-2:The first item shows 'Item 000'.
+		// 	Page.buttonLeft.moveTo();
+		// 	Page.spotlightRight();
+		// 	expectFocusedItem(0);
+		// 	expect(Page.textContent()).to.equal('Item 00');
+		// 	// Verify 1-3: The second item shows 'Item 001'.
+		// 	Page.spotlightDown();
+		// 	expectFocusedItem(1);
+		// 	expect(Page.textContent()).to.equal('Item 01');
+		// 	// 5-way Spot and Select 'Child Props' toggle button.
+		// 	Page.buttonChildProps.click();
+		// 	// Verify: The first item shows 'Item 000 child props'.
+		// 	Page.item(0).moveTo();
+		// 	expectFocusedItem(0);
+		// 	expect(Page.textContent()).to.equal('Item 00 child props');
+		// 	// Verify The second item shows 'Item 001 child props'.
+		// 	Page.spotlightDown();
+		// 	expectFocusedItem(1);
+		// 	expect(Page.textContent()).to.equal('Item 01 child props');
+		// });
+	 });
+
+	describe('Disabled item', function () {
+		// it('should navigate disabled and enabled items', function () {
+		// 	// Uncheck the "Native Scrolling" Checkbox item.
+		// 	// Set translate ScrollMode.
+		// 	Page.buttonNativeScroll.moveTo();
+		// 	Page.spotlightSelect();
+		// 	// Click on DisabledItems CheckboxItem.(DisabledItem Button for this test.)
+		// 	Page.buttonDisabledItem.moveTo();
+		// 	Page.spotlightSelect();
+		// 	// 5-way Spot the first item ('Item 000').
+		// 	Page.buttonLeft.moveTo();
+		// 	Page.spotlightRight();
+		// 	expectFocusedItem(0);
+		// 	// 5-way Down several times to the next enabled item.
+		// 	// Verify: Spotlight displays on each Item (Disabled and Enabled) as the list scrolls up.
+		// 	let index = 1;
+		// 	for (; index < 15; index++) {
+		// 		Page.fiveWayToItem(index);
+		// 		expect(Page.itemDisabled()).to.be.true();
+		// 	}
+		// 	Page.fiveWayToItem(index);
+		// 	expect(Page.itemDisabled()).to.be.false();
+		// 	// Spotlight displays on the next Enabled item.
+		// 	expectFocusedItem(15);
+		// 	// 5-way Up several times to the previous enabled item.
+		// 	// Spotlight displays on each Item (Disabled and Enabled) as the list scrolls down.
+		// 	for (index = 14; index > 0; index--) {
+		// 		Page.fiveWayToItem(index);
+		// 		expect(Page.itemDisabled()).to.be.true();
+		// 	}
+		// 	Page.fiveWayToItem(index);
+		// 	expect(Page.itemDisabled()).to.be.false();
+		// 	// Spotlight displays on the previous Enabled item.
+		// 	expectFocusedItem(0);
+		// });
+		//
+		// it('should navigate disabled and enabled items with native scrollmode [GT-29035]', function () {
+		// 	// Toggle on DisabledItems.(DisabledItem Button for this test.)
+		// 	Page.buttonDisabledItem.moveTo();
+		// 	Page.spotlightSelect();
+		// 	// 5-way Spot the first item ('Item 000').
+		// 	Page.buttonLeft.moveTo();
+		// 	Page.spotlightRight();
+		// 	expectFocusedItem(0);
+		// 	// 5-way Down several times to the next enabled item.
+		// 	let index = 1;
+		// 	for (; index < 15; index++) {
+		// 		Page.fiveWayToItem(index);
+		// 		expect(Page.itemDisabled()).to.be.true();
+		// 	}
+		// 	Page.fiveWayToItem(index);
+		// 	expect(Page.itemDisabled()).to.be.false();
+		// 	// Spotlight displays on the next Enabled item.
+		// 	expectFocusedItem(15);
+		// 	// 5-way Up several times.
+		// 	for (index = 14; index > 0; index--) {
+		// 		Page.fiveWayToItem(index);
+		// 		expect(Page.itemDisabled()).to.be.true();
+		// 	}
+		// 	Page.fiveWayToItem(index);
+		// 	expect(Page.itemDisabled()).to.be.false();
+		// 	// Spotlight displays on the previous Enabled item.
+		// 	expectFocusedItem(0);
+		// });
+
+		// it('should change spotlight size when item`s size changing', function () {
+		// 	// Verify: The default value for the 'itemSize' knob is itemSizeValue.
+		// 	const defaultItemSize = Page.getItemSize();
+		// 	Page.buttonLeft.moveTo();
+		// 	Page.spotlightRight();
+		// 	const defaultSpotlightSize = Page.spotlightSize();
+		// 	expect(defaultSpotlightSize).to.equal(94);
+		// 	// Set item size to 150
+		// 	Page.inputfieldItemSize.moveTo();
+		// 	Page.spotlightSelect();
+		// 	Page.backSpace();
+		// 	Page.backSpace();
+		// 	Page.backSpace();
+		// 	Page.numPad(1);
+		// 	Page.numPad(5);
+		// 	Page.numPad(0);
+		// 	Page.backKey();
+		// 	// Verify item size
+		// 	const curItemSize = Page.getItemSize();
+		// 	expect(curItemSize.height).to.equal(150);
+		// 	expect(curItemSize.width).to.equal(defaultItemSize.width);
+		// 	Page.spotlightDown();
+		// 	Page.item(2).moveTo();
+		// 	expectFocusedItem(2);
+		// 	const curSpotlightSize = Page.spotlightSize();
+		// 	// spotLight size is ItemSize - 2*border-witdth
+		// 	expect(curSpotlightSize).to.equal(144);
+		// 	// Set item size to 50
+		// 	Page.inputfieldItemSize.moveTo();
+		// 	Page.spotlightSelect();
+		// 	Page.backSpace();
+		// 	Page.backSpace();
+		// 	Page.backSpace();
+		// 	Page.numPad(5);
+		// 	Page.numPad(0);
+		// 	Page.backKey();
+		// 	// Verify item size
+		// 	const newItemSize = Page.getItemSize();
+		// 	expect(newItemSize.height).to.equal(50);
+		// 	expect(newItemSize.width).to.equal(defaultItemSize.width);
+		// 	Page.spotlightDown();
+		// 	Page.item(3).moveTo();
+		// 	expectFocusedItem(3);
+		// 	const newSpotlightSize = Page.spotlightSize();
+		// 	expect(newSpotlightSize).to.equal(44);
+		// });
+
+		// describe('Scrollbar Size', function () {
+		// 	it('should have 96px padding for the top and bottom of the scrollbarTrack', function () {
+		// 		// Verify: The scrollbar size fit to the size of the list.
+		// 		expect(Page.getListRect().height).to.equal(Page.getVerticalScrollbarRect().height);
+		// 		// There is a button with size=small at the top and bottom
+		// 		// Check top
+		// 		expect(Page.getVerticalScrollbarTrackRect().top - Page.getVerticalScrollbarRect().top).to.equal(48);
+		// 		// Check bottom
+		// 		expect(Page.getVerticalScrollbarRect().bottom - Page.getVerticalScrollbarTrackRect().bottom).to.equal(48);
+		// 	});
+		// });
+
+	});
+
+
+});
---
+++ tests/ui/specs/VirtualList/Wrap/VirtualListWrap-specs.js
@@ -0,0 +1,68 @@
+const Page = require('../VirtualListPage');
+const {expectFocusedItem, waitUntilFocused} = require('../VirtualList-utils');
+
+describe('Wrap', function () {
+	beforeEach(function () {
+		Page.open();
+	});
+
+	it('should wrap when', function () {
+		// Set wrap to true
+		Page.buttonWrap.moveTo();
+		Page.spotlightSelect();
+		// 5-way Spot the first item.
+		Page.buttonLeft.moveTo();
+		Page.spotlightRight();
+		expectFocusedItem(0, 'focus first item');
+		// 5-Way Up to move focus to the last item in the list.
+		Page.spotlightUp();
+		Page.delay(1500);  // TODO: Need better way to detect scroll end
+		// Verify: Spotlight displays on the last item.
+		expectFocusedItem(99, 'focus last item before first item');
+		// 5-Way Down to move focus to the first item in the list.
+		Page.spotlightDown();
+		Page.delay(1500);  // TODO: Need better way to detect scroll end
+		// Verify: Spotlight displays on the first item.
+		expectFocusedItem(0, 'focus first item after last item');
+	});
+
+	it('should not scroll when leaving list with 5-way up/down', function () {
+		// set dataSize to 10 for Speed up Test.
+		Page.inputfieldNumItems.moveTo();
+		Page.spotlightSelect();
+		Page.backSpace();
+		Page.backSpace();
+		Page.backSpace();
+		Page.numPad(1);
+		Page.numPad(0);
+		Page.backKey();
+		Page.spotlightDown();
+		// change to 5-way mode
+		Page.buttonLeft.moveTo();
+		// 5-way Spot the first item.
+		Page.spotlightRight();
+		// Verify: Spotlight displays on the first item.
+		expectFocusedItem(0, 'step 5 focus');
+		// Step 6: 5-way Up.
+		Page.spotlightUp();
+		// Verify: The list *does not* Scroll to the Bottom. 2. Spotlight is on the close button 'x'.
+		expect(Page.buttonTop.isFocused(), 'step 6 focus').to.be.true();  // buttonTop replaces the X button
+		Page.spotlightDown();
+		expectFocusedItem(0);
+		Page.pageDown();
+		waitUntilFocused(4, 'focus Item 4');
+		Page.fiveWayToItem(9);
+		// Step 7: 2. Click the last item.
+		Page.spotlightSelect();
+		// Verify Step 7: Spotlight is on the last item.
+		Page.delay(1000);
+		expectFocusedItem(9, 'focus Item 9');
+		// Step 8: 5-way Down
+		Page.spotlightDown();
+		Page.spotlightDown(); // 1 extra 5-way down to check Spotlight does not pass buttonBottom when wrap is off.
+		Page.delay(1000);
+		// Verify Step 8: 1. The list *does not* Scroll to the Top. 2. Spotlight stays on the last item.
+		// Checking focus is on buttonBottom instead of last item since 5-way Down on last item using this app takes Spotlight to buttonBottom.
+		expect(Page.buttonBottom.isFocused(), 'step 8 focus').to.be.true();
+	});
+});
