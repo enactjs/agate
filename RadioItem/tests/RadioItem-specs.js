9aa34eef1d2c1315d4429c2d48fd68462af2b7af
Adrian Cocoara <adrian.cocoara@lgepartner.com>
2020-12-17 16:40:21 +0200

    added more unit tests for RadioButton

--- RadioItem/tests/RadioItem-specs.js
+++ RadioItem/tests/RadioItem-specs.js
@@ -1,9 +1,24 @@
-import React from 'react';
 import {mount} from 'enzyme';
+import React from 'react';
+
 import RadioItem from '../RadioItem';
+
 import css from '../RadioItem.module.less';
 
 describe('RadioItem', () => {
+	test('should support adding text as children', () => {
+		const radioItem = mount(
+			<RadioItem>
+				Hello RadioItem
+			</RadioItem>
+		);
+
+		const expected = 'Hello RadioItem';
+		const actual = radioItem.find('ItemContent').text();
+
+		expect(actual).toBe(expected);
+	});
+
 	test('should render correct icon when not selected', () => {
 		const radioItem = mount(
 			<RadioItem>
@@ -12,7 +27,7 @@ describe('RadioItem', () => {
 		);
 
 		const expected = 0;
-		const actual = radioItem.find(`.${css.selected}`).hostNodes().length;
+		const actual = radioItem.find(`.${css.selected}`).length;
 
 		expect(actual).toBe(expected);
 	});
@@ -24,10 +39,77 @@ describe('RadioItem', () => {
 			</RadioItem>
 		);
 
-		const expected = 1;
-		const actual = radioItem.find(`.${css.selected}`).hostNodes().length;
+		const expected = 'selected';
+		const actual = radioItem.find(`div.${css.selected}`).prop('className');
+
+		expect(actual).toContain(expected);
+	});
+
+	test('should support custom icon', () => {
+		const radioItem = mount(
+			<RadioItem icon="check">
+				Hello RadioItem
+			</RadioItem>
+		);
+
+		// console.log(radioItem.debug());
+		const expected = 'check';
+		const actual = radioItem.find('Icon').prop('children');
 
 		expect(actual).toBe(expected);
 	});
 
+	test('should toggle selected prop when clicked', () => {
+		const handleToggle = jest.fn();
+		const radioItem = mount(
+			<RadioItem onToggle={handleToggle}>
+				Hello RadioItem
+			</RadioItem>
+		);
+
+		radioItem.find('RadioItemBase').simulate('click');
+
+		const expected = true;
+		const actual = handleToggle.mock.calls[0][0].selected;
+
+		expect(actual).toBe(expected);
+	});
+
+	test('should toggle selected prop to false when initiated as selected', () => {
+		const handleToggle = jest.fn();
+		const radioItem = mount(
+			<RadioItem
+				onToggle={handleToggle}
+				selected
+			>
+				Hello RadioItem
+			</RadioItem>
+		);
+
+		radioItem.find('RadioItemBase').simulate('click');
+
+		const expected = false;
+		const actual = handleToggle.mock.calls[0][0].selected;
+
+		expect(actual).toBe(expected);
+	});
+
+	test('should not toggle selected prop when initiated as disabled', () => {
+		const handleToggle = jest.fn();
+		const radioItem = mount(
+			<RadioItem
+				disabled
+				onToggle={handleToggle}
+			>
+				Hello RadioItem
+			</RadioItem>
+		);
+
+		radioItem.find('RadioItemBase').simulate('click');
+
+		const expected = 0;
+		const actual = handleToggle.mock.calls.length;
+
+		expect(actual).toBe(expected);
+	});
 });
