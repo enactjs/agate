9e7b4d95fbca1b1dfd95a62fefffe61a69d78965
Daniel Stoian <daniel.stoian@lgepartner.com>
2020-12-18 18:13:01 +0200

    added progressBarTooltip

--- ProgressBar/ProgressBar.js
+++ ProgressBar/ProgressBar.js
@@ -7,14 +7,20 @@
  * @module agate/ProgressBar
  * @exports ProgressBar
  * @exports ProgressBarBase
+ * @exports ProgressBarTooltip
  */
 
 import kind from '@enact/core/kind';
+import ComponentOverride from '@enact/ui/ComponentOverride';
+import Pure from '@enact/ui/internal/Pure';
 import UiProgressBar from '@enact/ui/ProgressBar';
+import Slottable from '@enact/ui/Slottable';
 import PropTypes from 'prop-types';
+import compose from 'ramda/src/compose';
 import React from 'react';
 
 import Skinnable from '../Skinnable';
+import {ProgressBarTooltip} from './ProgressBarTooltip';
 
 import componentCss from './ProgressBar.module.less';
 
@@ -33,6 +39,17 @@ const ProgressBarBase = kind({
 
 	propTypes: /** @lends agate/ProgressBar.ProgressBar.prototype */ {
 		/**
+		 * The proportion of the loaded portion of the progress bar.
+		 *
+		 * * Valid values are between `0` and `1`.
+		 *
+		 * @type {Number}
+		 * @default 0
+		 * @public
+		 */
+		backgroundProgress: PropTypes.number,
+
+		/**
 		 * Customizes the component by mapping the supplied collection of CSS class names to the
 		 * corresponding internal elements and states of this component.
 		 *
@@ -44,6 +61,14 @@ const ProgressBarBase = kind({
 		 * @public
 		 */
 		css: PropTypes.object,
+
+		/**
+		 * Highlights the filled portion.
+		 *
+		 * @type {Boolean}
+		 * @public
+		 */
+		highlighted: PropTypes.bool,
 
 		/**
 		 * Sets the orientation of the progress bar.
@@ -64,16 +89,64 @@ const ProgressBarBase = kind({
 		progress: PropTypes.number,
 
 		/**
+		 * Displays an anchor at `progressAnchor`.
+		 *
+		 * @type {Boolean}
+		 * @public
+		 */
+		showAnchor: PropTypes.bool,
+
+		/**
 		 * The size of the progress bar.
 		 *
 		 * @type {('small'|'large')}
 		 * @default 'large'
 		 * @public
 		 */
-		size: PropTypes.oneOf(['small', 'large'])
+		size: PropTypes.oneOf(['small', 'large']),
+
+		/**
+		 * Enables the built-in tooltip.
+		 *
+		 * To customize the tooltip, pass either a custom tooltip component or an instance of
+		 * [ProgressBarTooltip]{@link agate/ProgressBar.ProgressBarTooltip} with additional
+		 * props configured.
+		 *
+		 * The provided component will receive the following props from `ProgressBar`:
+		 *
+		 * * `orientation`  - The value of `orientation`
+		 * * `percent`      - Always `true` indicating the value should be presented as a percentage
+		 *                    rather than an absolute value
+		 * * `progress`     - The `value` as a proportion between `min` and `max`
+		 * * `visible`      - Always `true` indicating that the tooltip should be visible
+		 *
+		 * Usage:
+		 * ```
+		 * <ProgressBar
+		 *   tooltip={
+		 *     <ProgressBarTooltip position="after" />
+		 *   }
+		 * />
+		 * ```
+		 *
+		 * The tooltip may also be passed as a child via the `"tooltip"` slot. See
+		 * [Slottable]{@link ui/Slottable} for more information on how slots can be used.
+		 *
+		 * Usage:
+		 * ```
+		 * <ProgressBar>
+		 *   <ProgressBarTooltip position="after" />
+		 * </ProgressBar>
+		 * ```
+		 *
+		 * @type {Boolean|Component|Element}
+		 * @public
+		 */
+		tooltip: PropTypes.oneOfType([PropTypes.bool, PropTypes.element, PropTypes.func])
 	},
 
 	defaultProps: {
+		backgroundProgress: 0,
 		orientation: 'horizontal',
 		progress: 0,
 		size: 'large'
@@ -85,26 +158,69 @@ const ProgressBarBase = kind({
 	},
 
 	computed: {
-		className: ({size, styler}) => styler.append(size)
+		className: ({highlighted, showAnchor, size, styler}) => styler.append({
+			highlighted,
+			showAnchor,
+			size
+		}),
+		tooltip: ({tooltip}) => tooltip === true ? ProgressBarTooltip : tooltip
 	},
 
-	render: ({css, ...rest}) => {
+	render: ({css, orientation, progress, tooltip, ...rest}) => {
+		delete rest.tooltip;
+		delete rest.highlighted;
+		delete rest.showAnchor;
 		delete rest.size;
 
 		return (
 			<UiProgressBar
 				{...rest}
+				orientation={orientation}
+				progress={progress}
 				css={css}
-			/>
+			>
+				<ComponentOverride
+					component={tooltip}
+					orientation={orientation}
+					percent
+					proportion={progress}
+					visible
+				/>
+			</UiProgressBar>
 		);
 	}
 });
 
-// Not documenting as base is not very useful to developers.
-const ProgressBar = Skinnable(ProgressBarBase);
+/**
+ * Agate-specific behaviors to apply to [ProgressBar]{@link agate/ProgressBar.ProgressBarBase}.
+ *
+ * @hoc
+ * @memberof agate/ProgressBar
+ * @mixes agate/Skinnable.Skinnable
+ * @public
+ */
+const ProgressBarDecorator = compose(
+	Pure,
+	Slottable({slots: ['tooltip']}),
+	Skinnable
+);
+
+/**
+ * The ready-to-use Agate-styled ProgressBar.
+ *
+ * @class ProgressBar
+ * @memberof agate/ProgressBar
+ * @extends agate/ProgressBar.ProgressBarBase
+ * @mixes agate/ProgressBar.ProgressBarDecorator
+ * @ui
+ * @public
+ */
+const ProgressBar = ProgressBarDecorator(ProgressBarBase);
 
 export default ProgressBar;
 export {
 	ProgressBar,
-	ProgressBarBase
+	ProgressBarBase,
+	ProgressBarDecorator,
+	ProgressBarTooltip
 };
