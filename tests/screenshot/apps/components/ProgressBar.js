e642e07669c366d8a4587fd42c01cd79ba71d712
Daniel Stoian <daniel.stoian@lgepartner.com>
2020-12-22 15:42:59 +0200

    updated ss-test

--- tests/screenshot/apps/components/ProgressBar.js
+++ tests/screenshot/apps/components/ProgressBar.js
@@ -1,12 +1,14 @@
-import Heading from '../../../../Heading';
-import ProgressBar, {ProgressBarTooltip} from '../../../../ProgressBar';
+import ProgressBar from '../../../../ProgressBar';
 import React from 'react';
 
 const ProgressBarTests = [
 	<ProgressBar />,
 	<ProgressBar orientation="vertical" />,
 	<ProgressBar orientation="radial" />,
+	<ProgressBar progress={0.75} highlighted />,
 	<ProgressBar progress={0.75} size="small" />,
+	<ProgressBar progress={0.75} disabled />,
+	<ProgressBar progress={0.75} progressAnchor={0.5} showAnchor />
 ];
 
 export default ProgressBarTests;
