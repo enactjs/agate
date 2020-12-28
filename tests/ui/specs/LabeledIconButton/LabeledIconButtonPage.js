a4bbec0d68205289bedd22b44420c7544a6c82de
adrian-cocoara-lgp <63335823+adrian-cocoara-lgp@users.noreply.github.com>
2020-12-28 09:47:19 +0200

    PLAT-128479: Add UI tests and unit tests for LabeledIconButton (#447)
    * first commit
    
    * added UI tests and unit tests for LabeledIconButton
    
    * removed unnecessary comment
    
    * code changes based on review
    
    * removed some tests
    
    * code chanes based on review
    
    Co-authored-by: Mikyung Kim <mikyung27.kim@lge.com>

---
+++ tests/ui/specs/LabeledIconButton/LabeledIconButtonPage.js
@@ -0,0 +1,83 @@
+'use strict';
+const {getSubComponent, getText, Page} = require('@enact/ui-test-utils/utils');
+
+const getMarqueeText = getSubComponent({lib: 'ui', component: 'Marquee', child: 'text'});
+
+class LabeledIconButtonInterface {
+	constructor (id) {
+		this.id = id;
+		this.selector = `#${this.id}`;
+	}
+
+	focus () {
+		return browser.execute((el) => el.focus(), $(`#${this.id}`));
+	}
+	hover () {
+		return $(this.selector).moveTo({xOffset: 0, yOffset: 0});
+	}
+
+	get self () {
+		return $(this.selector);
+	}
+	get valueText () {
+		return getText(getMarqueeText(this.self));
+	}
+	iconValue () {
+		return getText($(this.selector + ' .Icon_Icon_icon')).codePointAt();
+	}
+	isSelected () {
+		return $(this.selector + ' .Button_Button_selected').isExisting();
+	}
+	isHighlighted () {
+		return $(this.selector + ' .Button_Button_highlighted').isExisting();
+	}
+	isTransparent () {
+		return $(this.selector + ' .Button_Button_transparent').isExisting();
+	}
+	isSmallest () {
+		return $(this.selector + ' .Button_Button_smallest').isExisting();
+	}
+	isSmall () {
+		return $(this.selector + ' .Button_Button_small').isExisting();
+	}
+	isHuge () {
+		return $(this.selector + ' .Button_Button_huge').isExisting();
+	}
+	isDisabled () {
+		return $(this.selector + ' .Button_Button_button').getAttribute('aria-disabled');
+	}
+}
+
+class LabeledIconButtonPage extends Page {
+	constructor () {
+		super();
+		this.title = 'LabeledIconButton Test';
+		const LabeledIconButtonDefault = new LabeledIconButtonInterface('labeledIconButtonDefault');
+		const LabeledIconButtonCustom = new LabeledIconButtonInterface('labeledIconButtonCustomIcon');
+		const LabeledIconButtonSelected = new LabeledIconButtonInterface('labeledIconButtonSelected');
+		const LabeledIconButtonHighlighted = new LabeledIconButtonInterface('labeledIconButtonHighlighted');
+		const LabeledIconButtonTransparent = new LabeledIconButtonInterface('labeledIconButtonTransparent');
+		const LabeledIconButtonSmallest = new LabeledIconButtonInterface('labeledIconButtonSmallest');
+		const LabeledIconButtonSmall = new LabeledIconButtonInterface('labeledIconButtonSmall');
+		const LabeledIconButtonHuge = new LabeledIconButtonInterface('labeledIconButtonHuge');
+		const LabeledIconButtonDisabled = new LabeledIconButtonInterface('labeledIconButtonDisabled');
+
+		this.components = {
+			LabeledIconButtonDefault,
+			LabeledIconButtonCustom,
+			LabeledIconButtonSelected,
+			LabeledIconButtonHighlighted,
+			LabeledIconButtonTransparent,
+			LabeledIconButtonSmallest,
+			LabeledIconButtonSmall,
+			LabeledIconButtonHuge,
+			LabeledIconButtonDisabled
+		};
+	}
+
+	open (urlExtra) {
+		super.open('LabeledIconButton-View', urlExtra);
+	}
+}
+
+module.exports = new LabeledIconButtonPage();
