90406d43e2e350b65ac16b4edd65619b37b11e8e
YunBum Sung <yb.sung@lge.com>
2020-12-31 15:25:33 +0900

    Revert "Fix"
    This reverts commit 182e5db316d3b647f1895a88019a17ea446052a3.

--- samples/qa-a11y/src/views/ArcPicker.js
+++ samples/qa-a11y/src/views/ArcPicker.js
@@ -13,14 +13,14 @@ const ArcPickerView = () => (
 	<>
 		<Section horizontal title="Default">
 			<ArcPicker alt="Normal">{[1, 2, 3, 4, 5]}</ArcPicker>
-			<ArcPicker alt="With slotCenter" slotCenter="Text">{[1, 2, 3, 4, 5]}</ArcPicker>
+			<ArcPicker alt="With slotCenter" slotCenter="Center">{[1, 2, 3, 4, 5]}</ArcPicker>
 			<ArcPicker alt="Disabled" disabled>{[1, 2, 3, 4, 5]}</ArcPicker>
 			<Button aria-label="5 way Down to move to the next line" icon="arrowdown" />
 		</Section>
 
 		<Section horizontal title="Aria-ValueText">
 			<AriaValueTextArcPicker alt="Aria-valuetext" aria-valuetext="This is a Label.">{[1, 2, 3, 4, 5]}</AriaValueTextArcPicker>
-			<AriaValueTextArcPicker alt="Aria-valuetext with slotCenter" aria-valuetext="This is a Label." slotCenter="Text">{[1, 2, 3, 4, 5]}</AriaValueTextArcPicker>
+			<AriaValueTextArcPicker alt="Aria-valuetext with slotCenter" aria-valuetext="This is a Label." slotCenter="Center">{[1, 2, 3, 4, 5]}</AriaValueTextArcPicker>
 			<AriaValueTextArcPicker alt="Aria-valuetext and Disabled" aria-valuetext="This is a Label." disabled>{[1, 2, 3, 4, 5]}</AriaValueTextArcPicker>
 			<Button aria-label="5 way Up to move to the previous line" icon="arrowup" />
 		</Section>
