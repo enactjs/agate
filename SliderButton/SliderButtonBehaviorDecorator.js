1fc5a820b82537b3d12f613249abfb735eb4cb03
Adrian Cocoara <adrian.cocoara@lgepartner.com>
2021-01-19 11:40:39 +0200

    *WIP* 5-way navigation for SliderButton

--- SliderButton/SliderButtonBehaviorDecorator.js
+++ SliderButton/SliderButtonBehaviorDecorator.js
@@ -1,8 +1,12 @@
 import {forward} from '@enact/core/handle';
+import {is} from '@enact/core/keymap';
 import platform from '@enact/core/platform';
 import PropTypes from 'prop-types';
 import React from 'react';
 import {findDOMNode} from 'react-dom';
+
+const isLeft = is('left');
+const isRight = is('right');
 
 /**
  * SliderButtonBehaviorDecorator passes props to support a11y feature in SliderButton
@@ -17,6 +21,7 @@ const SliderButtonBehaviorDecorator = (Wrapped) => {
 		const {children} = props;
 		const [valueText, setValueText] = React.useState(children ? children[0] : null);
 		const ref = React.useRef();
+		console.log(props);
 
 		function handleChange ({value}) {
 			setValueText(children[value]);
@@ -33,6 +38,45 @@ const SliderButtonBehaviorDecorator = (Wrapped) => {
 			}
 		}
 
+		function handleKeyDown (ev, props) {
+			forward('onKeyDown', ev, props);
+
+			if (isLeft(ev.keyCode)) {
+				handleChange({value: 0});
+				// forward('onKeyDown', ev, props);
+			} else if (isRight(ev.keyCode)) {
+				handleChange({value: 1});
+				// forward('onKeyDown', ev, props);
+			}
+		}
+
+		// function handleKeyDown (ev, props) {
+		// 	const value = ((valueText || valueText === 0) ? valueText : children[0]);
+		// 	const index = children.findIndex(child => child === value);
+		//
+		// 	forward('onKeyDown', ev, props);
+		//
+		// 	if (!disabled) {
+		// 		if (isLeft(ev.keyCode)) {
+		// 			handleChange(valueText);
+		// 		} else if (isRight(ev.keyCode)) {
+		// 			handleChange(children[Math.min(index + 1, value.length - 1)]);
+		// 		}
+		// 	}
+		// }
+
+		// function handleKeyDown ({value}) {
+		// 	forward('onKeyDown', {
+		// 		type: 'onChange',
+		// 		value
+		// 	}, props);
+		// 	setValueText(children[value]);
+		// }
+
+		// function handleKeyDown () {
+		// 	console.log('test');
+		// }
+
 		return (
 			<Wrapped
 				aria-valuetext={valueText}
@@ -41,6 +85,7 @@ const SliderButtonBehaviorDecorator = (Wrapped) => {
 				{...props}
 				onChange={handleChange}
 				onDragStart={handleDragStart}
+				onKeyDown={handleKeyDown}
 			/>
 		);
 	}
