8329ab44911ccac806351e0cf35afd6a5f269911
Stanca Pop <stanca.pop@lgepartner.com>
2021-02-04 20:18:48 +0200

    updated screenshot tests

--- tests/screenshot/apps/components/LabeledIconButton.js
+++ tests/screenshot/apps/components/LabeledIconButton.js
@@ -2,13 +2,14 @@ import LabeledIconButton from '../../../../LabeledIconButton';
 import React from 'react';
 
 const LabeledIconButtonTests = [
+	<LabeledIconButton />,
 	<LabeledIconButton backgroundOpacity="lightOpaque">Hello!</LabeledIconButton>,
 	<LabeledIconButton highlighted />,
-	<LabeledIconButton icon="home">Hello Home!</LabeledIconButton>,
-	<LabeledIconButton inline size="smallest">Hello LabeledIconButton</LabeledIconButton>,
+	<LabeledIconButton icon="home" labelPosition="after">Hello Home!</LabeledIconButton>,
+	<LabeledIconButton inline labelPosition="above" size="smallest">Hello LabeledIconButton</LabeledIconButton>,
 	<LabeledIconButton selected size="small" />,
 	<LabeledIconButton size="huge" tooltipText="Hello tooltip">Hello huge button!</LabeledIconButton>,
-	<LabeledIconButton disabled>I am disabled</LabeledIconButton>
+	<LabeledIconButton disabled labelPosition="right">I am disabled</LabeledIconButton>
 ];
 
 export default LabeledIconButtonTests;
