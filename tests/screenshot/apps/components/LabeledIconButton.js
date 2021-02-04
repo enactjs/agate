0abd4218e52dc1e5fb034012477002b60db0e5be
Stanca Pop <stanca.pop@lgepartner.com>
2021-02-04 17:19:11 +0200

    changed size variable value, updated screenshot tests

--- tests/screenshot/apps/components/LabeledIconButton.js
+++ tests/screenshot/apps/components/LabeledIconButton.js
@@ -2,9 +2,13 @@ import LabeledIconButton from '../../../../LabeledIconButton';
 import React from 'react';
 
 const LabeledIconButtonTests = [
-	<LabeledIconButton />,
+	<LabeledIconButton backgroundOpacity="lightOpaque">Hello!</LabeledIconButton>,
 	<LabeledIconButton highlighted />,
-	<LabeledIconButton icon="plus" />
+	<LabeledIconButton icon="home">Hello Home!</LabeledIconButton>,
+	<LabeledIconButton inline size="smallest">Hello LabeledIconButton</LabeledIconButton>,
+	<LabeledIconButton selected size="small" />,
+	<LabeledIconButton size="huge" tooltipText="Hello tooltip">Hello huge button!</LabeledIconButton>,
+	<LabeledIconButton disabled>I am disabled</LabeledIconButton>
 ];
 
 export default LabeledIconButtonTests;
