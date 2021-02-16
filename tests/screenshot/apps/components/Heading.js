68b6de3d46639324a9727c428656c18d69c426b3
Stanca Pop <stanca.pop@lgepartner.com>
2021-02-16 17:24:16 +0200

    review fixes

--- tests/screenshot/apps/components/Heading.js
+++ tests/screenshot/apps/components/Heading.js
@@ -4,7 +4,8 @@ import React from 'react';
 const HeadingTests = [
 	<Heading />,
 	<Heading size="title">This is a new Heading</Heading>,
-	<Heading showLine spacing="small" />
+	<Heading showLine spacing="small">This Heading has a line underneath</Heading>,
+	<Heading disabled showBackButton>This Heading also shows a back button</Heading>
 ];
 
 export default HeadingTests;
