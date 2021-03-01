198923617acef715ce21338894eabd4407ae95c4
Stanca Pop <stanca.pop@lgepartner.com>
2021-03-01 14:41:41 +0200

    updates ss tests

--- tests/screenshot/apps/components/LabeledIcon.js
+++ tests/screenshot/apps/components/LabeledIcon.js
@@ -2,8 +2,10 @@ import LabeledIcon from '../../../../LabeledIcon';
 
 const LabeledIconTests = [
 	<LabeledIcon />,
-	<LabeledIcon flip="horizontal" />,
-	<LabeledIcon icon="time" />
+	<LabeledIcon flip="horizontal" icon="temperature">Hello LabeledIcon</LabeledIcon>,
+	<LabeledIcon icon="time">Hello LabeledIcon</LabeledIcon>,
+	<LabeledIcon icon="menu" labelPosition="left">Hello LabeledIcon</LabeledIcon>,
+	<LabeledIcon icon="search" size="small">Hello LabeledIcon</LabeledIcon>
 ];
 
 export default LabeledIconTests;
