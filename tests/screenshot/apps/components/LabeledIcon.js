8dff1ff10c0f6981e8d05a4c440a2073e1be5a08
Stanca Pop <stanca.pop@lgepartner.com>
2021-03-03 11:06:48 +0200

    added more screenshot tests

--- tests/screenshot/apps/components/LabeledIcon.js
+++ tests/screenshot/apps/components/LabeledIcon.js
@@ -5,7 +5,11 @@ const LabeledIconTests = [
 	<LabeledIcon flip="horizontal" icon="temperature">Hello LabeledIcon</LabeledIcon>,
 	<LabeledIcon icon="time">Hello LabeledIcon</LabeledIcon>,
 	<LabeledIcon icon="menu" labelPosition="left">Hello LabeledIcon</LabeledIcon>,
-	<LabeledIcon icon="search" size="small">Hello LabeledIcon</LabeledIcon>
+	<LabeledIcon icon="temperature" labelPosition="above" size="smallest">Hello LabeledIcon</LabeledIcon>,
+	<LabeledIcon icon="search" labelPosition="before" size="small">Hello LabeledIcon</LabeledIcon>,
+	<LabeledIcon icon="sun" inline labelPosition="after">Hello LabeledIcon</LabeledIcon>,
+	<LabeledIcon disabled icon="temperature">Hello LabeledIcon</LabeledIcon>,
+	<LabeledIcon flip="both" icon="stop" labelPosition="right" size="huge">Hello LabeledIcon</LabeledIcon>
 ];
 
 export default LabeledIconTests;
