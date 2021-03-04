5e8d9060ae656827b5572a883f8195402b927491
Stanca <63341832+stanca-pop-lgp@users.noreply.github.com>
2021-03-04 13:56:13 +0200

    Update tests/screenshot/apps/components/MediaPlayer.js
    Co-authored-by: Jeonghee Ahn <jeonghee27.ahn@lge.com>

--- tests/screenshot/apps/components/MediaPlayer.js
+++ tests/screenshot/apps/components/MediaPlayer.js
@@ -4,7 +4,7 @@ const MediaPlayerTests = [
 	<MediaPlayer>
 		<source src="https://sampleswap.org/mp3/artist/254731/BossPlayer_Your-Right-Here-160.mp3" type="audio/mp3" />
 	</MediaPlayer>,
-	<MediaPlayer spotlightDisabled>
+	<MediaPlayer disabled>
 		<source src="https://sampleswap.org/mp3/artist/254731/BossPlayer_Your-Right-Here-160.mp3" type="audio/mp3" />
 	</MediaPlayer>,
 	<MediaPlayer paused>
