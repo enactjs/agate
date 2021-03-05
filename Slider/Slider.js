078dafe6e443777054b14269e09de6b9c552cd5a
Daniel Stoian <daniel.stoian@lgepartner.com>
2021-03-05 11:03:05 +0200

    fixes

--- Slider/Slider.js
+++ Slider/Slider.js
@@ -74,14 +74,14 @@ const SliderBase = kind({
 		 */
 		active: PropTypes.bool,
 
-		// /**
-		//  * Background progress, as a proportion between `0` and `1`.
-		//  *
-		//  * @type {Number}
-		//  * @default 0
-		//  * @public
-		//  */
-		// backgroundProgress: PropTypes.number,
+		/**
+		 * Background progress, as a proportion between `0` and `1`.
+		 *
+		 * @type {Number}
+		 * @default 0
+		 * @public
+		 */
+		backgroundProgress: PropTypes.number,
 
 		/**
 		 * Customizes the component by mapping the supplied collection of CSS class names to the
