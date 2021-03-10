da468a34b71a30eb713ecbffb66023db8c14a3d4
Daniel Stoian <daniel.stoian@lgepartner.com>
2021-03-10 16:36:31 +0200

    adapted styling for DrumPicker

--- samples/sampler/stories/default/Picker.js
+++ samples/sampler/stories/default/Picker.js
@@ -12,19 +12,22 @@ storiesOf('Agate', module)
 	.add(
 		'Picker',
 		() => (
-			<Picker
-				aria-label={text('aria-label', Config, '')}
-				decrementAriaLabel={text('decrementAriaLabel', Config, '')}
-				disabled={boolean('disabled', Config)}
-				incrementAriaLabel={text('incrementAriaLabel', Config, '')}
-				noAnimation={boolean('noAnimation', Config)}
-				onChange={action('onChange')}
-				orientation={select('orientation', ['vertical', 'horizontal'], Config)}
-				spotlightDisabled={boolean('spotlightDisabled', Config)}
-				wrap={boolean('wrap', Config)}
-			>
-				{['LO', '16\xB0', '17\xB0', '18\xB0', '19\xB0', 'HI']}
-			</Picker>
+			<div style={{padding: '0 20%'}}>
+				<Picker
+					aria-label={text('aria-label', Config, '')}
+					decrementAriaLabel={text('decrementAriaLabel', Config, '')}
+					disabled={boolean('disabled', Config)}
+					incrementAriaLabel={text('incrementAriaLabel', Config, '')}
+					noAnimation={boolean('noAnimation', Config)}
+					onChange={action('onChange')}
+					orientation={select('orientation', ['vertical', 'horizontal'], Config)}
+					spotlightDisabled={boolean('spotlightDisabled', Config)}
+					wrap={boolean('wrap', Config)}
+				>
+					{['LO', '16\xB0', '17\xB0', '18\xB0', '19\xB0', 'HI']}
+				</Picker>
+			</div>
+
 		),
 		{
 			text: 'Basic usage of Picker'
