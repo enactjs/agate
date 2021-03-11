14637b1544904b62c862792d2a8e85da6a9cdec4
paul-beldean-lgp <77739822+paul-beldean-lgp@users.noreply.github.com>
2021-03-11 03:24:58 +0200

    PLAT-121001: Agate Docs: Write missing documentation for components (#499)
    * Added documentation
    
    * Added documentation
    
    * Added missing documentation
    
    * Fixed indentation
    
    * Fixed code review issues
    
    * Fixed code review issues
    
    * Fixed code review issues
    
    * Fixed code review issues
    
    * Fixed code review issues
    
    * Fixed review issues
    
    Co-authored-by: Daniel Stoian <daniel.stoian@lgepartner.com>

--- SliderButton/SliderButton.js
+++ SliderButton/SliderButton.js
@@ -25,10 +25,32 @@ import SliderButtonBehaviorDecorator from './SliderButtonBehaviorDecorator';
 
 import componentCss from './SliderButton.module.less';
 
+/**
+ * Renders the slider knob.
+ *
+ * @class SliderKnob
+ * @memberof agate/SliderButton
+ * @ui
+ * @private
+ */
 const SliderKnob = kind({
 	name: 'SliderKnob',
-	propTypes: {
+	propTypes: /** @lends agate/SliderButton.SliderKnob.prototype */ {
+		/**
+		 * Customizes the component by mapping the supplied collection of CSS class names to the
+		 * corresponding internal elements and states of this component.
+		 *
+		 * @type {Object}
+		 * @public
+		 */
 		css: PropTypes.object,
+
+		/**
+		 * Value of SliderButton.
+		 *
+		 * @type {Number}
+		 * @public
+		 */
 		value: PropTypes.number
 	},
 	styles: {
@@ -57,10 +79,32 @@ const SliderKnob = kind({
 	}
 });
 
+/**
+ * Renders the slider progress.
+ *
+ * @class SliderProgress
+ * @memberof agate/SliderButton
+ * @ui
+ * @private
+ */
 const SliderProgress = kind({
 	name: 'SliderProgress',
-	propTypes: {
+	propTypes: /** @lends agate/SliderButton.SliderProgress.prototype */ {
+		/**
+		 * Customizes the component by mapping the supplied collection of CSS class names to the
+		 * corresponding internal elements and states of this component.
+		 *
+		 * @type {Object}
+		 * @public
+		 */
 		css: PropTypes.object,
+
+		/**
+		 * Value of SliderProgress.
+		 *
+		 * @type {String[]}
+		 * @public
+		 */
 		values: PropTypes.arrayOf(PropTypes.string)
 	},
 	styles: {
@@ -96,8 +140,23 @@ const SliderProgress = kind({
  */
 const SliderButtonBase = kind({
 	name: 'SliderButton',
-	propTypes: {
+	propTypes: /** @lends agate/SliderButton.SliderButtonBase.prototype */ {
+		/**
+		 * The value options of SliderButtonBase.
+		 *
+		 * @type {Array}
+		 * @required
+		 * @public
+		 */
 		children: PropTypes.array.isRequired,
+
+		/**
+		 * Customizes the component by mapping the supplied collection of CSS class names to the
+		 * corresponding internal elements and states of this component.
+		 *
+		 * @type {Object}
+		 * @public
+		 */
 		css: PropTypes.object
 	},
 	styles: {
