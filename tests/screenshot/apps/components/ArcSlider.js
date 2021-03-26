e7668f032f793452a303cd121372796ef8d429d7
Paul Beldean <paul.beldean@lgepartner.com>
2021-03-26 10:36:00 +0200

    Fixed ss test focus

--- tests/screenshot/apps/components/ArcSlider.js
+++ tests/screenshot/apps/components/ArcSlider.js
@@ -3,9 +3,16 @@ import ArcSlider from '../../../../ArcSlider';
 const ArcSliderTests = [
 	<ArcSlider />,
 	<ArcSlider endAngle={300} startAngle={10} />,
-	<ArcSlider disabled endAngle={250} startAngle={30} value={100} />,
-	<ArcSlider endAngle={250} isFocused startAngle={30} value={100} />,
-	<ArcSlider disabled endAngle={250} isFocused startAngle={30} value={100} />
+	<ArcSlider endAngle={250} startAngle={30} value={50} />,
+	<ArcSlider disabled endAngle={250} startAngle={30} value={60} />,
+	{
+		component: <ArcSlider endAngle={250} startAngle={30} value={70} />,
+		focus: true
+	},
+	{
+		component: <ArcSlider disabled endAngle={250} startAngle={30} value={80} />,
+		focus: true
+	}
 ];
 
 export default ArcSliderTests;
