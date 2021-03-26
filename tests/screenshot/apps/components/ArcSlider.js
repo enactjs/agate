ada2cac0994cfdb7d104c9c908777f8316b1d0a4
Paul Beldean <paul.beldean@lgepartner.com>
2021-03-26 10:17:05 +0200

    Added ss tests

--- tests/screenshot/apps/components/ArcSlider.js
+++ tests/screenshot/apps/components/ArcSlider.js
@@ -2,7 +2,10 @@ import ArcSlider from '../../../../ArcSlider';
 
 const ArcSliderTests = [
 	<ArcSlider />,
-	<ArcSlider endAngle={300} startAngle={10} />
+	<ArcSlider endAngle={300} startAngle={10} />,
+	<ArcSlider disabled endAngle={250} startAngle={30} value={100} />,
+	<ArcSlider endAngle={250} isFocused startAngle={30} value={100} />,
+	<ArcSlider disabled endAngle={250} isFocused startAngle={30} value={100} />
 ];
 
 export default ArcSliderTests;
