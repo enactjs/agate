5deb0d4f3768e5c9583d9303adf7926123b837af
Paul Beldean <paul.beldean@lgepartner.com>
2021-03-26 10:54:26 +0200

    Changed ss tests

--- tests/screenshot/apps/components/FanSpeedControl.js
+++ tests/screenshot/apps/components/FanSpeedControl.js
@@ -1,16 +1,17 @@
 import FanSpeedControl from '../../../../FanSpeedControl';
 
 const FanSpeedControlTests = [
-	<FanSpeedControl />,
-	<FanSpeedControl min={1} />,
-	<FanSpeedControl max={10} />,
-	<FanSpeedControl value={5} />,
+	<FanSpeedControl icon="fan" min={1} />,
+	<FanSpeedControl icon="fan" max={10} />,
 	<FanSpeedControl icon="fan" min={1} max={10} />,
 	<FanSpeedControl icon="fan" min={1} max={10} value={1} />,
-	<FanSpeedControl min={1} max={10} value={5} />,
 	<FanSpeedControl icon="fan" min={1} max={10} value={5} />,
-	<FanSpeedControl icon="fan" min={1} max={10} value={10} />,
-	<FanSpeedControl icon="fan" min={1} max={10} value={5} />
+	<FanSpeedControl disabled icon="fan" min={1} max={10} value={6} />,
+	{
+		component: <FanSpeedControl icon="fan" min={1} max={10} value={7} />,
+		focus: true
+	},
+	<FanSpeedControl icon="fan" min={1} max={10} value={10} />
 ];
 
 export default FanSpeedControlTests;
