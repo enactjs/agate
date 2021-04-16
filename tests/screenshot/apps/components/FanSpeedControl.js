22de2e31e1061b92de933e048519782bd6233250
Mikyung Kim <mikyung27.kim@lge.com>
2021-04-16 09:55:19 +0900

    Made default icon for FanSpeedControl to 'fan'
    Enact-DCO-1.0-Signed-off-by: Mikyung Kim (mikyung27.kim@lge.com)

--- tests/screenshot/apps/components/FanSpeedControl.js
+++ tests/screenshot/apps/components/FanSpeedControl.js
@@ -1,17 +1,17 @@
 import FanSpeedControl from '../../../../FanSpeedControl';
 
 const FanSpeedControlTests = [
-	<FanSpeedControl icon="fan" min={1} />,
-	<FanSpeedControl icon="fan" max={10} />,
-	<FanSpeedControl icon="fan" min={1} max={10} />,
-	<FanSpeedControl icon="fan" min={1} max={10} value={1} />,
-	<FanSpeedControl icon="fan" min={1} max={10} value={5} />,
-	<FanSpeedControl disabled icon="fan" min={1} max={10} value={6} />,
+	<FanSpeedControl min={1} />,
+	<FanSpeedControl max={10} />,
+	<FanSpeedControl icon="fanoff" min={1} max={10} />,
+	<FanSpeedControl min={1} max={10} value={1} />,
+	<FanSpeedControl min={1} max={10} value={5} />,
+	<FanSpeedControl disabled min={1} max={10} value={6} />,
 	{
-		component: <FanSpeedControl icon="fan" min={1} max={10} value={7} />,
+		component: <FanSpeedControl min={1} max={10} value={7} />,
 		focus: true
 	},
-	<FanSpeedControl icon="fan" min={1} max={10} value={10} />
+	<FanSpeedControl min={1} max={10} value={10} />
 ];
 
 export default FanSpeedControlTests;
