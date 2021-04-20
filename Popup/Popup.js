1de99da465c8f16e6d903a9fab60021628ebcc2a
Adrian Cocoara <adrian.cocoara@lgepartner.com>
2021-04-20 16:36:40 +0300

    fixed bug for long title in popup

--- Popup/Popup.js
+++ Popup/Popup.js
@@ -282,7 +282,7 @@ const PopupBase = kind({
 			return type ? type : setTransitionType;
 		}
 	},
-	render: ({buttons, children, closeButton, css, direction, duration, noAnimation, onClose, onHide, onShow, open, skin, spotlightId, spotlightRestrict, title, transitionContainerClassName, transitionType, ...rest}) => {
+	render: ({buttons, centered, children, closeButton, css, direction, duration, noAnimation, onClose, onHide, onShow, open, skin, spotlightId, spotlightRestrict, title, transitionContainerClassName, transitionType, ...rest}) => {
 		const closeButtonSize = skin === 'silicon' ? 'large' : 'small';
 		const wideLayout = (skin === 'carbon');
 		delete rest.centered;
@@ -313,7 +313,7 @@ const PopupBase = kind({
 						onTap={onClose}
 						size={closeButtonSize}
 					/> : null}
-					{title ? <Heading className={css.title} marqueeDisabled>{title}</Heading> : null}
+					{title ? <Heading className={css.title} alignment={centered ? 'center' : null}>{title}</Heading> : null}
 					<Layout orientation={wideLayout ? 'horizontal' : 'vertical'} className={css.body}>
 						<Cell shrink={!wideLayout} className={css.content}>
 							{children}
