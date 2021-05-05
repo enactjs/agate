fb66f9a1812c15c19d9a88009d1c9ddb00200ce1
Daniel Stoian <daniel.stoian@lgepartner.com>
2021-05-05 09:42:22 +0300

    added new screenshot tests

--- tests/screenshot/apps/components/Header.js
+++ tests/screenshot/apps/components/Header.js
@@ -3,7 +3,18 @@ import Header from '../../../../Header';
 const HeaderTests = [
 	<Header hideLine title="Header Title" />,
 	<Header subtitle="Subtitle" title="Header Title" />,
-	<Header title="Header Title" titleAbove="Title above" />
+	<Header title="Header Title" titleAbove="Title above" />,
+	<Header
+		title="Header Very Long Title Header Very Long Title Header Very Long Title Header Very Long Title Header Very Long Title Header Very Long Title Header Very Long Title"
+	/>,
+	<Header
+		subtitle="Very long subtitle Very long subtitle Very long subtitle Very long subtitle Very long subtitle Very long subtitle Very long subtitle Very long subtitle Very long subtitle Very long subtitle Very long subtitle Very long subtitle Very long subtitle Very long subtitle"
+		title="Header Very Long Title Header Very Long Title Header Very Long Title Header Very Long Title Header Very Long Title Header Very Long Title Header Very Long Title"
+	/>,
+	<Header
+		title="Header Very Long Title Header Very Long Title Header Very Long Title Header Very Long Title Header Very Long Title Header Very Long Title Header Very Long Title"
+		titleAbove="Very long title above Very long title above Very long title above Very long title above Very long title above Very long title above Very long title above Very long title above Very long title above Very long title above Very long title above Very long title above"
+	/>
 ];
 
 export default HeaderTests;
