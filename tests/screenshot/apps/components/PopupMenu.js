8875eabcecafee3d77af40322d71a0524ff009e7
Adrian Cocoara <adrian.cocoara@lgepartner.com>
2021-05-07 15:24:45 +0300

    fixed PopupMenu title to marquee for long text

--- tests/screenshot/apps/components/PopupMenu.js
+++ tests/screenshot/apps/components/PopupMenu.js
@@ -3,7 +3,9 @@ import PopupMenu from '../../../../PopupMenu';
 const PopupMenuTests = [
 	<PopupMenu open title="Title">PopupMenu!</PopupMenu>,
 	<PopupMenu closeButton open title="Title">PopupMenu!</PopupMenu>,
-	<PopupMenu open scrimType="transparent" title="Title">PopupMenu!</PopupMenu>
+	<PopupMenu open scrimType="transparent" title="Title">PopupMenu!</PopupMenu>,
+	<PopupMenu open title="Title">Very long title to test marquee! Very long title to test marquee! Very long title to test marquee!</PopupMenu>,
+	<PopupMenu open closeButton title="Title">Very long title to test marquee! Very long title to test marquee! Very long title to test marquee!</PopupMenu>
 ];
 
 export default PopupMenuTests;
