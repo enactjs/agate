6e53230d5f36ceb6027509491c82c91354b6e4f6
Alex Morariu <alexandru.morariu@lgepartner.com>
2021-05-12 15:49:19 +0300

    Problematic change

--- MediaPlayer/MediaControls.js
+++ MediaPlayer/MediaControls.js
@@ -251,7 +251,7 @@ const MediaControls = kind({
 					onClick={paused ? onPlay : onPause}
 					size="large"
 				>
-					<Icon css={css}>{paused ? playIcon : pauseIcon}</Icon>
+					<Icon className={css.playPauseIcon} css={css}>{paused ? playIcon : pauseIcon}</Icon>
 				</Button>
 				<Button
 					aria-label={$L('Next')}
