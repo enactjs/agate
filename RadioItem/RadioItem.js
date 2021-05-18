967ca5dbce45f9deb6b70aa69f402261043a06ec
Adrian Cocoara <adrian.cocoara@lgepartner.com>
2021-05-18 14:36:53 +0300

    fix travis

--- RadioItem/RadioItem.js
+++ RadioItem/RadioItem.js
@@ -112,7 +112,7 @@ const RadioItemBase = kind({
 				{...(skin === 'silicon') ? {size: 'small'} : {}}
 			>
 				<slotBefore>
-					<Icon className={css.icon} size={icon === 'circle' ? "small" : "smallest"}>{icon}</Icon>
+					<Icon className={css.icon} size={icon === 'circle' ? 'small' : 'smallest'}>{icon}</Icon>
 					{slotBefore}
 				</slotBefore>
 				{children}
