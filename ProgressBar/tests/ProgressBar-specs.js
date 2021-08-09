7e228dc50baddeead0c6ba7dc867efcfa10c589f
Adrian Cocoara <adrian.cocoara@lgepartner.com>
2021-08-09 19:28:25 +0300

    deleted unnecessary comment

--- ProgressBar/tests/ProgressBar-specs.js
+++ ProgressBar/tests/ProgressBar-specs.js
@@ -27,29 +27,4 @@ describe('ProgressBar Specs', () => {
 
 		expect(actual).toBe(expected);
 	});
-
-	// test('should only show tooltip when tooltip is true', () => {
-	// 	const progressBar = mount(
-	// 		<ProgressBar tooltip />
-	// 	);
-	//
-	// 	const expected = 1;
-	// 	const actual = progressBar.find('ProgressBarTooltip').length;
-	//
-	// 	expect(actual).toBe(expected);
-	// });
-	//
-	// test('should have tooltip show progress as percentage', () => {
-	// 	const progressBar = mount(
-	// 		<ProgressBar
-	// 			tooltip
-	// 			progress={0.6}
-	// 		/>
-	// 	);
-	//
-	// 	const expected = '60%';
-	// 	const actual = progressBar.find('ProgressBarTooltip').text();
-	//
-	// 	expect(actual).toBe(expected);
-	// });
 });
