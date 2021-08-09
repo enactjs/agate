99228857e31c5ec9c6c0e64d837fd0cc49e3d245
Alex Morariu <alexandru.morariu@lgepartner.com>
2021-08-09 17:29:15 +0300

    Review fixes

--- Picker/tests/Picker-specs.js
+++ Picker/tests/Picker-specs.js
@@ -5,11 +5,7 @@ import {Picker, PickerBase} from '../Picker';
 
 describe('Picker Specs', () => {
 	test('should render selected child wrapped with <PickerItem/>', () => {
-		render(
-			<Picker value={1}>
-				{[1, 2, 3, 4]}
-			</Picker>
-		);
+		render(<Picker value={1}>{[1, 2, 3, 4]}</Picker>);
 		const pickerValue = screen.getByRole('spinbutton');
 
 		const expected = '2';
@@ -18,11 +14,7 @@ describe('Picker Specs', () => {
 	});
 
 	test('should set the max of <Picker> to be one less than the number of children', () => {
-		render(
-			<Picker value={3}>
-				{[1, 2, 3, 4]}
-			</Picker>
-		);
+		render(<Picker value={3}>{[1, 2, 3, 4]}</Picker>);
 		const pickerValue = screen.getByLabelText('4 next item');
 
 		const expectedAttribute = 'disabled';
@@ -31,11 +23,7 @@ describe('Picker Specs', () => {
 	});
 
 	test('should be disabled when empty', () => {
-		render(
-			<PickerBase>
-				{[]}
-			</PickerBase>
-		);
+		render(<PickerBase>{[]}</PickerBase>);
 		const pickerPreviousValue = screen.getByLabelText('undefined previous item');
 		const pickerNextValue = screen.getByLabelText('undefined next item');
 
