7e29921cdbb77d787dbd01407f04819021e23b4e
Vladut Clapou <vladut.clapou@lgepartner.com>
2022-01-06 15:35:23 +0200

    Checkbox component migration

--- samples/sampler/stories/default/Checkbox.js
+++ samples/sampler/stories/default/Checkbox.js
@@ -1,32 +1,35 @@
-// import {action} from '@enact/storybook-utils/addons/actions';
-// import {boolean, select} from '@enact/storybook-utils/addons/knobs';
-// import {mergeComponentMetadata} from '@enact/storybook-utils';
-// import Checkbox, {CheckboxBase} from '@enact/agate/Checkbox';
+import { action } from '@enact/storybook-utils/addons/actions';
+import { boolean, select } from '@enact/storybook-utils/addons/controls';
+import { mergeComponentMetadata } from '@enact/storybook-utils';
+import Checkbox, { CheckboxBase } from '@enact/agate/Checkbox';
 
-// import {iconList} from './util/icons';
+import { iconList } from './util/icons';
 
-// Checkbox.displayName = 'Checkbox';
-// const Config = mergeComponentMetadata('Checkbox', CheckboxBase, Checkbox);
+Checkbox.displayName = 'Checkbox';
+const Config = mergeComponentMetadata('Checkbox', CheckboxBase, Checkbox);
 
-// export default {
-// 	title: 'Agate/Checkbox',
-// 	component: 'Checkbox'
-// };
+export default {
+  title: 'Agate/Checkbox',
+  component: 'Checkbox',
+};
 
-// export const _Checkbox = () => (
-// 	<Checkbox
-// 		disabled={boolean('disabled', Config)}
-// 		indeterminate={boolean('indeterminate', Config)}
-// 		indeterminateIcon={select('indeterminateIcon', ['', ...iconList], Config)}
-// 		onToggle={action('onToggle')}
-// 	>
-// 		{select('children', ['', ...iconList], Config)}
-// 	</Checkbox>
-// );
-
-// _Checkbox.storyName = 'Checkbox';
-// _Checkbox.parameters = {
-// 	info: {
-// 		text: 'The basic Checkbox'
-// 	}
-// };
+export const _Checkbox = (args) => (
+  <Checkbox
+    disabled={args['disabled']}
+    indeterminate={args['indeterminate']}
+    indeterminateIcon={args['indeterminateIcon']}
+    onToggle={action('onToggle')}
+  >
+    {args['children']}
+  </Checkbox>
+);
+boolean('disabled', _Checkbox, Config);
+boolean('indeterminate', _Checkbox, Config);
+select('indeterminateIcon', _Checkbox, ['', ...iconList], Config);
+select('children', _Checkbox, ['', ...iconList], Config);
+_Checkbox.storyName = 'Checkbox';
+_Checkbox.parameters = {
+  info: {
+    text: 'The basic Checkbox',
+  },
+};
