f7dcadf0622665a5707412bad2225596751e381f
Vladut Clapou <vladut.clapou@lgepartner.com>
2022-01-06 16:04:33 +0200

    CheckboxItem component migration

--- samples/sampler/stories/default/CheckboxItem.js
+++ samples/sampler/stories/default/CheckboxItem.js
@@ -1,46 +1,54 @@
-// import {mergeComponentMetadata} from '@enact/storybook-utils';
-// import {action} from '@enact/storybook-utils/addons/actions';
-// import {boolean, select, text} from '@enact/storybook-utils/addons/knobs';
-// import Checkbox, {CheckboxBase} from '@enact/agate/Checkbox';
-// import CheckboxItem, {CheckboxItemBase} from '@enact/agate/CheckboxItem';
-// import Icon from '@enact/agate/Icon';
-// import Item, {ItemBase} from '@enact/agate/Item';
+import {mergeComponentMetadata} from '@enact/storybook-utils';
+import {action} from '@enact/storybook-utils/addons/actions';
+import {boolean, select, text} from '@enact/storybook-utils/addons/controls';
+import Checkbox, {CheckboxBase} from '@enact/agate/Checkbox';
+import CheckboxItem, {CheckboxItemBase} from '@enact/agate/CheckboxItem';
+import Icon from '@enact/agate/Icon';
+import Item, {ItemBase} from '@enact/agate/Item';
 
-// import {iconList} from './util/icons';
+import {iconList} from './util/icons';
 
-// CheckboxItem.displayName = 'CheckboxItem';
-// const Config = mergeComponentMetadata('CheckboxItem', ItemBase, Item, CheckboxBase, Checkbox, CheckboxItemBase, CheckboxItem);
-// Config.defaultProps.icon = CheckboxBase.defaultProps.children;
+CheckboxItem.displayName = 'CheckboxItem';
+const Config = mergeComponentMetadata('CheckboxItem', ItemBase, Item, CheckboxBase, Checkbox, CheckboxItemBase, CheckboxItem);
+Config.defaultProps.icon = CheckboxBase.defaultProps.children;
 
-// export default {
-// 	title: 'Agate/CheckboxItem',
-// 	component: 'CheckboxItem'
-// };
+export default {
+	title: 'Agate/CheckboxItem',
+	component: 'CheckboxItem'
+};
 
-// export const _CheckboxItem = () => {
-// 	const slotBeforeSelection = select('slotBefore', ['', ...iconList], Config);
-// 	const slotBefore = slotBeforeSelection ? (<Icon slot="slotBefore">{slotBeforeSelection}</Icon>) : null;
+export const _CheckboxItem = (args) => {
+	// const slotBeforeSelection = select('slotBefore', ['', ...iconList], Config);
+	const slotBefore = args['slotBefore'] ? (<Icon slot="slotBefore">{args['slotBefore']}</Icon>) : null;
 
-// 	return (
-// 		<CheckboxItem
-// 			disabled={boolean('disabled', Config)}
-// 			icon={select('icon', iconList, Config)}
-// 			indeterminate={boolean('indeterminate', Config)}
-// 			indeterminateIcon={select('indeterminateIcon', iconList, Config)}
-// 			inline={boolean('inline', Config)}
-// 			label={text('label', Config)}
-// 			labelPosition={select('labelPosition', ['above', 'after', 'before', 'below'], Config)}
-// 			onToggle={action('onToggle')}
-// 		>
-// 			{slotBefore}
-// 			{text('children', Config, 'Hello CheckboxItem')}
-// 		</CheckboxItem>
-// 	);
-// };
-
-// _CheckboxItem.storyName = 'CheckboxItem';
-// _CheckboxItem.parameters = {
-// 	info: {
-// 		text: 'The basic CheckboxItem'
-// 	}
-// };
+	return (
+		<CheckboxItem
+			disabled={args['disabled']}
+			icon={args['icon']}
+			indeterminate={args['indeterminate']}
+			indeterminateIcon={args['indeterminateIcon']}
+			inline={args['inline']}
+			label={args['label']}
+			labelPosition={args['labelPosition']}
+			onToggle={action('onToggle')}
+		>
+			{slotBefore}
+			{args['children']}
+		</CheckboxItem>
+	);
+};
+boolean('disabled', _CheckboxItem,Config);
+select('icon', _CheckboxItem, iconList, Config);
+boolean('indeterminate', _CheckboxItem, Config);
+select('indeterminateIcon', _CheckboxItem, iconList, Config);
+boolean('inline', _CheckboxItem, Config)
+text('label', _CheckboxItem, Config);
+select('labelPosition', _CheckboxItem, ['above', 'after', 'before', 'below'], Config)
+text('children', _CheckboxItem, Config, 'Hello CheckboxItem')
+select('slotBefore', _CheckboxItem,  ['', ...iconList], Config)
+_CheckboxItem.storyName = 'CheckboxItem';
+_CheckboxItem.parameters = {
+	info: {
+		text: 'The basic CheckboxItem'
+	}
+};
