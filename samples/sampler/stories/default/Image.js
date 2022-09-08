d68da606b92926580e26feda21c19caaec3de562
dongsuwon <61816767+dongsuwon@users.noreply.github.com>
2022-09-08 13:47:47 +0900

    WRO-10825: Change addon 'object' to 'select' in imageItem sample. (#671)
    * WRO-10825: Change addon 'object' to 'select' in imageItem sample.
    
    * Change addon 'object' to 'select' in imageItem sample.
    
    Enact-DCO-1.0-Signed-off-by: DongSu Won (dongsu.won@lgepartner.com)
    
    * Change addon 'object' to 'select' in image sample.
    
    Enact-DCO-1.0-Signed-off-by: DongSu Won (dongsu.won@lgepartner.com)
    
    * * Add image to use local image in image and imageitem sample.
    
    Enact-DCO-1.0-Signed-off-by: DongSu Won (dongsu.won@lgepartner.com)
    
    * * Change image url in Image,ImageItem sample
    
    Enact-DCO-1.0-Signed-off-by: DongSu Won (dongsu.won@lgepartner.com)
    
    * * Remove image class in Image and ImageItem sample.
    
    Enact-DCO-1.0-Signed-off-by: DongSu Won (dongsu.won@lgepartner.com)
    
    * Fixed typo
    
    Enact-DCO-1.0-Signed-off-by: Juwon Jeong (juwon.jeong@lge.com)
    
    Co-authored-by: Juwon Jeong <juwon.jeong@lge.com>

--- samples/sampler/stories/default/Image.js
+++ samples/sampler/stories/default/Image.js
@@ -1,15 +1,21 @@
 import {action} from '@enact/storybook-utils/addons/actions';
-import {object, select} from '@enact/storybook-utils/addons/controls';
+import {select} from '@enact/storybook-utils/addons/controls';
 import {mergeComponentMetadata} from '@enact/storybook-utils';
 import ri from '@enact/ui/resolution';
 import Image, {ImageBase, ImageDecorator} from '@enact/agate/Image';
 
 import css from './Image.module.less';
 
+const svgGenerator = (width, height, bgColor, textColor, customText) => (
+	`data:image/svg+xml,%3Csvg xmlns='http://www.w3.org/2000/svg' viewBox='0 0 ${width} ${height}' width='${width}' height='${height}'%3E` +
+	`%3Crect width='${width}' height='${height}' fill='%23${bgColor}'%3E%3C/rect%3E` +
+	`%3Ctext x='50%25' y='50%25' dominant-baseline='middle' text-anchor='middle' font-size='36px' fill='%23${textColor}'%3E${customText}%3C/text%3E%3C/svg%3E`
+);
+
 const src = {
-	'hd':  'http://via.placeholder.com/200x200',
-	'fhd': 'http://via.placeholder.com/300x300',
-	'uhd': 'http://via.placeholder.com/600x600'
+	hd: svgGenerator(200, 200, '7ed31d', 'ffffff', '200 X 200'),
+	fhd: svgGenerator(300, 300, '7ed31d', 'ffffff', '300 X 300'),
+	uhd: svgGenerator(600, 600, '7ed31d', 'ffffff', '600 X 600')
 };
 
 Image.displayName = 'Image';
@@ -53,7 +59,7 @@ export const _Image = (args) => (
 );
 
 select('sizing', _Image, ['fill', 'fit', 'none'], Config, 'fill');
-object('src', _Image, Config, src);
+select('src', _Image, src, Config, src.hd);
 
 _Image.storyname = 'Image';
 _Image.parameters = {
