7dbf8792e4f0e6acc032b7351dc947ab0bd09736
dongsu.won <dongsu.won@lgepartner.com>
2022-09-08 11:36:13 +0900

    * Remove image class in Image and ImageItem sample.
    Enact-DCO-1.0-Signed-off-by: DongSu Won (dongsu.won@lgepartner.com)

--- samples/sampler/stories/default/ImageItem.js
+++ samples/sampler/stories/default/ImageItem.js
@@ -4,10 +4,14 @@ import {boolean, text, select} from '@enact/storybook-utils/addons/controls';
 import ri from '@enact/ui/resolution';
 import ImageItem, {ImageItemBase} from '@enact/agate/ImageItem';
 
+const svgGenerator = (width, height, bgColor, textColor, customText) => (
+    `data:image/svg+xml,%3Csvg xmlns='http://www.w3.org/2000/svg' viewBox='0 0 ${width} ${height}' width='${width}' height='${height}'%3E%3Crect width='${width}' height='${height}' fill='%23${bgColor}'%3E%3C/rect%3E%3Ctext x='50%25' y='50%25' dominant-baseline='middle' text-anchor='middle' font-size='36px' fill='%23${textColor}'%3E${customText}%3C/text%3E%3C/svg%3E`
+);
+
 const src = {
-	hd: "data:image/svg+xml,%3Csvg xmlns='http://www.w3.org/2000/svg' viewBox='0 0 200 200' class='img-fluid rounded mx-auto d-block' width='200' height='200'%3E%3Crect width='200' height='200' fill='%237ed31d'%3E%3C/rect%3E%3Ctext x='50%25' y='50%25' dominant-baseline='middle' text-anchor='middle' font-size='36px' fill='%23ffffff'%3E200 X 200%3C/text%3E%3C/svg%3E",
-	fhd: "data:image/svg+xml,%3Csvg xmlns='http://www.w3.org/2000/svg' viewBox='0 0 300 300' class='img-fluid rounded mx-auto d-block' width='300' height='300'%3E%3Crect width='300' height='300' fill='%237ed31d'%3E%3C/rect%3E%3Ctext x='50%25' y='50%25' dominant-baseline='middle' text-anchor='middle' font-size='36px' fill='%23ffffff'%3E300 X 300%3C/text%3E%3C/svg%3E",
-	uhd: "data:image/svg+xml,%3Csvg xmlns='http://www.w3.org/2000/svg' viewBox='0 0 600 600' class='img-fluid rounded mx-auto d-block' width='600' height='600'%3E%3Crect width='600' height='600' fill='%237ed31d'%3E%3C/rect%3E%3Ctext x='50%25' y='50%25' dominant-baseline='middle' text-anchor='middle' font-size='36px' fill='%23ffffff'%3E600 X 600%3C/text%3E%3C/svg%3E",
+	hd: svgGenerator(200, 200, '7ed31d', 'ffffff', 'image0'),
+	fhd: svgGenerator(300, 300, '7ed31d', 'ffffff', 'image0'),
+	uhd: svgGenerator(600, 600, '7ed31d', 'ffffff', 'image0'),
 	invalidUrl: "ttp://via.placeholder.com/300x300/9037ab/ffffff&text=Image0"
 };
 
