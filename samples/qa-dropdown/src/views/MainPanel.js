393e4785329ac14ea87df7773c3663c87ebe33c8
Alex Morariu <alexandru.morariu@lgepartner.com>
2022-09-19 18:21:26 +0300

    Travis PR fix.

--- samples/qa-dropdown/src/views/MainPanel.js
+++ samples/qa-dropdown/src/views/MainPanel.js
@@ -1,5 +1,6 @@
 import Button from '@enact/agate/Button';
 import Dropdown from '@enact/agate/Dropdown';
+import Header from '@enact/agate/Header';
 import {Panel, Panels} from '@enact/agate/Panels';
 import {useCallback, useState} from 'react';
 
@@ -12,7 +13,7 @@ const MainPanel = () => {
 	return (
 		<Panels noCloseButton>
 			<Panel title="QA Sample - Dropdown">
-				<Header	hideLine title="QA Sample - Dropdown" />
+				<Header hideLine title="QA Sample - Dropdown" />
 				<Dropdown onClose={handleClose} onOpen={handleOpen} open={open} size="large" title="language">
 					{['English', 'Korean', 'Spanish', 'Amharic', 'Thai', 'Arabic', 'Urdu', 'Simplified Chinese', 'Traditional Chinese', 'Vietnamese']}
 				</Dropdown>
