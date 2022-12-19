7dd1a7fb5d4158110f9f76a4a6b944acdddc80bd
Adrian Cocoara <adrian.cocoara@lgepartner.com>
2022-12-19 20:03:26 +0200

    try to import config with dynamic import

--- tests/ui/wdio.conf.js
+++ tests/ui/wdio.conf.js
@@ -1,1 +1,28 @@
-module.exports = require('@enact/ui-test-utils/ui/wdio.conf.js');
+// module.exports = require('@enact/ui-test-utils/ui/wdio.conf.js'); ORIGINAL IMPORT OF CONFIG OBJECT
+
+module.exports.config = import('@enact/ui-test-utils/ui/wdio.conf.js');
+module.exports = config;
+
+const loadConfig = async () => {
+	const config = await import('@enact/ui-test-utils/ui/wdio.conf.js');
+	module.exports = config;
+};
+loadConfig();
+
+let configObject;
+const loadConfig = async () => {
+	const configObject = await import('@enact/ui-test-utils/ui/wdio.conf.js');
+	return configObject;
+}
+module.exports = loadConfig();
+
+(async () => {
+	const config = await import('@enact/ui-test-utils/ui/wdio.conf.js');
+	module.exports = config;
+})();
+
+const logConfig = async () => {
+	const config = await import('@enact/ui-test-utils/ui/wdio.conf.js');
+	console.log(config);
+}
+logConfig();
