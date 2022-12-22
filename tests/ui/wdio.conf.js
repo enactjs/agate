efb306f9b422b7ecbe7bb846608ec4d0094c93ff
Adrian Cocoara <adrian.cocoara@lgepartner.com>
2022-12-22 16:41:08 +0200

    fix import

--- tests/ui/wdio.conf.js
+++ tests/ui/wdio.conf.js
@@ -2,30 +2,3 @@
 
 import * as config from '@enact/ui-test-utils/ui/wdio.conf.js';
 export default config;
-
-// module.exports.config = import('@enact/ui-test-utils/ui/wdio.conf.js');
-// module.exports = config;
-//
-// const loadConfig = async () => {
-// 	const config = await import('@enact/ui-test-utils/ui/wdio.conf.js');
-// 	module.exports = config;
-// };
-// loadConfig();
-//
-// let configObject;
-// const loadConfig = async () => {
-// 	configObject = await import('@enact/ui-test-utils/ui/wdio.conf.js');
-// 	return configObject;
-// }
-// module.exports = loadConfig();
-//
-// (async () => {
-// 	const config = await import('@enact/ui-test-utils/ui/wdio.conf.js');
-// 	module.exports = config;
-// })();
-//
-// const logConfig = async () => {
-// 	const config = await import('@enact/ui-test-utils/ui/wdio.conf.js');
-// 	console.log(config);
-// }
-// logConfig();
--- tests/ui/wdio.docker.conf.js
+++ tests/ui/wdio.docker.conf.js
@@ -1,4 +1,4 @@
-module.exports = require('@enact/ui-test-utils/ui/wdio.docker.conf.js');
+// module.exports = require('@enact/ui-test-utils/ui/wdio.docker.conf.js');
 
-// import * as config from '@enact/ui-test-utils/ui/wdio.docker.conf.js';
-// export default config;
+import * as config from '@enact/ui-test-utils/ui/wdio.docker.conf.js';
+export default config;
