baf0ffce0ee329ed5f82a7c524767022266b12ba
jeonghee27 <jeonghee27.ahn@lge.com>
2023-01-09 16:17:38 +0900

    add onSelect on qa-a11y disabled TabGroup

--- samples/qa-a11y/src/views/TabGroup.js
+++ samples/qa-a11y/src/views/TabGroup.js
@@ -7,7 +7,7 @@ import appCss from '../App/App.module.less';
 const tabs = [
 	{title: 'Home', icon: 'home'},
 	{title: 'Settings', icon: 'setting'},
-	{title: 'Theme', icon: 'display'}
+	{title: 'Theme', icon: 'display', disabled: true}
 ];
 
 const TabGroupView = () => {
@@ -21,13 +21,13 @@ const TabGroupView = () => {
 		<>
 			<Section title="Default">
 				<TabGroup alt="Normal" onSelect={handleSelected} selectedIndex={selectedNormal} tabPosition="before" tabs={tabs} />
-				<TabGroup alt="Disabled" disabled tabPosition="before" tabs={tabs} />
+				<TabGroup alt="Disabled" disabled onSelect={handleSelected} selectedIndex={selectedNormal} tabPosition="before" tabs={tabs} />
 			</Section>
 
 			<Section className={appCss.marginTop} title="Aria-labelled">
 				<TabGroup alt="Aria-labelled" aria-label="This is a Label." onSelect={handleSelectedAriaLabelled} selectedIndex={selectedAriaLabelled} tabPosition="before" tabs={tabs} />
 				<TabGroup
-					alt="Aria-labelled and Disabled" aria-label="This is a Label." disabled tabPosition="before"
+					alt="Aria-labelled and Disabled" aria-label="This is a Label." disabled onSelect={handleSelectedAriaLabelled} selectedIndex={selectedAriaLabelled} tabPosition="before"
 					tabs={tabs}
 				/>
 			</Section>
