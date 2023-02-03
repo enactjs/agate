f81670bb9de495184fc764a337f668e941af2bc2
Stanca Pop <stanca.pop@lgepartner.com>
2023-02-03 18:29:54 +0200

    fixes unit tests

--- DropManager/tests/DropManager-specs.js
+++ DropManager/tests/DropManager-specs.js
@@ -10,7 +10,7 @@ describe('DropManager Specs', () => {
 	const DraggableCell = Draggable(Cell);
 	const containerShapes = {
 		bottom: {orientation: 'landscape', edges: {bottom: true}},
-		center: {orientation: 'landscape', edges: {top: false, bottom: false}},
+		center: {orientation: 'portrait', edges: {top: false, bottom: false}},
 		top:    {orientation: 'landscape', edges: {top: true}}
 	};
 
@@ -26,14 +26,14 @@ describe('DropManager Specs', () => {
 		);
 	});
 
-	const CustomLayoutBase = () => {
+	const CustomLayoutBase = ({bottom, center, top, ...rest}) => {
 		return (
-			<Layout orientation="vertical">
-				<DraggableCell containerShape={containerShapes.top} name="top">top</DraggableCell>
+			<Layout {...rest} orientation="vertical">
+				<DraggableCell containerShape={containerShapes.top} name="top">{top}</DraggableCell>
 				<ResponsiveLayout>
-					<DraggableCell containerShape={containerShapes.center} name="center">center</DraggableCell>
+					<DraggableCell containerShape={containerShapes.center} name="center">{center}</DraggableCell>
 				</ResponsiveLayout>
-				<DraggableCell containerShape={containerShapes.bottom} name="bottom">bottom</DraggableCell>
+				<DraggableCell containerShape={containerShapes.bottom} name="bottom">{bottom}</DraggableCell>
 			</Layout>
 		);
 	};
@@ -43,17 +43,18 @@ describe('DropManager Specs', () => {
 	);
 
 	test('should render `top`, `center` and `bottom` slots', () => {
+		console.error = jest.fn(); // eslint-disable-line no-console
 		render(
 			<Component arrangeable>
-				<top />
-				<center />
-				<bottom />
+				<top data-testid="top" />
+				<center data-testid="center" />
+				<bottom data-testid="bottom" />
 			</Component>
 		);
 
-		const topSlot = screen.getByText('top');
-		const centerSlot = screen.getByText('center');
-		const bottomSlot = screen.getByText('bottom');
+		const topSlot = screen.getByTestId('top').parentElement;
+		const centerSlot = screen.getByTestId('center').parentElement;
+		const bottomSlot = screen.getByTestId('bottom').parentElement;
 
 		expect(topSlot).toBeInTheDocument();
 		expect(centerSlot).toBeInTheDocument();
@@ -62,31 +63,35 @@ describe('DropManager Specs', () => {
 
 	test('should change `data-slot` value when changing `arrangement`', () => {
 		const {rerender} = render(
-			<Component arrangeable arrangement={{bottom: "bottom", center: "center", top: "top"}}>
-				<top />
-				<center />
-				<bottom />
+			<Component arrangeable arrangement={{bottom: "bottom", center: "center", top: "top"}} data-testid="dropManager">
+				<top data-testid="top" />
+				<center data-testid="center" />
+				<bottom data-testid="bottom" />
 			</Component>
 		);
+
+		let topSlot = screen.getByTestId('dropManager').children[0];
+
+		expect(topSlot).toHaveAttribute('data-slot', 'top');
 
 		rerender(
-			<Component arrangeable arrangement={{bottom: "center", center: "top", top: "bottom"}}>
-				<top />
-				<center />
-				<bottom />
+			<Component arrangeable arrangement={{bottom: "center", center: "top", top: "bottom"}} data-testid="dropManager">
+				<top data-testid="top" />
+				<center data-testid="center" />
+				<bottom data-testid="bottom" />
 			</Component>
 		);
 
-		const topSlot = screen.getByText('top');
+		topSlot = screen.getByTestId('dropManager').children[0];
 
 		expect(topSlot).toHaveAttribute('data-slot', 'bottom');
 	});
 
 	test('should have `responsive layout` with `align-items: center` and `justify-content: space-evenly` style', () => {
 		render(
-			<Component arrangeable arrangement={{bottom: "bottom", center: "center", top: "top"}}>
+			<Component arrangeable>
 				<top />
-				<center>
+				<center data-testid="center">
 					<ResponsiveLayout>
 						<Cell shrink>
 							<Button>1</Button>
@@ -100,7 +105,7 @@ describe('DropManager Specs', () => {
 			</Component>
 		);
 
-		const responsiveLayout = screen.getByText('center').parentElement;
+		const responsiveLayout = screen.getByTestId('center').parentElement.parentElement;
 
 		expect(responsiveLayout).toHaveStyle({'align-items': 'center', 'justify-content': 'space-evenly'});
 	});
