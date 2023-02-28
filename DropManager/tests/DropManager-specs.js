b448481840effa2a40bd7383afffcba92b43ef1d
Stanca <63341832+stanca-pop-lgp@users.noreply.github.com>
2023-02-28 02:09:13 +0200

    WRP-3197: Create DropManager qa-sample and unit tests (#721)
    * added qa-sampler for DropManager
    
    * added unit test for DropManager
    
    * added new unit test for dropManager
    
    * added responsiveBox to qa-sampler and an extra unit test for DropManager
    
    * changed readme file
    
    * fixes unit tests
    
    * minor fix
    
    * travis error fix
    
    * travis error fix
    
    * removed unused prop
    
    * review fixes
    
    * changed quotes style
    
    * updated ilib to latest version
    
    * styling fix
    
    * Update samples/qa-drag-and-drop/resources/ilibmanifest.json
    
    Co-authored-by: Seungcheon Baek <5037955+0x64@users.noreply.github.com>
    
    * review fixes
    
    * review fixes
    
    ---------
    
    Co-authored-by: Daniel Stoian <daniel.stoian@lgepartner.com>
    Co-authored-by: Seungcheon Baek <5037955+0x64@users.noreply.github.com>

---
+++ DropManager/tests/DropManager-specs.js
@@ -0,0 +1,107 @@
+import {Layout, Cell} from '@enact/ui/Layout';
+import '@testing-library/jest-dom';
+import {render, screen} from '@testing-library/react';
+
+import Button from '../../Button';
+import Droppable, {Draggable, ResponsiveBox} from '../DropManager';
+
+describe('DropManager Specs', () => {
+	const allSlotNames = ['bottom', 'center', 'top'];
+	const DraggableCell = Draggable(Cell);
+	const containerShapes = {
+		bottom: {orientation: 'landscape', edges: {bottom: true}},
+		center: {orientation: 'portrait',  edges: {top: false, bottom: false}},
+		top:    {orientation: 'landscape', edges: {top: true}}
+	};
+
+	const ResponsiveLayout = ResponsiveBox(({containerShape, ...rest}) => {
+		const orientation = (containerShape.orientation === 'portrait') ? 'vertical' : 'horizontal';
+		let axisAlign = 'space-evenly';
+
+		return (
+			<Layout align={'center ' + axisAlign} orientation={orientation} {...rest} />
+		);
+	});
+
+	const CustomLayoutBase = ({bottom, center, top, ...rest}) => {
+		return (
+			<Layout {...rest} orientation="vertical">
+				<DraggableCell containerShape={containerShapes.top} name="top">{top}</DraggableCell>
+				<ResponsiveLayout>
+					<DraggableCell containerShape={containerShapes.center} name="center">{center}</DraggableCell>
+				</ResponsiveLayout>
+				<DraggableCell containerShape={containerShapes.bottom} name="bottom">{bottom}</DraggableCell>
+			</Layout>
+		);
+	};
+
+	const Component = Droppable({slots: allSlotNames}, CustomLayoutBase);
+
+	test('should render `top`, `center` and `bottom` slots', () => {
+		console.error = jest.fn(); // eslint-disable-line no-console
+		render(
+			<Component arrangeable>
+				<top data-testid="top" />
+				<center data-testid="center" />
+				<bottom data-testid="bottom" />
+			</Component>
+		);
+
+		const topSlot = screen.getByTestId('top').parentElement;
+		const centerSlot = screen.getByTestId('center').parentElement;
+		const bottomSlot = screen.getByTestId('bottom').parentElement;
+
+		expect(topSlot).toBeInTheDocument();
+		expect(centerSlot).toBeInTheDocument();
+		expect(bottomSlot).toBeInTheDocument();
+	});
+
+	test('should change `data-slot` value when changing `arrangement`', () => {
+		const {rerender} = render(
+			<Component arrangeable arrangement={{bottom: 'bottom', center: 'center', top: 'top'}} data-testid="dropManager">
+				<top />
+				<center />
+				<bottom />
+			</Component>
+		);
+
+		let topSlot = screen.getByTestId('dropManager').children[0];
+
+		expect(topSlot).toHaveAttribute('data-slot', 'top');
+
+		rerender(
+			<Component arrangeable arrangement={{bottom: 'center', center: 'top', top: 'bottom'}} data-testid="dropManager">
+				<top />
+				<center />
+				<bottom />
+			</Component>
+		);
+
+		topSlot = screen.getByTestId('dropManager').children[0];
+
+		expect(topSlot).toHaveAttribute('data-slot', 'bottom');
+	});
+
+	test('should have `responsive layout` with `align-items: center` and `justify-content: space-evenly` style', () => {
+		render(
+			<Component arrangeable>
+				<top />
+				<center data-testid="center">
+					<ResponsiveLayout>
+						<Cell shrink>
+							<Button>1</Button>
+						</Cell>
+						<Cell shrink>
+							<Button>2</Button>
+						</Cell>
+					</ResponsiveLayout>
+				</center>
+				<bottom />
+			</Component>
+		);
+
+		const responsiveLayout = screen.getByTestId('center').parentElement.parentElement;
+
+		expect(responsiveLayout).toHaveStyle({alignItems: 'center', justifyContent: 'space-evenly'});
+	});
+});
---
+++ samples/qa-drag-and-drop/src/views/MainPanel.js
@@ -0,0 +1,61 @@
+/* eslint-disable react/jsx-no-bind */
+
+import Button from '@enact/agate/Button';
+import {ResponsiveBox} from '@enact/agate/DropManager';
+import Heading from '@enact/agate/Heading';
+import Item from '@enact/agate/Item';
+import {Panel} from '@enact/agate/Panels';
+import Layout, {Cell} from '@enact/ui/Layout';
+import {scaleToRem} from '@enact/ui/resolution';
+import {useState} from 'react';
+
+import CustomLayout from '../components/CustomLayout';
+
+import css from './MainPanel.module.less';
+
+const ResponsiveLayout = ResponsiveBox(({containerShape, ...rest}) => {
+	const orientation = (containerShape.orientation === 'portrait') ? 'vertical' : 'horizontal';
+	let axisAlign = 'space-evenly';
+
+	return (
+		<Layout align={'center ' + axisAlign} orientation={orientation} {...rest} />
+	);
+});
+
+const MainPanel = (props) => {
+	const [arrangement, setArrangement] = useState({bottom: "bottom", center: "center", top: "top"});
+	const [editLayout, setEditLayout] = useState(false);
+
+	const handleArrangement = (ev) => {
+		setArrangement(ev.arrangement);
+	};
+
+	const handleEditing = () => {
+		setEditLayout(!editLayout);
+	};
+
+	return (
+		<Panel {...props} className={css.mainPanel}>
+			<Heading showLine>
+				Agate Drag & Drop
+			</Heading>
+			<CustomLayout arrangeable={editLayout} arrangement={arrangement} onArrange={handleArrangement} style={{height: scaleToRem(690)}}>
+				<top>
+					<Item className={css.firstItem} css={css}>Drag me</Item>
+				</top>
+				<center>
+					<ResponsiveLayout style={{height: scaleToRem(300)}}>
+						<Cell component={Button} shrink>1</Cell>
+						<Cell component={Button} shrink>2</Cell>
+					</ResponsiveLayout>
+				</center>
+				<bottom>
+					<Item className={css.secondItem} css={css}>Drag me too</Item>
+				</bottom>
+			</CustomLayout>
+			<Button onClick={handleEditing}>{!editLayout ? 'Edit Layout' : 'Done Editing'}</Button>
+		</Panel>
+	);
+};
+
+export default MainPanel;
