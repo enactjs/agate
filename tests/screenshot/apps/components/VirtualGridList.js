4c5017fec120efe62579305739459638e28ecf88
Adrian Cocoara <adrian.cocoara@lgepartner.com>
2024-08-17 12:49:10 +0300

    fix RTL horizontal VGL ss test

--- tests/screenshot/apps/components/VirtualGridList.js
+++ tests/screenshot/apps/components/VirtualGridList.js
@@ -126,6 +126,18 @@ const VirtualGridListTests = [
 
 	// RTL locale
 	...withConfig({locale: 'ar-SA'}, [
+		<div style={{padding: ri.scale(300)}}>
+			<VirtualGridList
+				dataSize={items.length}
+				direction="horizontal"
+				itemRenderer={renderItem}
+				itemSize={{
+					minWidth: ri.scale(180),
+					minHeight: ri.scale(270)
+				}}
+				style={{height: ri.scale(300)}}
+			/>
+		</div>,
 		<VirtualGridList
 			dataSize={items.length}
 			itemRenderer={renderItem}
