7e9bf43e3290750a145c70f252678450eb8a17b9
ion-andrusciac-lgp <ion.andrusciac@lgepartner.com>
2025-11-26 13:08:35 +0200

    Small fix

--- tests/ui/specs/ArcSlider/ArcSlider-specs.js
+++ tests/ui/specs/ArcSlider/ArcSlider-specs.js
@@ -10,7 +10,7 @@ describe('ArcSlider', function () {
 		const arcSlider = Page.components.arcSliderDefault;
 
 		it('should change the circle radius on focus', async function () {
-			await Page.delay(500);
+			await arcSlider.self.waitForDisplayed();
 			const initialCircleRadius = parseFloat(await arcSlider.circle.getAttribute('r'));
 			await arcSlider.focus();
 			const focusedCircleRadius = parseFloat(await arcSlider.circle.getAttribute('r'));
