f7f5067fafa81a450954e19938a588a794839a99
ion-andrusciac-lgp <122601449+ion-andrusciac-lgp@users.noreply.github.com>
2025-11-26 17:01:09 +0200

    NXT-3374: Fixed failed `ui-tests` (#847)
    * Fixed failing `ui-tests`
    
    * Fixed failing `ui-test`
    
    * Fixed failing `ui-test`
    
    * Small fix
    
    * Added new `ui-test`
    
    * Small fix
    
    * Small fix
    
    * Small fix

--- tests/ui/specs/ArcSlider/ArcSlider-specs.js
+++ tests/ui/specs/ArcSlider/ArcSlider-specs.js
@@ -9,11 +9,21 @@ describe('ArcSlider', function () {
 	describe('default', function () {
 		const arcSlider = Page.components.arcSliderDefault;
 
+		it('should change the circle radius on focus', async function () {
+			await Page.delay(500);
+			await arcSlider.blur();
+			const initialCircleRadius = parseFloat(await arcSlider.circle.getAttribute('r'));
+			await arcSlider.focus();
+			const focusedCircleRadius = parseFloat(await arcSlider.circle.getAttribute('r'));
+			expect(focusedCircleRadius).toBeGreaterThan(initialCircleRadius);
+		});
+
 		it('should change the position of the slider knob on each different click position', async function () {
 			await Page.delay(500);
 			await arcSlider.focus();
 			const {cx: originalCx, cy: originalCy} = await arcSlider.knobPosition();
 
+			await arcSlider.waitForClickable();
 			await arcSlider.self.click({x: 50, y: -70});
 			await Page.delay(500);
 
